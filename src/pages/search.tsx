6106b8f037566dcff00be799f02909708fff9633
Matthias Kretschmann <m@kretschmann.io>
2020-06-30 19:43:37 +0200

    make production build work

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,13 +3,6 @@ import PageSearch from '../components/templates/Search'
 import { PageProps } from 'gatsby'
 import Layout from '../components/Layout'
 import queryString from 'query-string'
-import {
-  Router,
-  Link,
-  createHistory,
-  createMemorySource,
-  LocationProvider
-} from '@reach/router'
 
 export default function PageGatsbySearch(props: PageProps): ReactElement {
   const parsed = queryString.parse(props.location.search)
@@ -17,7 +10,7 @@ export default function PageGatsbySearch(props: PageProps): ReactElement {
 
   return (
     <Layout title={`Search for ${text || tag}`} location={props.location}>
-      <PageSearch location={location} />
+      <PageSearch location={props.location} />
     </Layout>
   )
 }
