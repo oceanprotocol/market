f954537929af3ebafb42cdbdf3323f51e9e14234
Matthias Kretschmann <m@kretschmann.io>
2020-07-15 12:15:17 +0200

    fix env vars

--- app.config.js
+++ app.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   oceanConfig: {
-    nodeUri: process.env.NODE_URI || 'https://pacific.oceanprotocol.com',
+    nodeUri: process.env.GATSBY_NODE_URI || 'https://pacific.oceanprotocol.com',
     metadataStoreUri:
-      process.env.METADATA_STORE_URI ||
+      process.env.GATSBY_METADATA_STORE_URI ||
       'https://aquarius.rinkeby.v3.dev-ocean.com',
     providerUri:
-      process.env.PROVIDER_URI || 'https://provider.rinkeby.v3.dev-ocean.com',
+      process.env.GATSBY_PROVIDER_URI ||
+      'https://provider.rinkeby.v3.dev-ocean.com',
     factoryAddress:
-      process.env.FACTORY_ADDRESS ||
+      process.env.GATSBY_FACTORY_ADDRESS ||
       '0x00c6A0BC5cD0078d6Cd0b659E8061B404cfa5704',
     verbose: 3
   },
