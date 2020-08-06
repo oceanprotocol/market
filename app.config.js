a142509a6faa24112a2a8b6eddb92ce5e8a885e1
Matthias Kretschmann <m@kretschmann.io>
2020-08-06 14:39:56 +0200

    add existing fallback for marketFeeAddress

--- app.config.js
+++ app.config.js
@@ -16,7 +16,9 @@ const appConfig = {
   },
   network,
   infuraProjectId: process.env.GATSBY_INFURA_PROJECT_ID || 'xxx',
-  marketFeeAddress: process.env.GATSBY_MARKET_FEE_ADDRESS || '0xxxx',
+  marketFeeAddress:
+    process.env.GATSBY_MARKET_FEE_ADDRESS ||
+    '0x903322C7E45A60d7c8C3EA236c5beA9Af86310c7',
   marketFeeAmount: process.env.GATSBY_MARKET_FEE_AMOUNT || '0.1' // in %
 }
 
