8316a1ab8b587021917a0fd30f7725c17d40c787
Matthias Kretschmann <m@kretschmann.io>
2020-08-07 10:17:06 +0200

    config refactor

--- src/helpers/wrapRootElement.tsx
+++ src/helpers/wrapRootElement.tsx
@@ -1,16 +1,35 @@
 import React, { ReactElement } from 'react'
 import { OceanProvider } from '@oceanprotocol/react'
-import { appConfig } from '../../app.config'
+import { ConfigHelper } from '@oceanprotocol/lib'
 import { web3ModalOpts } from '../utils/wallet'
+import { network } from '../../app.config'
 
-const wrapRootElement = ({
+function getOceanConfig(network: string) {
+  return new ConfigHelper().getConfig(
+    network,
+    process.env.GATSBY_INFURA_PROJECT_ID
+  )
+}
+
+export default function wrapRootElement({
   element
 }: {
   element: ReactElement
-}): ReactElement => (
-  <OceanProvider config={appConfig.oceanConfig} web3ModalOpts={web3ModalOpts}>
-    {element}
-  </OceanProvider>
-)
+}): ReactElement {
+  const oceanInitialConfig = getOceanConfig(network)
 
-export default wrapRootElement
+  async function handleNetworkChanged(chainId: number) {
+    const config = getOceanConfig(chainId)
+    connect(config)
+  }
+
+  return (
+    <OceanProvider
+      initialConfig={oceanInitialConfig}
+      handleNetworkChanged={handleNetworkChanged}
+      web3ModalOpts={web3ModalOpts}
+    >
+      {element}
+    </OceanProvider>
+  )
+}
