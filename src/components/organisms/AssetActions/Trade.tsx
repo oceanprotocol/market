7e10f2232c0cf8ac5025fc0028207fa37f2bb247
Matthias Kretschmann <m@kretschmann.io>
2020-08-18 00:21:42 +0200

    get all the tokens

--- src/components/organisms/AssetActions/Trade.tsx
+++ src/components/organisms/AssetActions/Trade.tsx
@@ -5,19 +5,40 @@ import { DDO } from '@oceanprotocol/lib'
 export default function Trade({ ddo }: { ddo: DDO }): ReactElement {
   const { ocean, accountId } = useOcean()
   const [finalTokens, setFinalTokens] = useState()
+  const [currentTokens, setCurrentTokens] = useState<string[]>()
+  const [numTokens, setNumTokens] = useState()
 
   const poolAddress = '0xunknown' // How to get this?
 
   useEffect(() => {
     async function init() {
-      const finalTokens = await ocean.pool.getFinalTokens(
-        accountId,
-        poolAddress
-      )
-      setFinalTokens(finalTokens)
+      try {
+        const numTokens = await ocean.pool.getNumTokens(accountId, poolAddress)
+        setNumTokens(numTokens)
+
+        const currentTokens = await ocean.pool.getCurrentTokens(
+          accountId,
+          poolAddress
+        )
+        setCurrentTokens(currentTokens)
+
+        const finalTokens = await ocean.pool.getFinalTokens(
+          accountId,
+          poolAddress
+        )
+        setFinalTokens(finalTokens)
+      } catch (error) {
+        console.error(error.message)
+      }
     }
     init()
   }, [])
 
-  return <div>Final Tokens: {finalTokens}</div>
+  return (
+    <>
+      <div>Num Tokens: {numTokens}</div>
+      <div>Current Tokens: {currentTokens}</div>
+      <div>Final Tokens: {finalTokens}</div>
+    </>
+  )
 }
