d476259d7f44465f2f7d4b1f05cf10d476998196
Matthias Kretschmann <m@kretschmann.io>
2020-09-29 21:25:17 +0200

    dummy data test

--- src/components/pages/History/PoolTransactions.tsx
+++ src/components/pages/History/PoolTransactions.tsx
@@ -1,6 +1,36 @@
-import { PoolLogs } from '@oceanprotocol/lib/dist/node/balancer/OceanPool'
+import {
+  PoolAction,
+  PoolLogs
+} from '@oceanprotocol/lib/dist/node/balancer/OceanPool'
 import { useOcean } from '@oceanprotocol/react'
 import React, { ReactElement, useEffect, useState } from 'react'
+import DataTable from 'react-data-table-component'
+
+const data: PoolLogs = {
+  joins: [
+    {
+      poolAddress: '0xxxxx',
+      caller: '',
+      transactionHash: '0xxxxx',
+      blockNumber: 2,
+      tokenIn: 'OCEAN',
+      tokenAmountIn: '10'
+    }
+  ],
+  exits: [],
+  swaps: []
+}
+
+const columns = [
+  {
+    name: 'Title',
+    selector: (row: PoolAction) => `Add ${row.tokenAmountIn} ${row.tokenIn}`
+  },
+  {
+    name: 'Pool',
+    selector: 'poolAddress'
+  }
+]
 
 export default function PoolTransactions(): ReactElement {
   const { ocean, accountId } = useOcean()
@@ -18,6 +48,7 @@ export default function PoolTransactions(): ReactElement {
 
   return (
     <div>
+      <DataTable columns={columns} data={data.joins} />
       <pre>
         <code>{JSON.stringify(logs, null, 2)}</code>
       </pre>
