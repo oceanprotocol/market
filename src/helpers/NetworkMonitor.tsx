9a321e2500644b7d70897ce72dae6fe88a25e06a
Matthias Kretschmann <m@kretschmann.io>
2020-10-02 14:33:46 +0200

    another network change fix

--- src/helpers/NetworkMonitor.tsx
+++ src/helpers/NetworkMonitor.tsx
@@ -1,16 +1,30 @@
 import React, { ReactElement, useEffect } from 'react'
 import { useOcean } from '@oceanprotocol/react'
 import { getOceanConfig } from './wrapRootElement'
+import appConfig from '../../app.config'
+import { Logger } from '@oceanprotocol/lib'
 
 export function NetworkMonitor(): ReactElement {
+  const { metadataStoreUri } = appConfig
   const { connect, web3Provider } = useOcean()
 
   useEffect(() => {
     if (!web3Provider) return
 
     async function handleNetworkChanged(chainId: string) {
-      const config = getOceanConfig(Number(chainId.replace('0x', '')))
-      await connect(config)
+      const initialConfig = getOceanConfig(Number(chainId.replace('0x', '')))
+
+      const newConfig = {
+        ...initialConfig,
+        // add metadataStoreUri only when defined
+        ...(metadataStoreUri && { metadataStoreUri })
+      }
+
+      try {
+        await connect(newConfig)
+      } catch (error) {
+        Logger.error(error.message)
+      }
     }
 
     web3Provider.on('chainChanged', handleNetworkChanged)
@@ -18,7 +32,7 @@ export function NetworkMonitor(): ReactElement {
     return () => {
       web3Provider.removeListener('chainChanged', handleNetworkChanged)
     }
-  }, [web3Provider, connect])
+  }, [web3Provider, connect, metadataStoreUri])
 
   return <></>
 }
