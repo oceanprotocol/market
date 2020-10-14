48d5cd8765be02cc350c82566e1062e8b5e08697
Matthias Kretschmann <m@kretschmann.io>
2020-10-14 20:19:03 +0200

    copy

--- src/components/organisms/AssetActions/Pool/Add.tsx
+++ src/components/organisms/AssetActions/Pool/Add.tsx
@@ -21,6 +21,7 @@ const contentQuery = graphql`
           childContentJson {
             pool {
               add {
+                title
                 output {
                   titleIn
                   titleOut
