ebc1e22d888c9fd804e95cfa64be749c8a82b6f2
mihaisc <mihai.scarlat@smartcontrol.ro>
2020-10-23 18:56:04 +0300

    fix
    Signed-off-by: mihaisc <mihai.scarlat@smartcontrol.ro>

--- src/utils/checkPreviousOrder.ts
+++ src/utils/checkPreviousOrder.ts
@@ -7,11 +7,12 @@ export default async function checkPreviousOrder(
   serviceType: ServiceType
 ) {
   const service = ddo.findServiceByType(serviceType)
+  // apparenlty cost and timeout are not found, even though they are there...
   const previousOrder = await ocean.datatokens.getPreviousValidOrders(
     ddo.dataToken,
-    service.attributes.main.cost,
+    (service.attributes.main as any).cost,
     service.index,
-    service.attributes.main.timeout,
+    (service.attributes.main as any).timeout,
     accountId
   )
   return previousOrder
