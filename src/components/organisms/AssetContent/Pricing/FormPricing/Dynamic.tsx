d0c65a693595b0f69e351424760325ea6c44c5c2
Matthias Kretschmann <m@kretschmann.io>
2020-10-26 15:15:01 +0100

    price refactor

--- src/components/organisms/AssetContent/Pricing/FormPricing/Dynamic.tsx
+++ src/components/organisms/AssetContent/Pricing/FormPricing/Dynamic.tsx
@@ -12,6 +12,8 @@ import stylesIndex from './index.module.css'
 import { useFormikContext } from 'formik'
 import { PriceOptionsMarket } from '../../../../../@types/MetaData'
 import { DDO } from '@oceanprotocol/lib'
+import Fixed from './Fixed'
+import Price from './Price'
 
 export default function Dynamic({
   ddo,
@@ -67,6 +69,12 @@ export default function Dynamic({
         )}
         <Wallet />
       </aside>
+
+      <h4 className={styles.title}>
+        Price <Tooltip content={content.tooltips.poolInfo} />
+      </h4>
+
+      <Price ddo={ddo} />
 
       <h4 className={styles.title}>
         Datatoken Liquidity Pool <Tooltip content={content.tooltips.poolInfo} />
--- src/components/organisms/AssetContent/Pricing/FormPricing/Fixed.tsx
+++ src/components/organisms/AssetContent/Pricing/FormPricing/Fixed.tsx
@@ -2,12 +2,8 @@ import React, { ReactElement } from 'react'
 import stylesIndex from './index.module.css'
 import styles from './Fixed.module.css'
 import FormHelp from '../../../../atoms/Input/Help'
-import Conversion from '../../../../atoms/Price/Conversion'
-import { useField } from 'formik'
-import Input from '../../../../atoms/Input'
-import Error from './Error'
 import { DDO } from '@oceanprotocol/lib'
-import { usePricing } from '@oceanprotocol/react'
+import Price from './Price'
 
 export default function Fixed({
   ddo,
@@ -16,38 +12,10 @@ export default function Fixed({
   ddo: DDO
   content: any
 }): ReactElement {
-  const [field, meta] = useField('price')
-  const { dtName, dtSymbol } = usePricing(ddo)
-
   return (
     <div className={styles.fixed}>
       <FormHelp className={stylesIndex.help}>{content.info}</FormHelp>
-
-      <div className={styles.grid}>
-        <div className={styles.form}>
-          <Input
-            label="Ocean Token"
-            value={field.value}
-            name="price"
-            type="number"
-            prefix="OCEAN"
-            min="1"
-            {...field}
-            additionalComponent={
-              <Conversion
-                price={field.value}
-                className={stylesIndex.conversion}
-              />
-            }
-          />
-          <Error meta={meta} />
-        </div>
-        <div className={styles.datatoken}>
-          <h4>
-            = <strong>1</strong> {dtName} — {dtSymbol}
-          </h4>
-        </div>
-      </div>
+      <Price ddo={ddo} />
     </div>
   )
 }
