0208509af096a596944be1d3c40e395d313c6497
Matthias Kretschmann <m@kretschmann.io>
2020-11-03 23:27:30 +0100

    first stats

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,19 +1,45 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import styles from './Footer.module.css'
 import Markdown from '../atoms/Markdown'
 import { useSiteMetadata } from '../../hooks/useSiteMetadata'
 import { Link } from 'gatsby'
+import axios from 'axios'
+import PriceUnit from '../atoms/Price/PriceUnit'
+
+interface MarketStatsResponse {
+  datasets: number
+  owners: number
+  ocean: number
+  datatoken: number
+}
 
 export default function Footer(): ReactElement {
   const { copyright } = useSiteMetadata()
   const year = new Date().getFullYear()
 
+  const [stats, setStats] = useState<MarketStatsResponse>()
+
+  useEffect(() => {
+    async function getStats() {
+      const response = await axios('https://market-stats.vercel.app')
+      if (!response || response.status !== 200) return
+      setStats(response.data)
+    }
+    getStats()
+  }, [])
+
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
-        © {year} <Markdown text={copyright} /> — <Link to="/terms">Terms</Link>
-        {' — '}
-        <a href="https://oceanprotocol.com/privacy">Privacy</a>
+        <div className={styles.stats}>
+          Pooled <PriceUnit price={`${stats?.ocean}`} conversion />
+        </div>
+        <div>
+          © {year} <Markdown text={copyright} /> —{' '}
+          <Link to="/terms">Terms</Link>
+          {' — '}
+          <a href="https://oceanprotocol.com/privacy">Privacy</a>
+        </div>
       </div>
     </footer>
   )
