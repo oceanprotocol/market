84d1525e2777762382dbc295c227580d12f29f3f
Matthias Kretschmann <m@kretschmann.io>
2020-11-03 14:40:17 +0100

    eth address truncation in text search titles

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -4,12 +4,17 @@ import { PageProps } from 'gatsby'
 import Layout from '../components/Layout'
 import queryString from 'query-string'
 import { accountTruncate } from '../utils/wallet'
+import ethereumAddress from 'ethereum-address'
 
 export default function PageGatsbySearch(props: PageProps): ReactElement {
   const parsed = queryString.parse(props.location.search)
   const { text, owner, tags, categories } = parsed
 
-  const searchValue = text || tags || categories
+  const isETHAddress = ethereumAddress.isAddress(text as string)
+  const searchValue =
+    (isETHAddress ? accountTruncate(text as string) : text) ||
+    tags ||
+    categories
   const title = owner
     ? `Published by ${accountTruncate(owner as string)}`
     : `Search for ${searchValue || 'all data sets'}`
