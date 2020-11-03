b2bb89afd8859e172bba2e243c2c061e22d2dd43
Matthias Kretschmann <m@kretschmann.io>
2020-11-03 13:16:45 +0100

    simple footer links

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react'
 import styles from './Footer.module.css'
 import Markdown from '../atoms/Markdown'
 import { useSiteMetadata } from '../../hooks/useSiteMetadata'
+import { Link } from 'gatsby'
 
 export default function Footer(): ReactElement {
   const { copyright } = useSiteMetadata()
@@ -10,7 +11,9 @@ export default function Footer(): ReactElement {
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
-        © {year} <Markdown text={copyright} />
+        © {year} <Markdown text={copyright} /> — <Link to="/terms">Terms</Link>
+        {' — '}
+        <a href="https://oceanprotocol.com/privacy">Privacy</a>
       </div>
     </footer>
   )
