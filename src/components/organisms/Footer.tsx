3d763409814929ae34af2a84743f6a7b1ad635bd
Matthias Kretschmann <m@kretschmann.io>
2020-11-07 02:02:27 +0100

    add build id to footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -9,9 +9,23 @@ export default function Footer(): ReactElement {
   const { copyright } = useSiteMetadata()
   const year = new Date().getFullYear()
 
+  const commitBranch = process.env.VERCEL_GITHUB_COMMIT_REF || 'dev'
+  const commitId = process.env.VERCEL_GITHUB_COMMIT_SHA
+  const isMainBranch = commitBranch === 'main'
+
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
+        <a
+          className={styles.commitId}
+          href={`https://github.com/oceanprotocol/market/tree/${
+            isMainBranch ? commitId : commitBranch
+          }`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {isMainBranch ? commitId.substring(0, 7) : commitBranch}
+        </a>
         <MarketStats />© {year} <Markdown text={copyright} /> —{' '}
         <Link to="/terms">Terms</Link>
         {' — '}
