39bd767affbe2edcf864e676015bd2dde476cc26
Matthias Kretschmann <m@kretschmann.io>
2020-11-27 15:53:16 +0100

    add copy

--- src/components/organisms/AssetActions/Pool/Add/Output.tsx
+++ src/components/organisms/AssetActions/Pool/Add/Output.tsx
@@ -1,5 +1,6 @@
 import { graphql, useStaticQuery } from 'gatsby'
 import React, { ReactElement } from 'react'
+import FormHelp from '../../../../atoms/Input/Help'
 import Token from '../Token'
 import styles from './Output.module.css'
 
@@ -41,22 +42,30 @@ export default function Output({
     titleOut
   } = data.content.edges[0].node.childContentJson.pool.add.output
 
+  // TODO: calculation fun!
   const poolOcean = '70'
   const poolDatatoken = '30'
 
   return (
-    <div className={styles.output}>
-      <div>
-        <p>{titleIn}</p>
-        <Token symbol="pool shares" balance={newPoolTokens} />
-        <Token symbol="% of pool" balance={newPoolShare} />
-        <Token symbol="% swap fee" balance={swapFee} />
+    <>
+      <FormHelp className={styles.help}>
+        {`Providing liquidity will earn you ${swapFee}% on every
+        transaction happening in this pool, proportionally to your share of the
+        pool. Your token input will be converted based on the weight of the pool.`}
+      </FormHelp>
+      <div className={styles.output}>
+        <div>
+          <p>{titleIn}</p>
+          <Token symbol="pool shares" balance={newPoolTokens} />
+          <Token symbol="% of pool" balance={newPoolShare} />
+          {/* <Token symbol="% swap fee" balance={swapFee} /> */}
+        </div>
+        <div>
+          <p>{titleOut}</p>
+          <Token symbol="OCEAN" balance={poolOcean} />
+          <Token symbol={dtSymbol} balance={poolDatatoken} />
+        </div>
       </div>
-      <div>
-        <p>Pool conversion</p>
-        <Token symbol="OCEAN" balance={poolOcean} />
-        <Token symbol={dtSymbol} balance={poolDatatoken} />
-      </div>
-    </div>
+    </>
   )
 }
