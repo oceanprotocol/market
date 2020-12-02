26119d3fc65ba0e35741ca8e3c5cd10efdc97fad
Matthias Kretschmann <m@kretschmann.io>
2020-12-02 14:05:12 +0100

    move copy

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,24 @@ import styles from './App.module.css'
 import { useSiteMetadata } from '../hooks/useSiteMetadata'
 import { useOcean } from '@oceanprotocol/react'
 import Alert from './atoms/Alert'
-import { PageProps } from 'gatsby'
+import { graphql, PageProps, useStaticQuery } from 'gatsby'
+
+const contentQuery = graphql`
+  query AppQuery {
+    purgatory: allFile(filter: { relativePath: { eq: "purgatory.json" } }) {
+      edges {
+        node {
+          childContentJson {
+            account {
+              title
+              description
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default function App({
   children,
@@ -14,6 +31,9 @@ export default function App({
 }: {
   children: ReactElement
 }): ReactElement {
+  const data = useStaticQuery(contentQuery)
+  const purgatory = data.purgatory.edges[0].node.childContentJson.account
+
   const { warning } = useSiteMetadata()
   const {
     isInPurgatory: isAccountInPurgatory,
@@ -31,9 +51,9 @@ export default function App({
 
         {isAccountInPurgatory && accountPurgatory && (
           <Alert
-            title="Account In Purgatory"
+            title={purgatory.title}
             badge={`Reason: ${accountPurgatory.reason}`}
-            text="No further actions are permitted by this account. For more details go to [list-purgatory](https://github.com/oceanprotocol/list-purgatory)."
+            text={purgatory.description}
             state="error"
           />
         )}
