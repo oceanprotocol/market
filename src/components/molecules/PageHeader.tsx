5f22952acec3df22ce3cb990fd494bf9278981d0
Norbi <katunanorbert@gmail.com>
2021-01-08 13:11:30 +0200

    cut asset teasers title length, variable title font size on asset detail

--- src/components/molecules/PageHeader.tsx
+++ src/components/molecules/PageHeader.tsx
@@ -1,6 +1,8 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
+import ReactDOM from "react-dom";
 import classNames from 'classnames/bind'
 import styles from './PageHeader.module.css'
+import { ca } from 'date-fns/esm/locale';
 
 const cx = classNames.bind(styles)
 
@@ -18,9 +20,36 @@ export default function PageHeader({
     center: center
   })
 
+  const [fontSize, setFontSize] = useState("")
+  const [lineHeight, setLineHeight] = useState("")
+
+  const setFontSizeAndLineHeight = (fontSize:number, linesNumber: number, lineHeight: number) => {
+    if(linesNumber>1){
+      linesNumber+=4;
+      fontSize-=linesNumber
+      lineHeight-=linesNumber
+      setFontSize(fontSize+"px")
+      setLineHeight(lineHeight+"px")
+    }
+  }
+
+  const countLines=(element: any) => {
+    var elementHeight = element.offsetHeight
+    var lineHeight = parseInt(document.defaultView.getComputedStyle(element,null).getPropertyValue("line-height"))
+    var lines:number = elementHeight / lineHeight;
+    return {linesNumber:lines, lineHeight:lineHeight};
+ }
+
+  useEffect(()=>{
+    var titleElement = document.getElementsByClassName("PageHeader-module--title--2n3-e")[0]
+    var lines  = countLines(titleElement);
+    var currentFontSize = parseInt(document.defaultView.getComputedStyle(titleElement,null).getPropertyValue("font-size"))
+    setFontSizeAndLineHeight(currentFontSize,lines.linesNumber, lines.lineHeight);
+  },[])
+
   return (
     <header className={styleClasses}>
-      <h1 className={styles.title}>{title}</h1>
+      <h1 className={styles.title} style={{fontSize: fontSize, lineHeight: lineHeight}}>{title}</h1>
       {description && <p className={styles.description}>{description}</p>}
     </header>
   )
