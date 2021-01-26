1ec3f9af53fe1d57191f9fabd2a7f2435984a2ab
mihaisc <mihai.scarlat@smartcontrol.ro>
2021-01-26 22:40:22 +0200

    down gatsby
    Signed-off-by: mihaisc <mihai.scarlat@smartcontrol.ro>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,8 +3,3 @@ import wrapRootElementWithProviders from './src/helpers/wrapRootElement'
 
 export const wrapPageElement = wrapPageElementWithStyles
 export const wrapRootElement = wrapRootElementWithProviders
-
-// IntersectionObserver polyfill for gatsby-image (Safari, IE)
-if (typeof window.IntersectionObserver === 'undefined') {
-  import('intersection-observer')
-}
