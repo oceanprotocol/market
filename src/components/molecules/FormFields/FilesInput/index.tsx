a5f4c26910b057be8793ff54b09e35378f360649
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2021-02-01 14:07:36 +0200

    used provider api to check file'

--- src/components/molecules/FormFields/FilesInput/index.tsx
+++ src/components/molecules/FormFields/FilesInput/index.tsx
@@ -3,24 +3,14 @@ import { useField } from 'formik'
 import { toast } from 'react-toastify'
 import FileInfo from './Info'
 import FileInput from './Input'
-import { InputProps } from '../../../atoms/Input'
 import { useOcean } from '@oceanprotocol/react'
-import { File as FileMetadata } from '@oceanprotocol/lib'
+import { InputProps } from '../../../atoms/Input'
+import { getFileInfo } from '../../../../utils'
 
 export default function FilesInput(props: InputProps): ReactElement {
   const [field, meta, helpers] = useField(props.name)
   const [isLoading, setIsLoading] = useState(false)
-  const { ocean } = useOcean()
-
-  async function checkFileUrl(url: string): Promise<FileMetadata> {
-    const filesArray: FileMetadata[] = await ocean.provider.fileinfo(url)
-    if (filesArray[0] && filesArray[0].valid) {
-      filesArray[0].url = url
-      return filesArray[0]
-    }
-    toast.error('Could not fetch file info. Please check URL and try again')
-    return null
-  }
+  const { config } = useOcean()
 
   async function handleButtonClick(e: React.SyntheticEvent, url: string) {
     // hack so the onBlur-triggered validation does not show,
@@ -32,12 +22,10 @@ export default function FilesInput(props: InputProps): ReactElement {
 
     try {
       setIsLoading(true)
-      const fileInfo = await checkFileUrl(url)
+      const fileInfo = await getFileInfo(url, config.providerUri)
       fileInfo && helpers.setValue([fileInfo])
     } catch (error) {
-      toast.error(
-        'Could not fetch file info. Please connect your Web3 wallet and try again'
-      )
+      toast.error('Could not fetch file info. Please check URL and try again')
       console.error(error.message)
     } finally {
       setIsLoading(false)
