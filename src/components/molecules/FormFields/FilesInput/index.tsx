4f66fe814717cff809fce22f171800cf866298ff
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2021-02-02 17:34:09 +0200

    cancel fileinfo req on unmount

--- src/components/molecules/FormFields/FilesInput/index.tsx
+++ src/components/molecules/FormFields/FilesInput/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react'
+import React, { ReactElement, useState, useEffect } from 'react'
 import axios from 'axios'
 import { useField } from 'formik'
 import { toast } from 'react-toastify'
@@ -11,8 +11,34 @@ import { fileinfo } from '../../../../utils/provider'
 export default function FilesInput(props: InputProps): ReactElement {
   const [field, meta, helpers] = useField(props.name)
   const [isLoading, setIsLoading] = useState(false)
+  const [fileUrl, setFileUrl] = useState<string>()
   const { config } = useOcean()
-  const source = axios.CancelToken.source()
+
+  useEffect(() => {
+    const source = axios.CancelToken.source()
+
+    async function validateUrl() {
+      try {
+        setIsLoading(true)
+        const checkedFile = await fileinfo(
+          fileUrl,
+          config.providerUri,
+          undefined
+        )
+        checkedFile && helpers.setValue([checkedFile])
+      } catch (error) {
+        toast.error('Could not fetch file info. Please check URL and try again')
+        console.error(error.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fileUrl && validateUrl()
+
+    return () => {
+      source.cancel()
+    }
+  }, [fileUrl])
 
   async function handleButtonClick(e: React.SyntheticEvent, url: string) {
     // hack so the onBlur-triggered validation does not show,
@@ -22,16 +48,7 @@ export default function FilesInput(props: InputProps): ReactElement {
     // File example 'https://oceanprotocol.com/tech-whitepaper.pdf'
     e.preventDefault()
 
-    try {
-      setIsLoading(true)
-      const checkedFile = await fileinfo(url, config.providerUri, source.token)
-      checkedFile && helpers.setValue([checkedFile])
-    } catch (error) {
-      toast.error('Could not fetch file info. Please check URL and try again')
-      console.error(error.message)
-    } finally {
-      setIsLoading(false)
-    }
+    setFileUrl(url)
   }
 
   return (
--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -1,32 +1,42 @@
 import axios, { CancelToken, AxiosResponse } from 'axios'
 import { toast } from 'react-toastify'
-import { File as FileMetadata } from '@oceanprotocol/lib'
+import { File as FileMetadata, Logger } from '@oceanprotocol/lib'
 
 export async function fileinfo(
   url: string,
   providerUri: string,
   cancelToken: CancelToken
 ): Promise<FileMetadata> {
-  const response: AxiosResponse = await axios({
-    method: 'POST',
-    url: `${providerUri}/api/v1/services/fileinfo`,
-    data: { url, cancelToken }
-  })
-  console.log(response.data[0])
-  if (!response || response.status !== 200 || !response.data) {
-    toast.error('Could not connect to File API')
-    return
-  }
-  if (!response.data[0] || !response.data[0].valid) {
-    toast.error('Could not fetch file info. Please check URL and try again')
-    return
-  }
+  try {
+    const response: AxiosResponse = await axios.post(
+      `${providerUri}/api/v1/services/fileinfo`,
+      {
+        url,
+        cancelToken
+      }
+    )
 
-  const { contentLength, contentType } = response.data[0]
+    if (!response || response.status !== 200 || !response.data) {
+      toast.error('Could not connect to File API')
+      return
+    }
+    if (!response.data[0] || !response.data[0].valid) {
+      toast.error('Could not fetch file info. Please check URL and try again')
+      return
+    }
 
-  return {
-    contentLength,
-    contentType: contentType || '', // need to do that cause lib-js File interface requires contentType
-    url
+    const { contentLength, contentType } = response.data[0]
+
+    return {
+      contentLength,
+      contentType: contentType || '', // need to do that cause lib-js File interface requires contentType
+      url
+    }
+  } catch (error) {
+    if (axios.isCancel(error)) {
+      Logger.log(error.message)
+    } else {
+      Logger.error(error.message)
+    }
   }
 }
