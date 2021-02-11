3e78ce78e0d2e2b0d29cdfe9e1a6ba1c2617c2c7
Norbi <katunanorbert@gmail.com>
2021-02-11 14:16:35 +0200

    added pagination to Published list, used aquarius to get assets

--- src/components/pages/History/PublishedList.tsx
+++ src/components/pages/History/PublishedList.tsx
@@ -4,19 +4,38 @@ import { useOcean } from '@oceanprotocol/react'
 import React, { ReactElement, useEffect, useState } from 'react'
 import Loader from '../../atoms/Loader'
 import AssetList from '../../organisms/AssetList'
+import axios from 'axios'
+import { queryMetadata } from '../../../utils/aquarius'
 
 export default function PublishedList(): ReactElement {
-  const { ocean, status, accountId } = useOcean()
+  const { accountId } = useOcean()
   const [queryResult, setQueryResult] = useState<QueryResult>()
   const [isLoading, setIsLoading] = useState(false)
+  const { config } = useOcean()
+  const source = axios.CancelToken.source()
+  const [page, setPage] = useState<number>(1)
 
   useEffect(() => {
     async function getPublished() {
-      if (!accountId || !ocean) return
-
+      if (!accountId) return
+      const queryPublishedAssets = {
+        page: page,
+        offset: 9,
+        query: {
+          nativeSearch: 1,
+          query_string: {
+            query: `(publicKey.owner:${accountId})`
+          }
+        },
+        sort: { 'price.ocean': -1 }
+      }
       try {
         setIsLoading(true)
-        const queryResult = await ocean.assets.ownerAssets(accountId)
+        const queryResult = await queryMetadata(
+          queryPublishedAssets as any,
+          config.metadataCacheUri,
+          source.token
+        )
         setQueryResult(queryResult)
       } catch (error) {
         Logger.error(error.message)
@@ -25,12 +44,20 @@ export default function PublishedList(): ReactElement {
       }
     }
     getPublished()
-  }, [ocean, status, accountId])
+  }, [accountId, page])
 
   return isLoading ? (
     <Loader />
-  ) : accountId && ocean && queryResult ? (
-    <AssetList assets={queryResult.results} showPagination={false} />
+  ) : accountId && queryResult ? (
+    <AssetList
+      assets={queryResult.results}
+      showPagination
+      page={queryResult.page}
+      totalPages={queryResult.totalPages}
+      onPageChange={(newPage) => {
+        setPage(newPage)
+      }}
+    />
   ) : (
     <div>Connect your wallet to see your published data sets.</div>
   )
