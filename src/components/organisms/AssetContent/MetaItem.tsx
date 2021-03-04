fd619b5b4b426f91302ebce39f2dffd1c1dcfedf
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2021-03-04 18:49:36 +0200

    set fix width for metadata item content

--- src/components/organisms/AssetContent/MetaItem.tsx
+++ src/components/organisms/AssetContent/MetaItem.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement, ReactNode } from 'react'
 import styles from './MetaItem.module.css'
+import { syncBuiltinESMExports } from 'module'
+import style from 'react-syntax-highlighter/dist/esm/styles/hljs/github-gist'
 
 export default function MetaItem({
   title,
@@ -11,7 +13,7 @@ export default function MetaItem({
   return (
     <div className={styles.metaItem}>
       <h3 className={styles.title}>{title}</h3>
-      {content}
+      <div className={styles.content}>{content}</div>
     </div>
   )
 }
