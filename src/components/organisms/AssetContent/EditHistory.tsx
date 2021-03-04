be82d9ef2e99c9b4ebc288f0ad85a3c4fd6f72a5
claudiaHash <49017601+claudiaHash@users.noreply.github.com>
2021-03-04 19:31:10 +0200

    Feature/edit history subgraph (#423)
    * fetching Subgraph data
    
    * updates ordered by timestamp
    
    * comment removed
    
    * comment removed
    
    * apollo generated  typing used
    
    * date converted to string
    
    Co-authored-by: claudia.holhos <claudia.holhos@hpm.ro>

--- src/components/organisms/AssetContent/EditHistory.tsx
+++ src/components/organisms/AssetContent/EditHistory.tsx
@@ -4,52 +4,53 @@ import { useAsset } from '../../../providers/Asset'
 import EtherscanLink from '../../atoms/EtherscanLink'
 import Time from '../../atoms/Time'
 import styles from './EditHistory.module.css'
+import { gql, useQuery } from '@apollo/client'
+import { ReceiptData_datatokens_updates as ReceiptData } from '../../../@types/apollo/ReceiptData'
 
-interface Receipt {
-  hash: string
-  timestamp: string
-}
-
-// TODO: fetch for real
-const fakeReceipts = [
-  {
-    hash: '0xxxxxxxxx',
-    timestamp: '1607460269'
-  },
-  {
-    hash: '0xxxxxxxxx',
-    timestamp: '1606460159'
-  },
-  {
-    hash: '0xxxxxxxxx',
-    timestamp: '1506460159'
+const getReceipts = gql`
+  query ReceiptData($address: String!) {
+    datatokens(where: { id: $address }) {
+      createTime
+      tx
+      updates(orderBy: timestamp, orderDirection: desc) {
+        id
+        tx
+        timestamp
+      }
+    }
   }
-]
+`
 
 export default function EditHistory(): ReactElement {
   const { networkId } = useOcean()
   const { ddo } = useAsset()
+  const { data } = useQuery(getReceipts, {
+    variables: { address: ddo?.dataToken.toLowerCase() }
+  })
 
-  const [receipts, setReceipts] = useState<Receipt[]>()
+  const [receipts, setReceipts] = useState<ReceiptData[]>()
+  const [creationTx, setCreationTx] = useState<string>()
 
   useEffect(() => {
-    setReceipts(fakeReceipts)
-  }, [])
+    if (!data) return
+    setReceipts(data.datatokens[0].updates)
+    setCreationTx(data.datatokens[0].tx)
+  }, [data])
 
   return (
     <>
       <h3 className={styles.title}>Metadata History</h3>
       <ul className={styles.history}>
         {receipts?.map((receipt) => (
-          <li key={receipt.hash} className={styles.item}>
-            <EtherscanLink networkId={networkId} path={`/tx/${receipt.hash}`}>
-              edited <Time date={receipt.timestamp} relative isUnix />
+          <li key={receipt.id} className={styles.item}>
+            <EtherscanLink networkId={networkId} path={`/tx/${receipt.tx}`}>
+              edited{' '}
+              <Time date={receipt.timestamp.toString()} relative isUnix />
             </EtherscanLink>
           </li>
         ))}
         <li className={styles.item}>
-          {/* TODO: get this initial metadata creation tx somehow */}
-          <EtherscanLink networkId={networkId} path="/tx/xxx">
+          <EtherscanLink networkId={networkId} path={`/tx/${creationTx}`}>
             published <Time date={ddo.created} relative />
           </EtherscanLink>
         </li>
