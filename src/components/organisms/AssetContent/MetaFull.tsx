f1196f10de5e79fd329c6a07d37278b9f7c55034
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2021-03-05 14:01:20 +0200

    removed commented block and published and updated from metafull

--- src/components/organisms/AssetContent/MetaFull.tsx
+++ src/components/organisms/AssetContent/MetaFull.tsx
@@ -29,16 +29,6 @@ export default function MetaFull(): ReactElement {
       {type === 'algorithm' && (
         <MetaItem title="Docker Image" content={<DockerImage />} />
       )}
-      {/* <MetaItem
-        title="Data Created"
-        content={<Time date={metadata?.main.dateCreated} />}
-      /> */}
-
-      {/* TODO: remove those 2 date items here when EditHistory component is ready */}
-      <MetaItem title="Published" content={<Time date={ddo?.created} />} />
-      {ddo?.created !== ddo?.updated && (
-        <MetaItem title="Updated" content={<Time date={ddo?.updated} />} />
-      )}
       <MetaItem title="DID" content={<code>{ddo?.id}</code>} />
     </div>
   )
