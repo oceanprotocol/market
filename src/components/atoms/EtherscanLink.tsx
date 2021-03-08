394cf3e90d164b3089452ffabb3b02b25ce199a4
mihaisc <mihai.scarlat@smartcontrol.ro>
2021-03-08 16:16:43 +0200

    cleanup

--- src/components/atoms/EtherscanLink.tsx
+++ src/components/atoms/EtherscanLink.tsx
@@ -1,8 +1,6 @@
 import React, { ReactElement, ReactNode, useEffect, useState } from 'react'
-import { EthereumListsChain, getNetworkData } from '../../utils/wallet'
 import { ReactComponent as External } from '../../images/external.svg'
 import styles from './EtherscanLink.module.css'
-import { useSiteMetadata } from '../../hooks/useSiteMetadata'
 import { graphql, useStaticQuery } from 'gatsby'
 import { useOcean } from '@oceanprotocol/react'
 import { ConfigHelperConfig } from '@oceanprotocol/lib/dist/node/utils/ConfigHelper'
@@ -22,7 +20,6 @@ const networksQuery = graphql`
 `
 
 export default function EtherscanLink({
-  networkId,
   path,
   children
 }: {
@@ -30,7 +27,6 @@ export default function EtherscanLink({
   path: string
   children: ReactNode
 }): ReactElement {
-  const data = useStaticQuery(networksQuery)
   const { config } = useOcean()
   const [url, setUrl] = useState<string>()
 
