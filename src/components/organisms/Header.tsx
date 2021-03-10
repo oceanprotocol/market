fa9fabf256e17eb87aba8e5261f3dbc2e779c3e9
Matthias Kretschmann <m@kretschmann.io>
2021-03-11 00:10:58 +0100

    quick ocean config switching prototype
    * verify that changes can be used to solve #432

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,11 +1,36 @@
 import React, { ReactElement } from 'react'
+import { useOcean } from '../../providers/Ocean'
+import { getOceanConfig } from '../../providers/Ocean/utils'
 import Menu from '../molecules/Menu'
 import styles from './Header.module.css'
 
 export default function Header(): ReactElement {
+  const { connect } = useOcean()
+
+  // PROTOTYPE
+  // to see if #432 can be solved with the changes
+  // https://github.com/oceanprotocol/market/issues/432
+  async function handleEthMain() {
+    const config = getOceanConfig('mainnet')
+    await connect(config)
+  }
+
+  async function handleEthRinkeby() {
+    const config = getOceanConfig('rinkeby')
+    await connect(config)
+  }
+
+  async function handleMatic() {
+    const config = getOceanConfig('polygon')
+    await connect(config)
+  }
+
   return (
     <header className={styles.header}>
       <Menu />
+      <button onClick={handleEthMain}>ETH Mainnet</button>{' '}
+      <button onClick={handleEthRinkeby}>ETH Rinkeby</button>
+      <button onClick={handleMatic}>Matic</button>
     </header>
   )
 }
