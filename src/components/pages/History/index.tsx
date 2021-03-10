8ceaec8381cbd6c81058da22a8c4c0cfaf4f76ea
Norbi <katunanorbert@gmail.com>
2021-03-10 16:06:30 +0200

    displayed content using tabs

--- src/components/pages/History/index.tsx
+++ src/components/pages/History/index.tsx
@@ -1,56 +1,39 @@
-import React, { ReactElement, ReactNode } from 'react'
+import React, { ReactElement } from 'react'
 import ComputeJobs from './ComputeJobs'
 import styles from './index.module.css'
 import PoolShares from './PoolShares'
 import PoolTransactions from '../../molecules/PoolTransactions'
 import PublishedList from './PublishedList'
 import Downloads from './Downloads'
+import Tabs from '../../atoms/Tabs'
 
-const sections = [
+const tabs = [
   {
     title: 'Published',
-    component: <PublishedList />
+    content: <PublishedList />
   },
   {
     title: 'Pool Shares',
-    component: <PoolShares />
+    content: <PoolShares />
   },
   {
     title: 'Pool Transactions',
-    component: <PoolTransactions />
+    content: <PoolTransactions />
   },
   {
     title: 'Downloads',
-    component: <Downloads />
+    content: <Downloads />
   },
   {
     title: 'Compute Jobs',
-    component: <ComputeJobs />
+    content: <ComputeJobs />
   }
 ]
-
-const Section = ({
-  title,
-  component
-}: {
-  title: string
-  component: ReactNode
-}) => {
-  return (
-    <div className={styles.section}>
-      <h2 className={styles.sectionTitle}>{title}</h2>
-      {component}
-    </div>
-  )
-}
 
 export default function HistoryPage(): ReactElement {
   return (
     <article className={styles.content}>
-      {sections.map((section) => {
-        const { title, component } = section
-        return <Section key={title} title={title} component={component} />
-      })}
+      <Tabs items={tabs} className={styles.historyTabs} />
     </article>
   )
 }
