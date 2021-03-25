701ee2fddff4a933fd6197e35ef04bc589baee37
Matthias Kretschmann <m@kretschmann.io>
2021-03-25 08:21:26 +0100

    modify publish defaults

--- src/hooks/usePublish.ts
+++ src/hooks/usePublish.ts
@@ -113,9 +113,9 @@ function usePublish(): UsePublish {
             servers
           )
           const origComputePrivacy: ServiceComputePrivacy = {
-            allowRawAlgorithm: true,
+            allowRawAlgorithm: false,
             allowNetworkAccess: false,
-            allowAllPublishedAlgorithms: true,
+            allowAllPublishedAlgorithms: false,
             publisherTrustedAlgorithms: []
           }
           const computeService = ocean.compute.createComputeService(
