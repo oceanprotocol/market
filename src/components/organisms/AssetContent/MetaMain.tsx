ab4e66d976192cd005a89a5b233a2260ee6eee2c
Matthias Kretschmann <m@kretschmann.io>
2021-03-26 15:50:29 +0100

    metadata display tweaks

--- src/components/organisms/AssetContent/MetaMain.tsx
+++ src/components/organisms/AssetContent/MetaMain.tsx
@@ -15,21 +15,6 @@ export default function MetaMain(): ReactElement {
 
   return (
     <aside className={styles.meta}>
-      <p>
-        <ExplorerLink
-          networkId={networkId}
-          path={
-            networkId === 137
-              ? `tokens/${ddo?.dataToken}`
-              : `token/${ddo?.dataToken}`
-          }
-        >
-          {`${ddo?.dataTokenInfo.name} — ${ddo?.dataTokenInfo.symbol}`}
-        </ExplorerLink>
-      </p>
-      <div>
-        Published By <Publisher account={owner} />
-      </div>
       <div className={styles.typeAndDate}>
         <AssetType
           type={type}
@@ -46,6 +31,21 @@ export default function MetaMain(): ReactElement {
           )}
         </p>
       </div>
+      <p>
+        <ExplorerLink
+          networkId={networkId}
+          path={
+            networkId === 137
+              ? `tokens/${ddo?.dataToken}`
+              : `token/${ddo?.dataToken}`
+          }
+        >
+          {`${ddo?.dataTokenInfo.name} — ${ddo?.dataTokenInfo.symbol}`}
+        </ExplorerLink>
+      </p>
+      <p>
+        Published By <Publisher account={owner} />
+      </p>
     </aside>
   )
 }
