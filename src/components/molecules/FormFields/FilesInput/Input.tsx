7d941a0a5e23f532343c4d4ce691fcec41d920fa
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-03-29 15:04:51 +0300

    adding onBlur event to validate the data input

--- src/components/molecules/FormFields/FilesInput/Input.tsx
+++ src/components/molecules/FormFields/FilesInput/Input.tsx
@@ -18,7 +18,12 @@ export default function FileInput({
 
   return (
     <InputGroup>
-      <input className={styles.input} {...props} type="url" />
+      <input
+        className={styles.input}
+        {...props}
+        type="url"
+        onBlur={(e: React.SyntheticEvent) => handleButtonClick(e, field.value)}
+      />
 
       <Button
         size="small"
