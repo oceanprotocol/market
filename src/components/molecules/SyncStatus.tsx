db3a5cbd8ea528066a3109059e75030dda5d5b11
mihaisc <mihai.scarlat@smartcontrol.ro>
2021-04-01 18:14:34 +0300

    quick fix
    Signed-off-by: mihaisc <mihai.scarlat@smartcontrol.ro>

--- src/components/molecules/SyncStatus.tsx
+++ src/components/molecules/SyncStatus.tsx
@@ -2,35 +2,46 @@ import React, { ReactElement, useEffect, useState } from 'react'
 import styles from './SyncStatus.module.css'
 import Status from '../atoms/Status'
 import { useWeb3 } from '../../providers/Web3'
-import { gql, useQuery } from '@apollo/client'
-
-const blockInfo = gql`
-  query Meta {
-    _meta {
-      block {
-        hash
-        number
-      }
-      deployment
-      hasIndexingErrors
-    }
-  }
-`
+import useSWR from 'swr'
+import { useOcean } from '../../providers/Ocean'
+import fetch from 'cross-fetch'
+const refreshInterval = 120000
 
 export default function SyncStatus(): ReactElement {
   const { web3 } = useWeb3()
+  const { config } = useOcean()
   const [state, setState] = useState<string>('success')
   const [blockNumber, setBlockNumber] = useState<number>()
-  const { data, error } = useQuery(blockInfo)
+
   useEffect(() => {
     web3 && web3.eth.getBlockNumber().then((resp) => setBlockNumber(resp))
   }, [web3])
-  useEffect(() => {
+
+  async function fetchData(url: string): Promise<Response> {
+    try {
+      const queryBody =
+        '{"query":"  query Meta {   _meta {      block {        hash       number     }      deployment      hasIndexingErrors    }  }","variables":{},"operationName":"Meta"}'
+      const response = await fetch(
+        `${url}/subgraphs/name/oceanprotocol/ocean-subgraph`,
+        {
+          method: 'POST',
+          body: queryBody
+        }
+      )
+
+      return response.json()
+    } catch (error) {
+      console.error('Error parsing json: ' + error.message)
+    }
+  }
+  const onSuccess = async (data: any) => {
     console.log(data)
-    console.log(error)
-    if (!data) return
-    console.log(data)
-  }, [data, error])
+  }
+
+  useSWR(config.subgraphUri, fetchData, {
+    refreshInterval,
+    onSuccess
+  })
   return (
     <div className={styles.sync}>
       <span className={styles.text}>{blockNumber}</span>
