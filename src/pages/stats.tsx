e72858f78cd6f1413542bc1c09317b270fbb5c75
Dimo Dzhurenov <dimitar@infinitex.tech>
2021-04-01 18:12:53 +0300

    getting page data

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -1,7 +1,22 @@
 import React, { ReactElement } from 'react'
 import PageStats from '../components/pages/Stats'
 import Page from '../components/templates/Page'
-import { PageProps } from 'gatsby'
+import { PageProps, useStaticQuery, graphql } from 'gatsby'
+
+export const contentQuery = graphql`
+  query StatsPageQuery {
+    content: allFile(filter: { relativePath: { eq: "pages/stats.json" } }) {
+      edges {
+        node {
+          childPagesJson {
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
 
 export default function PageGatsbyStats(props: PageProps): ReactElement {
   const content = (props.data as any).content.edges[0].node.childPagesJson
