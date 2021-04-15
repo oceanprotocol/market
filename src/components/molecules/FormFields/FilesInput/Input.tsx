2826df82874c283b1d129d12707248899bd699c5
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-04-15 11:18:48 +0300

    Allowing urls without proceeding http or https (#493)
    * allowing urls without proceeding http or https
    
    * Installing package: valid-url-utf8
    
    * removing pre-submit URL validation
    
    * removing new package
    
    * updating package-lock.json

--- src/components/molecules/FormFields/FilesInput/Input.tsx
+++ src/components/molecules/FormFields/FilesInput/Input.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from 'react'
-import isUrl from 'is-url-superb'
 import Button from '../../../atoms/Button'
 import { FieldInputProps, useField } from 'formik'
 import Loader from '../../../atoms/Loader'
@@ -29,11 +28,7 @@ export default function FileInput({
         style="primary"
         size="small"
         onClick={(e: React.SyntheticEvent) => handleButtonClick(e, field.value)}
-        disabled={
-          !field.value ||
-          // weird static page build fix so is-url-superb won't error
-          !isUrl(typeof field.value === 'string' ? field.value : '')
-        }
+        disabled={!field.value}
       >
         {isLoading ? <Loader /> : 'Add File'}
       </Button>
