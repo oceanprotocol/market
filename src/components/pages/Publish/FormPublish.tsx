2842e2d76d8a586755a399dfb5f48eaa3c98a20a
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2021-04-22 11:22:57 +0300

    fix for terms state

--- src/components/pages/Publish/FormPublish.tsx
+++ src/components/pages/Publish/FormPublish.tsx
@@ -39,8 +39,11 @@ export default function FormPublish({
     e: ChangeEvent<HTMLInputElement>,
     field: FormFieldProps
   ) {
+    const value =
+      field.type === 'terms' ? !JSON.parse(e.target.value) : e.target.value
+
     validateField(field.name)
-    setFieldValue(field.name, e.target.value)
+    setFieldValue(field.name, value)
   }
 
   const resetFormAndClearStorage = (e: FormEvent<Element>) => {
