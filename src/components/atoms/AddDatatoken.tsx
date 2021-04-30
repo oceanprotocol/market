aa3d7dcca499fc52dfc627371bd19a8e006644ac
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-04-30 14:02:59 +0300

    removing empty div element

--- src/components/atoms/AddDatatoken.tsx
+++ src/components/atoms/AddDatatoken.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, useEffect, useState } from 'react'
 import { DDO } from '@oceanprotocol/lib'
-import Button from '../atoms/Button'
+import Button from './Button'
 import { addDatatokenToWallet } from '../../utils/web3'
 import { useWeb3 } from '../../providers/Web3'
 import { getProviderInfo, IProviderInfo } from 'web3modal'
@@ -15,20 +15,18 @@ export default function AddToken({ ddo }: { ddo: DDO }): ReactElement {
     setProviderInfo(providerInfo)
   }, [web3Provider])
 
-  return (
-    <div>
-      {providerInfo?.name === 'MetaMask' && (
-        <Button
-          style="text"
-          size="small"
-          onClick={() => {
-            console.log('Add datatoken to wallet 1')
-            addDatatokenToWallet(ddo, web3Provider)
-          }}
-        >
-          {`Add ${ddo.dataTokenInfo.symbol} to wallet`}
-        </Button>
-      )}
-    </div>
-  )
+  if (providerInfo?.name === 'MetaMask') {
+    return (
+      <Button
+        style="text"
+        size="small"
+        onClick={() => {
+          console.log('Add datatoken to wallet 1')
+          addDatatokenToWallet(ddo, web3Provider)
+        }}
+      >
+        {`Add ${ddo.dataTokenInfo.symbol} to wallet`}
+      </Button>
+    )
+  }
 }
