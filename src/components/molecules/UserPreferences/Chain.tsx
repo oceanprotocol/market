f0ed9f68cb5954be17296373ecdb22e268f1c287
Norbi <37236152+KatunaNorbert@users.noreply.github.com>
2021-05-17 16:32:01 +0300

    Add new custom form field for single value selection (#504)
    * created BoxSelection component
    
    * wip
    
    * design changes
    
    * integrate with form
    
    * refactoring
    
    * used inside appearance component
    
    * WIP, added space between options
    
    * adde new fields to BoxSelection used for chain selection
    
    * fixed errors
    
    * removed access type option from publish.json
    
    * updated component for chain selection
    
    * updated for compute type on publish dataset
    
    * added component to dockerImageOpions
    
    * removed Dotdotdot component from options
    
    * remove space
    
    * styling updates, fix React warning for terms checkbox
    
    Co-authored-by: Norbi <katunanorbert@gmai.com>
    Co-authored-by: Matthias Kretschmann <m@kretschmann.io>

--- src/components/molecules/UserPreferences/Chain.tsx
+++ src/components/molecules/UserPreferences/Chain.tsx
@@ -1,53 +1,58 @@
 import { ConfigHelperConfig } from '@oceanprotocol/lib'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, ChangeEvent } from 'react'
 import { useOcean } from '../../../providers/Ocean'
 import { useWeb3 } from '../../../providers/Web3'
 import { getOceanConfig } from '../../../utils/ocean'
-import Button from '../../atoms/Button'
 import FormHelp from '../../atoms/Input/Help'
 import Label from '../../atoms/Input/Label'
+import BoxSelection, { BoxSelectionOption } from '../FormFields/BoxSelection'
 import styles from './Chain.module.css'
 
 export default function Chain(): ReactElement {
   const { web3 } = useWeb3()
   const { config, connect } = useOcean()
 
-  async function connectOcean(networkName: string) {
-    const config = getOceanConfig(networkName)
+  async function connectOcean(event: ChangeEvent<HTMLInputElement>) {
+    const config = getOceanConfig(event.target.value)
     await connect(config)
   }
 
-  const chains = [
-    { name: 'ETH', oceanConfig: 'mainnet', label: 'Mainnet' },
-    { name: 'Polygon/Matic', oceanConfig: 'polygon', label: 'Mainnet' },
-    { name: 'Moonbase Alpha', oceanConfig: 'moonbeamalpha', label: 'Testnet' }
+  function isNetworkSelected(oceanConfig: string) {
+    return (config as ConfigHelperConfig).network === oceanConfig
+  }
+
+  const options: BoxSelectionOption[] = [
+    {
+      name: 'mainnet',
+      checked: isNetworkSelected('mainnet'),
+      title: 'ETH',
+      text: 'Mainnet'
+    },
+    {
+      name: 'polygon',
+      checked: isNetworkSelected('polygon'),
+      title: 'Polygon/Matic',
+      text: 'Mainnet'
+    },
+    {
+      name: 'moonbeamalpha',
+      checked: isNetworkSelected('moonbeamalpha'),
+      title: 'Moonbase Alpha',
+      text: 'Testnet'
+    }
   ]
 
   // TODO: to fully solve https://github.com/oceanprotocol/market/issues/432
   // there are more considerations for users with a wallet connected (wallet network vs. setting network).
   // For now, only show the setting for non-wallet users.
   return !web3 ? (
-    <li>
+    <li className={styles.chains}>
       <Label htmlFor="">Chain</Label>
-      <div className={styles.buttons}>
-        {chains.map((button) => {
-          const selected =
-            (config as ConfigHelperConfig).network === button.oceanConfig
-
-          return (
-            <Button
-              key={button.name}
-              className={`${styles.button} ${selected ? styles.selected : ''}`}
-              size="small"
-              style="text"
-              onClick={() => connectOcean(button.oceanConfig)}
-            >
-              {button.name}
-              <span>{button.label}</span>
-            </Button>
-          )
-        })}
-      </div>
+      <BoxSelection
+        options={options}
+        name="chain"
+        handleChange={connectOcean}
+      />
       <FormHelp>Switch the data source for the interface.</FormHelp>
     </li>
   ) : null
