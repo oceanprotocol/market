75d391bba7bf81c4db5be9a0f8b87b04134c18f2
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-05-17 13:26:04 +0300

    testing rbac request on homepage

--- src/utils/rbac.ts
+++ src/utils/rbac.ts
@@ -12,14 +12,19 @@ export async function rbacRequest(
       token
     }
   }
+  console.log('data', data)
+  console.log('process.env.RBAC_URL', process.env.RBAC_URL)
   try {
-    const response = await fetch(process.env.RBAC_URL, {
+    console.log('Start fetch')
+    const response = await fetch('http://localhost:3000', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
     })
+    console.log('Start fetch')
+    console.log('response', response)
     return await response.json()
   } catch (error) {
     console.error('Error parsing json: ' + error.message)
