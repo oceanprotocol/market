6bc37b9b7bf65ea95c18588046dcc0e07d2579fe
Matthias Kretschmann <m@kretschmann.io>
2021-05-27 14:01:20 +0200

    remove NetworkBanner

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,10 +7,9 @@ import Styles from '../global/Styles'
 import { useWeb3 } from '../providers/Web3'
 import { useSiteMetadata } from '../hooks/useSiteMetadata'
 import { useAccountPurgatory } from '../hooks/useAccountPurgatory'
-import NetworkBanner from './molecules/NetworkBanner'
-import styles from './App.module.css'
 import AnnouncementBanner from './atoms/AnnouncementBanner'
 import { useGraphSyncStatus } from '../hooks/useGraphSyncStatus'
+import styles from './App.module.css'
 
 const contentQuery = graphql`
   query AppQuery {
@@ -52,8 +51,6 @@ export default function App({
             state="error"
           />
         )}
-        {!location.pathname.includes('/asset/did') && <NetworkBanner />}
-
         <Header />
 
         {(props as PageProps).uri === '/' && (
