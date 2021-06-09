9943dae93ce1ad2c2df42962e11fbc34eb150253
Matthias Kretschmann <m@kretschmann.io>
2021-06-09 19:40:43 +0200

    full width menu bar

--- src/components/molecules/Menu.tsx
+++ src/components/molecules/Menu.tsx
@@ -4,7 +4,6 @@ import { useLocation } from '@reach/router'
 import loadable from '@loadable/component'
 import styles from './Menu.module.css'
 import { useSiteMetadata } from '../../hooks/useSiteMetadata'
-import Container from '../atoms/Container'
 import UserPreferences from './UserPreferences'
 import Badge from '../atoms/Badge'
 import Logo from '../atoms/Logo'
@@ -36,28 +35,26 @@ export default function Menu(): ReactElement {
 
   return (
     <nav className={styles.menu}>
-      <Container>
-        <Link to="/" className={styles.logoUnit}>
-          <Logo />
-          <h1 className={styles.title}>
-            {siteTitle} <Badge label="beta" />
-          </h1>
-        </Link>
+      <Link to="/" className={styles.logoUnit}>
+        <Logo />
+        <h1 className={styles.title}>
+          {siteTitle} <Badge label="beta" />
+        </h1>
+      </Link>
 
-        <ul className={styles.navigation}>
-          {menu.map((item: MenuItem) => (
-            <li key={item.name}>
-              <MenuLink item={item} />
-            </li>
-          ))}
-          <li>
-            <Wallet />
+      <ul className={styles.navigation}>
+        {menu.map((item: MenuItem) => (
+          <li key={item.name}>
+            <MenuLink item={item} />
           </li>
-          <li>
-            <UserPreferences />
-          </li>
-        </ul>
-      </Container>
+        ))}
+        <li>
+          <Wallet />
+        </li>
+        <li>
+          <UserPreferences />
+        </li>
+      </ul>
     </nav>
   )
 }
