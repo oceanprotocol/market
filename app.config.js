4a57d4a551bb6d65b58485d5dc7b502c33f1a90b
KRIS LIEW <39853992+krisliew@users.noreply.github.com>
2021-06-16 09:12:48 +0800

    `allowFreePricing` default to false

--- app.config.js
+++ app.config.js
@@ -45,7 +45,7 @@ module.exports = {
   // tab to publishers during the price creation.
   allowFixedPricing: process.env.GATSBY_ALLOW_FIXED_PRICING || 'true',
   allowDynamicPricing: process.env.GATSBY_ALLOW_DYNAMIC_PRICING || 'true',
-  allowFreePricing: process.env.GATSBY_ALLOW_FREE_PRICING || 'true',
+  allowFreePricing: process.env.GATSBY_ALLOW_FREE_PRICING || 'false',
 
   // Used to show or hide advanced settings button in asset details page
   allowAdvancedSettings: process.env.GATSBY_ALLOW_ADVANCED_SETTINGS || 'false',
