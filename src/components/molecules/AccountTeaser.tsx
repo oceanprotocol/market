bd48f74655e2ac714ab4363fc7469799fdd55484
claudiaHash <clawww1996@gmail.com>
2021-06-23 16:23:20 +0300

    fetched profile data, added style

--- src/components/molecules/AccountTeaser.tsx
+++ src/components/molecules/AccountTeaser.tsx
@@ -1,30 +1,61 @@
 import axios from 'axios'
+import { toDataUrl } from 'ethereum-blockies'
+import { Link } from 'gatsby'
 import React, { useEffect, useState } from 'react'
+import Dotdotdot from 'react-dotdotdot'
 import { Profile } from '../../models/Profile'
+import { useWeb3 } from '../../providers/Web3'
 import get3BoxProfile from '../../utils/profile'
+import styles from './AccountTeaser.module.css'
 
 declare type AccountTeaserProps = {
   account: string
 }
 
+const Blockies = ({ account }: { account: string | undefined }) => {
+  if (!account) return null
+  const blockies = toDataUrl(account)
+
+  return (
+    <img
+      className={styles.blockies}
+      src={blockies}
+      alt="Blockies"
+      aria-hidden="true"
+    />
+  )
+}
+
 const AccountTeaser: React.FC<AccountTeaserProps> = ({ account }) => {
+  const { accountId, networkId } = useWeb3()
   const [profile, setProfile] = useState<Profile>()
 
   useEffect(() => {
-    if (!account) return
+    if (!accountId) return
     const source = axios.CancelToken.source()
     async function get3Box() {
-      const profile = await get3BoxProfile(account, source.token)
+      const profile = await get3BoxProfile(accountId, source.token)
       if (!profile) return
       setProfile(profile)
-      console.log('PROFILE: ', profile)
     }
     get3Box()
-  }, [account])
+  }, [accountId, networkId])
 
   return (
-    <article>
-      <div>Profile</div>
+    <article className={styles.teaser}>
+      <Link to={`/account/${accountId}`} className={styles.link}>
+        <header className={styles.header}>
+          <Dotdotdot clamp={3}>{profile && <h3> {profile.name}</h3>}</Dotdotdot>
+          <div className={styles.details}>
+            <Blockies account={accountId} />
+            <p>{accountId}</p>
+          </div>
+        </header>
+        {profile && <div className={styles.content}>{profile.description}</div>}
+        <footer className={styles.footer}>
+          {/* <div>add more account details</div> */}
+        </footer>
+      </Link>
     </article>
   )
 }
