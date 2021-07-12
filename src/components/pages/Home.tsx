3bca0e0b1c7beeba136cf57e4379f5c59bd3000b
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2021-07-12 11:45:23 +0300

    fixed highest liquidity number of assets

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -28,7 +28,7 @@ async function getQueryHighest(
   // TODO: this query needs to adapt to chainIds
   const queryHighest = {
     page: 1,
-    offset: 15,
+    offset: 9,
     query: {
       query_string: {
         query: `(${dids}) AND (${transformChainIdsListToQuery(
