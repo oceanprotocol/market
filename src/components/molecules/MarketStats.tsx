675ef446178c93c2549668acf8b29982280743bb
Norbert <katunanorbert@gmail.com>
2021-07-19 14:10:56 +0300

    display multiple chains statistics

--- src/components/molecules/MarketStats.tsx
+++ src/components/molecules/MarketStats.tsx
@@ -4,6 +4,8 @@ import { gql, useQuery } from 'urql'
 import Conversion from '../atoms/Price/Conversion'
 import PriceUnit from '../atoms/Price/PriceUnit'
 import Tooltip from '../atoms/Tooltip'
+import { useUserPreferences } from '../../providers/UserPreferences'
+import NetworkName from '../atoms/NetworkName'
 
 const getTotalPoolsValues = gql`
   query PoolsData {
@@ -19,6 +21,7 @@ export default function MarketStats(): ReactElement {
   const [totalValueLocked, setTotalValueLocked] = useState<string>()
   const [totalOceanLiquidity, setTotalOceanLiquidity] = useState<string>()
   const [poolCount, setPoolCount] = useState<number>()
+  const { chainIds } = useUserPreferences()
   const [result] = useQuery({
     query: getTotalPoolsValues
     // pollInterval: 20000
@@ -33,18 +36,29 @@ export default function MarketStats(): ReactElement {
   }, [data])
 
   return (
-    <div className={styles.stats}>
-      <Conversion price={`${totalValueLocked}`} hideApproximateSymbol />{' '}
-      <abbr title="Total Value Locked">TVL</abbr> across{' '}
-      <strong>{poolCount}</strong> data set pools that contain{' '}
-      <PriceUnit price={totalOceanLiquidity} small className={styles.total} />,
-      plus datatokens for each pool.
-      <Tooltip
-        className={styles.info}
-        content="Counted on-chain from our pool factory. Does not filter out data sets in "
-        reference="list-purgatory"
-        link="https://github.com/oceanprotocol/list-purgatory"
-      />
-    </div>
+    <>
+      {chainIds.map((chainId, key) => (
+        <div key={key}>
+          <NetworkName networkId={chainId} />
+          <div className={styles.stats}>
+            <Conversion price={`${totalValueLocked}`} hideApproximateSymbol />{' '}
+            <abbr title="Total Value Locked">TVL</abbr> across{' '}
+            <strong>{poolCount}</strong> data set pools that contain{' '}
+            <PriceUnit
+              price={totalOceanLiquidity}
+              small
+              className={styles.total}
+            />
+            , plus datatokens for each pool.
+            <Tooltip
+              className={styles.info}
+              content="Counted on-chain from our pool factory. Does not filter out data sets in "
+              reference="list-purgatory"
+              link="https://github.com/oceanprotocol/list-purgatory"
+            />
+          </div>
+        </div>
+      ))}
+    </>
   )
 }
