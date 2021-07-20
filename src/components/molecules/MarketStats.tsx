0228ad6521a77f4856aefe3d1f49077e8bb16b68
Norbert <katunanorbert@gmail.com>
2021-07-20 16:03:05 +0300

    display one combined stat for all main networks

--- src/components/molecules/MarketStats.tsx
+++ src/components/molecules/MarketStats.tsx
@@ -4,9 +4,11 @@ import { gql, OperationContext, useQuery } from 'urql'
 import Conversion from '../atoms/Price/Conversion'
 import PriceUnit from '../atoms/Price/PriceUnit'
 import Tooltip from '../atoms/Tooltip'
-import { useUserPreferences } from '../../providers/UserPreferences'
 import NetworkName from '../atoms/NetworkName'
 import { fetchData, getSubgrahUri } from '../../utils/subgraph'
+import { filterNetworksByType } from './UserPreferences/Networks/index'
+import { useSiteMetadata } from '../../hooks/useSiteMetadata'
+import useNetworkMetadata from '../../hooks/useNetworkMetadata'
 
 const getTotalPoolsValues = gql`
   query PoolsData {
@@ -24,11 +26,26 @@ export default function MarketStats(): ReactElement {
   const [totalOceanLiquidity, setTotalOceanLiquidity] =
     useState<{ [chainId: number]: string }>()
   const [poolCount, setPoolCount] = useState<{ [chainId: number]: string }>()
-  const { chainIds } = useUserPreferences()
+  const [sumTotalValueLocked, setSumTotalValueLocked] = useState<string>()
+  const [sumTotalOceanLiquidity, setSumTotalOceanLiquidity] = useState<string>()
+  const [sumPoolCount, setSumPoolCount] = useState<string>()
+  const [mainChainIds, setMainChainIds] = useState<number[]>()
   const [loading, setLoading] = useState(true)
+  const { appConfig } = useSiteMetadata()
+  const { networksList } = useNetworkMetadata()
 
-  async function getMarketStatsForChainIds(chainIds: number[]) {
-    for (const chainId of chainIds) {
+  async function getMarketStats() {
+    const mainChainIdsList = await filterNetworksByType(
+      'mainnet',
+      appConfig.chainIdsSupported,
+      networksList
+    )
+    setMainChainIds(mainChainIdsList)
+    let newSumTotalValueLocked = 0
+    let newSumTotalOceanLiquidity = 0
+    let newSumPoolCount = 0
+    for (const chainId of mainChainIdsList) {
+      console.log(chainId)
       const context: OperationContext = {
         url: `${getSubgrahUri(
           chainId
@@ -50,48 +67,73 @@ export default function MarketStats(): ReactElement {
           ...prevState,
           [chainId]: response.data?.poolFactories[0].finalizedPoolCount
         }))
+        newSumTotalValueLocked += parseInt(
+          response.data?.poolFactories[0].totalValueLocked
+        )
+        newSumTotalOceanLiquidity += parseInt(
+          response.data?.poolFactories[0].totalOceanLiquidity
+        )
+        newSumPoolCount += parseInt(
+          response.data?.poolFactories[0].finalizedPoolCount
+        )
       } catch (error) {
         console.error('Error fetchData: ', error.message)
       }
     }
+    setSumTotalValueLocked(newSumTotalValueLocked.toString())
+    setSumTotalOceanLiquidity(newSumTotalOceanLiquidity.toString())
+    setSumPoolCount(newSumPoolCount.toString())
     setLoading(false)
   }
 
   useEffect(() => {
     setLoading(true)
-    if (!chainIds) return
-    getMarketStatsForChainIds(chainIds)
-  }, [chainIds])
+    getMarketStats()
+  }, [])
+
+  const tooltipContent = !loading && (
+    <ul className={styles.statsList}>
+      {mainChainIds.map((chainId, key) => (
+        <li key={key}>
+          <NetworkName networkId={chainId} />
+          <div className={styles.tooltipStats}>
+            <Conversion
+              price={`${totalValueLocked[chainId]}`}
+              hideApproximateSymbol
+            />{' '}
+            <abbr title="Total Value Locked">TVL</abbr> across{' '}
+            <strong>{poolCount[chainId]}</strong> data set pools that contain{' '}
+            <PriceUnit
+              price={totalOceanLiquidity[chainId]}
+              small
+              className={styles.total}
+            />
+            , plus datatokens for each pool.
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
 
   return (
     !loading && (
-      <ul>
-        {chainIds.map((chainId, key) => (
-          <li key={key}>
-            <NetworkName networkId={chainId} />
-            <div className={styles.stats}>
-              <Conversion
-                price={`${totalValueLocked[chainId]}`}
-                hideApproximateSymbol
-              />{' '}
-              <abbr title="Total Value Locked">TVL</abbr> across{' '}
-              <strong>{poolCount[chainId]}</strong> data set pools that contain{' '}
-              <PriceUnit
-                price={totalOceanLiquidity[chainId]}
-                small
-                className={styles.total}
-              />
-              , plus datatokens for each pool.
-              <Tooltip
-                className={styles.info}
-                content="Counted on-chain from our pool factory. Does not filter out data sets in "
-                reference="list-purgatory"
-                link="https://github.com/oceanprotocol/list-purgatory"
-              />
-            </div>
-          </li>
-        ))}
-      </ul>
+      <div className={styles.stats}>
+        <Conversion price={`${sumTotalValueLocked}`} hideApproximateSymbol />{' '}
+        <abbr title="Total Value Locked">TVL</abbr> across{' '}
+        <strong>{sumPoolCount}</strong> data set pools that contain{' '}
+        <PriceUnit
+          price={sumTotalOceanLiquidity}
+          small
+          className={styles.total}
+        />
+        , plus datatokens for each pool.
+        <Tooltip
+          className={styles.info}
+          content={tooltipContent}
+          reference="list-purgatory"
+          link="https://github.com/oceanprotocol/list-purgatory"
+        />
+      </div>
     )
   )
 }
