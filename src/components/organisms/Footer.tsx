891548f0bfe0cdb2ededd9513d4d0187d5511f7d
Moritz Kirstein <mkirstein@protonmail.com>
2021-08-05 16:44:05 +0200

    add cookie button to footer component

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -5,10 +5,15 @@ import { useSiteMetadata } from '../../hooks/useSiteMetadata'
 import { Link } from 'gatsby'
 import MarketStats from '../molecules/MarketStats'
 import BuildId from '../atoms/BuildId'
-import SyncStatus from '../molecules/SyncStatus'
+import Button from '../atoms/Button'
+import { useUserPreferences } from '../../providers/UserPreferences'
+import { useGdprMetadata } from '../../hooks/useGdprMetadata'
 
 export default function Footer(): ReactElement {
-  const { copyright } = useSiteMetadata()
+  const { copyright, appConfig } = useSiteMetadata()
+  const { setShowPPC } = useUserPreferences()
+  const cookies = useGdprMetadata()
+
   const year = new Date().getFullYear()
 
   return (
@@ -22,6 +27,21 @@ export default function Footer(): ReactElement {
           <Link to="/terms">Terms</Link>
           {' — '}
           <a href="https://oceanprotocol.com/privacy">Privacy</a>
+          {appConfig.privacyPreferenceCenter && (
+            <>
+              {' — '}
+              <Button
+                style="text"
+                size="small"
+                className="link"
+                onClick={() => {
+                  setShowPPC(true)
+                }}
+              >
+                {cookies.optionalCookies ? 'Cookie Settings' : 'Cookies'}
+              </Button>
+            </>
+          )}
         </div>
       </div>
     </footer>
