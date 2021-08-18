4245a06333d9f9d83c6c4d715ee94d6ce8769bfa
Moritz Kirstein <mkirstein@protonmail.com>
2021-08-18 17:47:41 +0200

    unit tests forprivacy policy components

--- tests/unit/components/PrivacyPolicy.test.tsx
+++ tests/unit/components/PrivacyPolicy.test.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import PrivacyPolicy from '../../../src/components/molecules/PrivacyPolicy'
+import PrivacyHeader from '../../../src/components/molecules/PrivacyHeader'
+import PrivacyLanguages from '../../../src/components/atoms/PrivacyLanguages'
 
 const privacyPolicy = {
   tableOfContents: 'toc',
-  html: 'html',
-  date: '2021-08-01',
-  params: {
-    languageLabel: 'Language',
-    tocHeader: 'Header',
-    updated: 'Updated',
-    dateFormat: 'dd-MM-yyy'
-  }
+  policy: 'en'
 }
 
-describe('PrivacyPolicy', () => {
+describe('PrivacyLanguages', () => {
   it('renders correctly without crashing', () => {
-    const { container } = render(<PrivacyPolicy {...privacyPolicy} />)
+    const label = 'TEST'
+    const { container } = render(<PrivacyLanguages label={label} />)
+
+    expect(container.firstChild).toBeInTheDocument()
+    expect(container).toHaveTextContent(label)
+  })
+})
+
+describe('PrivacyHeader', () => {
+  it('renders correctly without crashing', () => {
+    const { container } = render(<PrivacyHeader {...privacyPolicy} />)
 
     expect(container.firstChild).toBeInTheDocument()
   })
