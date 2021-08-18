a0eaed7867d2984ac2498d35711e8cfcc5dd0ea9
Moritz Kirstein <mkirstein@protonmail.com>
2021-08-18 17:47:12 +0200

    setup useUserPreferences mock for unit tests

--- tests/unit/setupTests.ts
+++ tests/unit/setupTests.ts
@@ -2,10 +2,13 @@ import '@testing-library/jest-dom/extend-expect'
 import * as Gatsby from 'gatsby'
 import siteMetadata from './__fixtures__/siteMetadata.json'
 import gdprMetadata from './__fixtures__/gdprMetadata.json'
+import UserPreferences from './__fixtures__/useUserPreferences'
+import * as UseUserPreferences from '../../src/providers/UserPreferences'
 
 import('./__mocks__/matchMedia')
 
 const useStaticQuery = jest.spyOn(Gatsby, 'useStaticQuery')
+const useUserPreferences = jest.spyOn(UseUserPreferences, 'useUserPreferences')
 // const useWeb3 = jest.spyOn(oceanReact, 'useWeb3')
 // const useOcean = jest.spyOn(oceanReact, 'useOcean')
 
@@ -17,7 +20,7 @@ export const globalMock = {
 beforeAll(() => {
   jest.mock('web3')
   jest.mock('@oceanprotocol/lib')
-
   // useOcean.mockImplementation(() => mockReact.useOcean())
   useStaticQuery.mockImplementation(() => globalMock)
+  useUserPreferences.mockImplementation(() => UserPreferences)
 })
