1438a7d74dedd570e9d0ec6b9490b3584ac53285
Moritz Kirstein <mkirstein@protonmail.com>
2021-08-24 19:39:58 +0200

    remove whitelisting

--- address.config.js
+++ address.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   whitelists: {
-    'publicKey.owner': [
-      '0xd1cf7ea13B08A745fA8aa52B29dBD574a18600e1',
-      '0xD10615873eA1fDe7A79c8441c5C1f1cC8d37ef92',
-      '0xDF1c35c3d5178e9d98043b35a6737Bd861c191c9',
-      '0x01e66950353400E93AEe7F041C0303103E2ef5Ab',
-      '0x68C24FA5b2319C81b34f248d1f928601D2E5246B',
-      '0x5868f08084eFa4c7b075D6AC238229fF7fDa8669'
-    ],
-    dataToken: ['0xDd64fD4Ff847A2FBEC2596E7A58fbB439654acB5']
+    'publicKey.owner': [],
+    dataToken: []
   }
 }
