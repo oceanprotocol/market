447f4f62030ad4664831e53eb6b88a5442f6c464
Moritz Kirstein <mkirstein@protonmail.com>
2021-08-24 17:43:35 +0200

    change logo

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from 'react'
 import { ReactComponent as LogoAssetFull } from '@oceanprotocol/art/logo/logo.svg'
-import { ReactComponent as LogoAssetBranding } from '../../images/logo.svg'
+import { ReactComponent as LogoAssetBranding } from '../../images/gaia-x-logo.svg'
 import { ReactComponent as LogoAsset } from '../../images/ocean-logo.svg'
-
 import styles from './Logo.module.css'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 
 export default function Logo({
   noWordmark,
@@ -12,11 +14,16 @@ export default function Logo({
   noWordmark?: boolean
   branding?: boolean
 }): ReactElement {
+  const styleClasses = cx({
+    logo: true,
+    branding: branding
+  })
+
   return branding ? (
-    <LogoAssetBranding className={styles.logo} />
+    <LogoAssetBranding className={styleClasses} />
   ) : noWordmark ? (
-    <LogoAsset className={styles.logo} />
+    <LogoAsset className={styleClasses} />
   ) : (
-    <LogoAssetFull className={styles.logo} />
+    <LogoAssetFull className={styleClasses} />
   )
 }
