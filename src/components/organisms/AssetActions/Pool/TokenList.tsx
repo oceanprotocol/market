bccb56a18188d4b966107d7811abe633b20941ce
Dimo Dzhurenov <dimitar@infinitex.tech>
2021-08-24 11:11:01 +0300

    Update Number to Decimal (#653)
    * update number to decimal
    
    * Update FormTrade.tsx
    
    * update Pool/index.tsx
    
    * update Pool/utils.ts
    
    * update FormAdd.tsx
    
    * update bugs from switching to decimal
    
    * fixed errors in Output.tsx
    
    * rebase main
    
    * Update FormTrade.tsx
    
    * update Pool/index.tsx
    
    * update Pool/utils.ts
    
    * update FormAdd.tsx
    
    * update bugs from switching to decimal
    
    * fixed errors in Output.tsx
    
    * remove duplication
    
    * added missed conversion
    
    * fix style error
    
    * fixes
    
    * added function return type
    
    * add correct return type for function
    
    Co-authored-by: Matthias Kretschmann <m@kretschmann.io>

--- src/components/organisms/AssetActions/Pool/TokenList.tsx
+++ src/components/organisms/AssetActions/Pool/TokenList.tsx
@@ -2,6 +2,7 @@ import Conversion from '../../../atoms/Price/Conversion'
 import React, { ReactElement, ReactNode } from 'react'
 import Token from './Token'
 import styles from './TokenList.module.css'
+import Decimal from 'decimal.js'
 
 export default function TokenList({
   title,
@@ -20,7 +21,7 @@ export default function TokenList({
   dt: string
   dtSymbol: string
   poolShares: string
-  conversion: number
+  conversion: Decimal
   highlight?: boolean
   showTVLLabel?: boolean
 }): ReactElement {
@@ -31,9 +32,9 @@ export default function TokenList({
         <div>
           <Token symbol="OCEAN" balance={ocean} />
           <Token symbol={dtSymbol} balance={dt} />
-          {conversion > 0 && (
+          {conversion.greaterThan(0) && (
             <Conversion
-              price={`${conversion}`}
+              price={conversion.toString()}
               className={styles.totalLiquidity}
               showTVLLabel={showTVLLabel}
             />
