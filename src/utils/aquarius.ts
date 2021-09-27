521050761cc4245561be95cdd58ef47de02553f1
mihaisc <mihai@oceanprotocol.com>
2021-09-27 14:40:30 +0300

    Update src/utils/aquarius.ts
    Co-authored-by: Bogdan Fazakas <bogdan.fazakas@gmail.com>

--- src/utils/aquarius.ts
+++ src/utils/aquarius.ts
@@ -63,7 +63,7 @@ export function transformQueryResult(
     (hit: any) => new DDO(hit._source as DDO)
   )
   result.totalResults = queryResult.hits.total
-  result.totalPages = Math.floor(result.totalResults / size)
+  result.totalPages = (result.totalResults / size < 1) ? Math.floor(result.totalResults / size) : Math.ceil(result.totalResults / size)
   result.page = from ? from / size + 1 : 1
 
   return result
