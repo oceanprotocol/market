095eeb670ace147a20c1182703aa6b69eab941f9
Moritz Kirstein <mkirstein@protonmail.com>
2021-10-11 17:44:11 +0200

    changed default styling of PPC to small

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -61,7 +61,7 @@ export default function App({
         <Footer />
 
         {appConfig.privacyPreferenceCenter === 'true' && (
-          <PrivacyPreferenceCenter style="default" />
+          <PrivacyPreferenceCenter style="small" />
         )}
       </div>
     </Styles>
