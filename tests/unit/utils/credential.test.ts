7d1e04e020a55f99d189620f823ac0d5e0cf2bf9
KIANLAU <kian_yee.lau@daimler.com>
2021-10-15 13:32:50 +0800

    #904 add unit test to increse coverage

--- tests/unit/utils/credential.test.ts
+++ tests/unit/utils/credential.test.ts
@@ -1,36 +1,77 @@
-import { getCredentialList } from '../../../src/utils/credential'
+import {
+  getCredentialList,
+  updateCredential
+} from '../../../src/utils/credential'
 import { Credential } from '@oceanprotocol/lib'
 
 const addressType = 'address'
 const domainType = 'domain'
 const address1 = '0x0123456789'
 const address2 = '0x1234567890'
-const address3 = '0x2345678901'
-const address4 = '0x3456789012'
-// const domain1 = 'apple.com'
-// const domain2 = 'orange.com'
-const appleGroup = [address1, address2]
-const orangeGroup = [address3, address4]
+const domain1 = 'apple.com'
+const domain2 = 'orange.com'
+const addressList = [address1, address2]
+const domainList = [domain1, domain2]
 
-const allowCredentials: Credential[] = [
+const addressTypeCredential: Credential[] = [
   {
     type: addressType,
-    values: appleGroup
+    values: addressList
   }
 ]
 
-const denyCredentials: Credential[] = [
+const addressTypeAndDomainTYpeCredential: Credential[] = [
   {
     type: addressType,
-    values: orangeGroup
+    values: addressList
+  },
+  {
+    type: domainType,
+    values: domainList
   }
 ]
 
 describe('cleanupContentType()', () => {
   it('can get list of addresses in allow credential', async () => {
-    const addressList = getCredentialList(allowCredentials, addressType)
-    const domainList = getCredentialList(allowCredentials, domainType)
-    expect(addressList).toBe(appleGroup)
+    const addressList = getCredentialList(addressTypeCredential, addressType)
+    const domainList = getCredentialList(addressTypeCredential, domainType)
+    const emptyList = getCredentialList([], domainType)
+    expect(addressList).toBe(addressList)
     expect(domainList).toBe([])
+    expect(emptyList).toBe([])
+  })
+
+  it('can remove address2 from allow credential list', async () => {
+    const updatedCredential = updateCredential(
+      addressTypeCredential,
+      addressType,
+      [address1]
+    )
+    const addressList = getCredentialList(updatedCredential, addressType)
+    expect(addressList).toBe([address1])
+  })
+
+  it('can remove all address from allow credential list', async () => {
+    const updatedCredential = updateCredential(
+      addressTypeCredential,
+      addressType,
+      []
+    )
+    const addressList = getCredentialList(updatedCredential, addressType)
+    expect(addressList).toBe([])
+  })
+
+  it('can add address from into empty list', async () => {
+    const updatedCredential = updateCredential([], addressType, addressList)
+    expect(updatedCredential).toBe(addressTypeCredential)
+  })
+
+  it('can remove domain type crdential from existing list', async () => {
+    const updatedCredential = updateCredential(
+      addressTypeAndDomainTYpeCredential,
+      domainType,
+      []
+    )
+    expect(updatedCredential.length).toBe(1)
   })
 })
