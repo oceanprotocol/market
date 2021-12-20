4c115ba9c3c299f19902fff17e63cba13b2315af
ClaudiaHolhos <claudia@oceanprotocol.com>
2021-12-20 22:41:31 +0200

    working validation

--- src/@utils/provider.ts
+++ src/@utils/provider.ts
@@ -28,9 +28,35 @@ export async function getEncryptedFileUrls(
   }
 }
 
+async function fetchMethod(provider: string, url: string): Promise<Response> {
+  const requestOptions = {
+    method: 'POST',
+    body: url,
+    headers: { 'Content-Type': 'application/json' }
+  }
+  const result = await fetch(provider, requestOptions)
+  if (!result.status) {
+    LoggerInstance.error(`Response message: error`)
+    throw result
+  }
+  return result
+}
+
 export async function fileInfo(url: string, providerUri: string): Promise<any> {
   try {
-    const response = await ProviderInstance.fileinfo(url, providerUri, fetch)
+    const response = await ProviderInstance.fileinfo(
+      url,
+      'https://providerv4.rinkeby.oceanprotocol.com',
+      fetchMethod
+      // fetch(providerUri, {
+      //   method: 'POST',
+      //   body: url,
+      //   headers: {
+      //     'Content-type': 'application/json'
+      //   }
+      // })
+    )
+    console.log('RESPONSE: ', response)
     if (!response) return
     return response
   } catch (error) {
