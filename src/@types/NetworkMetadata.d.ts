b1399bd579fdf93395154caff0d503f4e25b47ce
Matthias Kretschmann <m@kretschmann.io>
2022-01-11 09:59:44 +0000

    network metadata updates
    * move over b2330fdc99c847afb5065223f49d9d2dc8e610d1 (#980)

--- src/@types/NetworkMetadata.d.ts
+++ src/@types/NetworkMetadata.d.ts
@@ -1,9 +1,9 @@
 interface EthereumListsChain {
   name: string
+  title?: string
   chainId: number
   shortName: string
   chain: string
-  network: string
   networkId: number
   nativeCurrency: { name: string; symbol: string; decimals: number }
   rpc: string[]
