ef30a6386bf8e3c2af97488313ba3b6a9820c37f
Matthias Kretschmann <m@kretschmann.io>
2022-01-12 15:20:18 +0000

    tx link tweaks, output tx count beforehand

--- src/components/Publish/Submission/Feedback.tsx
+++ src/components/Publish/Submission/Feedback.tsx
@@ -1,8 +1,8 @@
-import ExplorerLink from '@shared/ExplorerLink'
 import { useFormikContext } from 'formik'
 import React, { ReactElement } from 'react'
 import { FormPublishData } from '../_types'
 import styles from './Feedback.module.css'
+import TransactionCount from './TransactionCount'
 
 export function Feedback(): ReactElement {
   const { values } = useFormikContext<FormPublishData>()
@@ -10,15 +10,13 @@ export function Feedback(): ReactElement {
   const items = Object.entries(values.feedback).map(([key, value], index) => (
     <li key={index} className={styles[value.status]}>
       <h3 className={styles.title}>
-        {value.name}{' '}
-        {value.txHash && (
-          <ExplorerLink
-            networkId={values.user.chainId}
-            path={`/tx/${value.txHash}`}
-            className={styles.txHash}
-          >
-            View Transaction
-          </ExplorerLink>
+        {value.name}
+        {value.txCount > 0 && (
+          <TransactionCount
+            txCount={value.txCount}
+            chainId={values.user.chainId}
+            txHash={value.txHash}
+          />
         )}
       </h3>
       <p className={styles.description}>{value.description}</p>
