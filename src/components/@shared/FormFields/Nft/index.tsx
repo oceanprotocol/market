de9e082fa37996e8470b66119122e86224efa34d
Luca Milanese <luca.milanese90@gmail.com>
2022-01-18 12:42:20 +0100

    feature: add gas cost estimation for nft artwork

--- src/components/@shared/FormFields/Nft/index.tsx
+++ src/components/@shared/FormFields/Nft/index.tsx
@@ -1,21 +1,71 @@
 import Button from '@shared/atoms/Button'
 import { InputProps } from '@shared/FormInput'
-import { generateNftMetadata } from '@utils/nft'
+import {
+  generateNftCreateData,
+  generateNftMetadata,
+  NftMetadata
+} from '@utils/nft'
 import { useField } from 'formik'
-import React, { ReactElement, useEffect } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import Refresh from '@images/refresh.svg'
 import styles from './index.module.css'
+import { useWeb3 } from '@context/Web3'
+import useNftFactory from '@hooks/contracts/useNftFactory'
+import { NftFactory } from '@oceanprotocol/lib'
+
+const getEstGasFee = async ({
+  address,
+  nftFactory,
+  nftMetadata
+}: {
+  address: string
+  nftFactory: NftFactory
+  nftMetadata: NftMetadata
+}): Promise<string> => {
+  if (!address || !nftFactory) return
+
+  const { web3 } = nftFactory
+  const nft = generateNftCreateData(nftMetadata)
+
+  const gasPrice = await web3.eth.getGasPrice()
+  const gasLimit = await nftFactory?.estGasCreateNFT(address, nft)
+  const gasFeeEth = web3.utils.fromWei(
+    (+gasPrice * +gasLimit).toString(),
+    'ether'
+  )
+  return (+gasFeeEth).toFixed(8)
+}
 
 export default function Nft(props: InputProps): ReactElement {
+  const { accountId } = useWeb3()
+  const nftFactory = useNftFactory()
+
+  const [gasFee, setGasFee] = useState('')
   const [field, meta, helpers] = useField(props.name)
+
+  const refreshNftMetadata = async ({
+    address,
+    nftFactory
+  }: {
+    address: string
+    nftFactory: NftFactory
+  }) => {
+    if (!address || !nftFactory) return
+
+    const nftMetadata = generateNftMetadata()
+    helpers.setValue({ ...nftMetadata })
+    setGasFee(await getEstGasFee({ address, nftFactory, nftMetadata }))
+  }
 
   // Generate on first mount
   useEffect(() => {
-    if (field.value?.name !== '') return
+    if (field.value?.name !== '' || !accountId || !nftFactory) return
 
-    const nftOptions = generateNftMetadata()
-    helpers.setValue({ ...nftOptions })
-  }, [field.value?.name])
+    const initialize = async () =>
+      await refreshNftMetadata({ address: accountId, nftFactory })
+
+    initialize()
+  }, [field.value?.name, accountId, nftFactory])
 
   return (
     <div className={styles.nft}>
@@ -26,10 +76,9 @@ export default function Nft(props: InputProps): ReactElement {
           size="small"
           className={styles.refresh}
           title="Generate new image"
-          onClick={(e) => {
+          onClick={async (e) => {
             e.preventDefault()
-            const nftMetadata = generateNftMetadata()
-            helpers.setValue({ ...nftMetadata })
+            await refreshNftMetadata({ address: accountId, nftFactory })
           }}
         >
           <Refresh />
@@ -41,6 +90,9 @@ export default function Nft(props: InputProps): ReactElement {
         <strong>{field?.value?.symbol}</strong>
         <br />
         {field?.value?.description}
+      </div>
+      <div className={styles.gas}>
+        <p>{`Gas fee estimation for this artwork: ${gasFee} ETH`}</p>
       </div>
     </div>
   )
