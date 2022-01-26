b937f808fa0a4b2fe1af51d0bcb8f0be248015b3
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-01-26 18:28:25 +0300

    Conditional rendering - only showing start migration to the owner of the asset

--- src/components/molecules/migration/startMigration.tsx
+++ src/components/molecules/migration/startMigration.tsx
@@ -1,25 +1,30 @@
 import React, { ReactElement, useState, useEffect } from 'react'
 import Container from '../../atoms/Container'
-import Button from '../../atoms/Button'
 import Alert from '../../atoms/Alert'
 import styles from './startMigration.module.css'
+import { useWeb3 } from '../../../providers/Web3'
+import { useAsset } from '../../../providers/Asset'
 
 function startMigration() {
   console.log('Start Migration Clicked')
 }
 
 export default function StartMigration(): ReactElement {
+  const { accountId } = useWeb3()
+  const { ddo } = useAsset()
   return (
-    <Container className={styles.container}>
-      <Alert
-        text="**Time to migrate from V3 to V4** \n\nAs the asset publisher you can initiate the migration from a V3 pool to
+    ddo.proof.creator === accountId && (
+      <Container className={styles.container}>
+        <Alert
+          text="**Time to migrate from V3 to V4** \n\nAs the asset publisher you can initiate the migration from a V3 pool to
         a V4 pool. \n\nThe migration requires 80% of liquidity providers to lock their shares in the migration contract."
-        state="info"
-        action={{
-          name: 'Start Migration',
-          handleAction: () => startMigration()
-        }}
-      />
-    </Container>
+          state="info"
+          action={{
+            name: 'Start Migration',
+            handleAction: () => startMigration()
+          }}
+        />
+      </Container>
+    )
   )
 }
