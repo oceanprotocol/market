3d31461533a862afdf35452ec78ecd71953d9781
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-01-26 18:21:10 +0300

    Adding styles and updating text

--- src/components/molecules/migration/startMigration.tsx
+++ src/components/molecules/migration/startMigration.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useState, useEffect } from 'react'
 import Container from '../../atoms/Container'
 import Button from '../../atoms/Button'
 import Alert from '../../atoms/Alert'
+import styles from './startMigration.module.css'
 
 function startMigration() {
   console.log('Start Migration Clicked')
@@ -9,10 +10,10 @@ function startMigration() {
 
 export default function StartMigration(): ReactElement {
   return (
-    <Container>
+    <Container className={styles.container}>
       <Alert
-        text="As the asset publisher you can initiate the migration from a V3 pool to
-        a V4 pool"
+        text="**Time to migrate from V3 to V4** \n\nAs the asset publisher you can initiate the migration from a V3 pool to
+        a V4 pool. \n\nThe migration requires 80% of liquidity providers to lock their shares in the migration contract."
         state="info"
         action={{
           name: 'Start Migration',
