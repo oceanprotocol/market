dd234666034ded8ad894a56d799332c8a513bd4e
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-01-27 15:30:17 +0300

    creating Migration context provider

---
+++ src/providers/Migration.tsx
@@ -0,0 +1,58 @@
+import React, {
+  useContext,
+  useState,
+  useEffect,
+  createContext,
+  ReactElement,
+  ReactNode,
+  useCallback
+} from 'react'
+import Web3 from 'web3'
+import Web3Modal, { getProviderInfo, IProviderInfo } from 'web3modal'
+import { infuraProjectId as infuraId, portisId } from '../../app.config'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import { Logger } from '@oceanprotocol/lib'
+import { isBrowser } from '../utils'
+import {
+  EthereumListsChain,
+  getNetworkDataById,
+  getNetworkDisplayName,
+  getNetworkType,
+  NetworkType
+} from '../utils/web3'
+import { getEnsName } from '../utils/ens'
+import { UserBalance } from '../@types/TokenBalance'
+import { getOceanBalance } from '../utils/ocean'
+import useNetworkMetadata from '../hooks/useNetworkMetadata'
+
+interface MigrationStatus {}
+
+const MigrationContext = createContext({} as MigrationStatus)
+
+function MigrationProvider({
+  asset,
+  children
+}: {
+  asset: string | DDO
+  children: ReactNode
+}): ReactElement {
+  return (
+    <MigrationContext.Provider
+      value={{
+        migrationStatus
+      }}
+    >
+      {children}
+    </MigrationContext.Provider>
+  )
+}
+// Helper hook to access the provider values
+const useMigrationStatus = (): MigrationStatus => useContext(MigrationContext)
+
+export {
+  MigrationProvider,
+  useMigrationStatus,
+  MigrationStatus,
+  MigrationContext
+}
+export default MigrationProvider
