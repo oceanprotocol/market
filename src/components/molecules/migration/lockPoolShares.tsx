86e27d78ea497f17f39b7a03247411da74f00a38
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-02-03 16:39:30 +0300

    creating lockPoolShares component

---
+++ src/components/molecules/migration/lockPoolShares.tsx
@@ -0,0 +1,52 @@
+import React, { ReactElement, useState, useEffect } from 'react'
+import Container from '../../atoms/Container'
+import Alert from '../../atoms/Alert'
+import styles from './startMigration.module.css'
+import { useWeb3 } from '../../../providers/Web3'
+import Web3 from 'web3'
+import { useAsset } from '../../../providers/Asset'
+import { useMigrationStatus } from '../../../providers/Migration'
+import { Migration } from 'v4-migration-lib/'
+import { DDO, MetadataMain } from '@oceanprotocol/lib'
+
+async function addShares(
+  web3: Web3,
+  accountId: string,
+  migrationAddress: string,
+  poolV3Address: string
+) {
+  const migration = new Migration(web3)
+  await migration.addShares(
+    accountId,
+    migrationAddress,
+    poolV3Address,
+    lptV3Amount
+  )
+}
+
+export default function LockPoolShares(): ReactElement {
+  const { accountId } = useWeb3()
+  const { owner, did, ddo, metadata } = useAsset()
+  const [poolTokens, setPoolTokens] = useState<string>()
+  const { status, migrationAddress, poolV3Address, dtV3Address } =
+    useMigrationStatus()
+  const { web3 } = useWeb3()
+
+  return (
+    owner === accountId &&
+    status === 0 && (
+      <Container className={styles.container}>
+        <Alert
+          text="**The publisher of this data asset has initiated the migration of this pool from V3 to V4. You can now lock your liquidity pool tokens in the smart contract to ensure you will receive tokens from the new V4 pool when it is created.
+          \n\nThe migration requires 80% of liquidity providers to lock their shares in the migration contract."
+          state="info"
+          action={{
+            name: 'Lock Tokens',
+            handleAction: () =>
+              addShares(web3, accountId, migrationAddress, poolV3Address)
+          }}
+        />
+      </Container>
+    )
+  )
+}
