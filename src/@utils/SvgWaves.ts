d688b81a80b6014fc73311af320c11a36266ff82
Moritz Kirstein <mkirstein@protonmail.com>
2022-02-14 13:16:29 +0100

    Feature: SVG Generator (#1032)
    * use smaller SVG for publishing test, siwtch to Buffer encoding/decoding
    
    * shorter NFT name & symbol
    
    * fix: fix small test svg
    
    * feat: first draft random wave svg generation
    
    * refactor: utilize join to remove unecessary ',' separator
    
    * feature: add gas cost estimation for nft artwork
    
    * refacotr: allow fillColor as array and adjust opacity
    
    * refactor: adjust random wave generation
    
    * refactor: change nft help text
    
    * refactor: trying tooltip for nft image info
    
    * feat: add custom algorithm to generate svg waves
    
    * fix: use text colors for nft form field svgs
    
    * refactor: code cleanup nft utils
    
    * refactor: improve readability
    
    * refactor: improve comment
    
    * refactor: (re)move comments, cleanup
    
    * refactor: remove console log
    
    * refactor: adjust default svg wave values & remove logging
    
    * refactor: tweak default SvgWave prop values
    
    * fix: svg preview with disconnected wallet
    
    * feat: show artwork gas fee estimation in user preferred currency
    
    * refactor: extract gas fee estimation logic to new component
    
    * cleanup: remove oceanWaves, remove d3
    
    * icon visual weight tweak
    
    Co-authored-by: Luca Milanese <luca.milanese90@gmail.com>
    Co-authored-by: Matthias Kretschmann <m@kretschmann.io>

---
+++ src/@utils/SvgWaves.ts
@@ -0,0 +1,190 @@
+import { computeControlPoints } from './bezier-spline'
+import { randomIntFromInterval } from './numbers'
+
+export interface WaveProperties {
+  width?: number
+  height?: number
+  color?: string
+  fill?: boolean
+  layerCount?: number
+  pointsPerLayer?: number
+  variance?: number
+  maxOpacity?: number
+  opacitySteps?: number
+}
+
+class Point {
+  x: number
+  y: number
+
+  constructor(x: number, y: number) {
+    this.x = Math.floor(x)
+    this.y = Math.floor(y)
+  }
+}
+
+enum WaveColors {
+  Violet = '#e000cf',
+  Pink = '#ff4092',
+  Grey = '#8b98a9'
+}
+
+export class SvgWaves {
+  properties: WaveProperties
+  layers: Point[][]
+
+  static xmlns = 'http://www.w3.org/2000/svg'
+
+  /**
+   * Helper function to get randomly generated WaveProperties
+   * These are generated with a focus on small file size for the svg
+   * meaning low character count.
+   * - width & height: default is 2 digits max (99)
+   * - color pink is selected per default
+   * - randomly decide if fill or stroke coloring should be used
+   * - create 4 layers with 4 - 5 points per layers
+   *     -> results in random looking, yet small enough svgs
+   * - variance between 0.5 and 0.7 returns smooth & different results
+   *
+   * @returns new randomly generated WaveProperties
+   */
+  static getProps(): WaveProperties {
+    return {
+      width: 99,
+      height: 99,
+      color: WaveColors.Pink,
+      fill: Math.random() < 0.5, // random true or false
+      layerCount: 4,
+      pointsPerLayer: randomIntFromInterval(3, 4),
+      variance: Math.random() * 0.2 + 0.5, // 0.5 - 0.7
+      maxOpacity: 255, // 0xff
+      opacitySteps: 68 // 0x44
+    }
+  }
+
+  static generatePoint(
+    x: number,
+    y: number,
+    moveX: number,
+    moveY: number,
+    width: number
+  ): Point {
+    const varianceY = y - moveY / 2 + Math.random() * moveY
+    const varianceX =
+      x === 0 || x === width ? x : x - moveX / 2 + Math.random() * moveX
+    return new Point(varianceX, varianceY)
+  }
+
+  constructor() {
+    this.properties = SvgWaves.getProps()
+    this.layers = this.generateLayers()
+  }
+
+  generateLayers(): Point[][] {
+    const { width, height, layerCount, pointsPerLayer, variance } =
+      this.properties
+
+    const cellWidth = width / pointsPerLayer
+    const cellHeight = height / layerCount
+
+    // define movement constraints for point generation
+    // lower smoothness results in steeper curves in waves
+    const horizontalSmoothness = 0.5
+    const moveX = cellWidth * variance * (1 - horizontalSmoothness)
+    const moveY = cellHeight * variance
+
+    const layers = []
+    for (let y = cellHeight; y < height; y += cellHeight) {
+      const points = []
+      for (let x = 0; x <= width; x += cellWidth) {
+        points.push(SvgWaves.generatePoint(x, y, moveX, moveY, width))
+      }
+      layers.push(points)
+    }
+    return layers
+  }
+
+  generateSvg(): Element {
+    const svg = document.createElementNS(SvgWaves.xmlns, 'svg')
+    svg.setAttribute('width', this.properties.width.toString())
+    svg.setAttribute('height', this.properties.height.toString())
+    svg.setAttribute('fill', this.properties.fill ? undefined : 'transparent')
+    svg.setAttribute('xmlns', SvgWaves.xmlns)
+
+    for (let i = 0; i < this.layers.length; i++) {
+      const path = this.generatePath(
+        this.layers[i],
+        this.getOpacityForLayer(i + 1)
+      )
+      svg.appendChild(path)
+    }
+    return svg
+  }
+
+  generatePath(points: Point[], opacity = 'ff'): Element {
+    const xPoints = points.map((p) => Math.floor(p.x))
+    const yPoints = points.map((p) => Math.floor(p.y))
+
+    // get bezier control points
+    const xControlPoints = computeControlPoints(xPoints)
+    const yControlPoints = computeControlPoints(yPoints)
+
+    // Should the path be closed & filled or stroke only
+    const closed = this.properties.fill
+
+    // For closed paths define bottom corners as start & end point
+    const bottomLeftPoint = new Point(0, this.properties.height)
+    const bottomRightPoint = new Point(
+      this.properties.width,
+      this.properties.height
+    )
+    const startPoint = closed ? bottomLeftPoint : points[0]
+    const endPoint = closed ? bottomRightPoint : points[points.length - 1]
+
+    // start constructing the 'd' attribute for the <path>
+    let path = `M${startPoint.x},${startPoint.y}`
+
+    // if starting in bottom corner, move to start of wave first
+    if (closed) path += `L${xPoints[0]},${yPoints[0]}`
+
+    // add path curves
+    for (let i = 0; i < xPoints.length - 1; i++) {
+      path +=
+        `C${xControlPoints.p1[i]},${yControlPoints.p1[i]} ` +
+        `${xControlPoints.p2[i]},${yControlPoints.p2[i]} ` +
+        `${xPoints[i + 1]},${yPoints[i + 1]}`
+    }
+
+    path = closed
+      ? `${path}L${endPoint.x},${endPoint.y}Z` // if closing in bottom corners move there and close
+      : `${path}AZ` // else just close the path
+
+    // create the path element
+    const svgPath = document.createElementNS(SvgWaves.xmlns, 'path')
+    const colorStyle = closed ? 'fill' : 'stroke'
+    svgPath.setAttributeNS(
+      null,
+      colorStyle,
+      `${this.properties.color}${opacity}`
+    )
+    svgPath.setAttributeNS(null, 'd', path)
+
+    return svgPath
+  }
+
+  getOpacityForLayer(layer: number): string {
+    const { layerCount, maxOpacity, opacitySteps } = this.properties
+
+    const minOpacity = maxOpacity - (layerCount - 1) * opacitySteps
+    // calculate decimal opacity value for layer
+    const opacityDec = minOpacity + layer * opacitySteps
+
+    // translate to hex string
+    return opacityDec.toString(16)
+  }
+
+  setProps(properties: WaveProperties): void {
+    this.properties = { ...SvgWaves.getProps(), ...properties }
+    this.layers = this.generateLayers()
+  }
+}
--- src/@utils/nft.ts
+++ src/@utils/nft.ts
@@ -1,4 +1,4 @@
-import { renderStaticWaves } from './oceanWaves'
+import { SvgWaves } from './SvgWaves'
 
 // https://docs.opensea.io/docs/metadata-standards
 export interface NftMetadata {
@@ -21,6 +21,7 @@ function encodeSvg(svgString: string): string {
         ? '<svg'
         : '<svg xmlns="http://www.w3.org/2000/svg"'
     )
+    .replace('></path>', '/>')
     .replace(/"/g, "'")
     .replace(/%/g, '%25')
     .replace(/#/g, '%23')
@@ -32,41 +33,40 @@ function encodeSvg(svgString: string): string {
 }
 
 export function generateNftMetadata(): NftMetadata {
-  // TODO: crop image properly in the end as generated SVG waves are a super-wide image,
-  // and add a filled background deciding on either black or white.
-  const image = renderStaticWaves()
-  // const image = new XMLSerializer().serializeToString(waves)
-  // const image = `<svg><path d="M0 10.4304L16.3396 10.4304L8.88727 17.6833L10.2401 19L20 9.5L10.2401 0L8.88727 1.31491L16.3396 8.56959L0 8.56959V10.4304Z" /></svg>`
+  const waves = new SvgWaves()
+  const svg = waves.generateSvg()
+
+  // TODO: figure out if also image URI needs base64 encoding
+  // e.g. 'data:image/svg+xml;base64,'
+  // generated SVG embedded as 'data:image/svg+xml' and encoded characters
+  const imageData = `data:image/svg+xml,${encodeSvg(svg.outerHTML)}`
 
   const newNft: NftMetadata = {
-    name: 'Ocean Asset v4 NFT',
+    name: 'Ocean Asset NFT',
     symbol: 'OCEAN-NFT',
     description: `This NFT represents an asset in the Ocean Protocol v4 ecosystem.`,
     // TODO: ideally this includes the final DID
     external_url: 'https://market.oceanprotocol.com',
     background_color: '141414', // dark background
-    // TODO: figure out if also image URI needs base64 encoding
-    // generated SVG embedded as 'data:image/svg+xml' and encoded characters
-    image_data: `data:image/svg+xml,${encodeSvg(image)}`
-    // generated SVG embedded as 'data:image/svg+xml;base64'
-    // image: `data:image/svg+xml;base64,${window?.btoa(image)}`
-    // image: `data:image/svg+xml;base64,${Buffer.from(image).toString('base64')}`
+    image_data: imageData
   }
 
   return newNft
 }
 
 export function generateNftCreateData(nftMetadata: NftMetadata): any {
+  // TODO: figure out if Buffer.from method is working in browser in final build
+  // as BTOA is deprecated.
+  // tokenURI: window?.btoa(JSON.stringify(nftMetadata))
+  const encodedMetadata = Buffer.from(JSON.stringify(nftMetadata)).toString(
+    'base64'
+  )
+
   const nftCreateData = {
     name: nftMetadata.name,
     symbol: nftMetadata.symbol,
     templateIndex: 1,
-    // Gas estimation fails if we add our huge tokenURI
-    tokenURI: '{url:"https://coolImage.com, name: "just for test"}'
-    // TODO: figure out if Buffer.from method is working in browser in final build
-    // as BTOA is deprecated.
-    // tokenURI: window?.btoa(JSON.stringify(nftMetadata))
-    // tokenURI: Buffer.from(JSON.stringify(nftMetadata)).toString('base64') // should end up as data:application/json;base64
+    tokenURI: `data:application/json;base64,${encodedMetadata}`
   }
 
   return nftCreateData
