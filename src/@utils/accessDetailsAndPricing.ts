60c552054847b923d09946d5e43e8f2b340ab8af
mihaisc <mihai.scarlat@smartcontrol.ro>
2022-02-16 20:42:35 +0200

    update to ocean.js.next.20 (#1099)
    * update and fix fees
    
    * update lib
    
    * update aquarius endpoint
    
    * update lib and fix changes
    
    Signed-off-by: mihaisc <mihai.scarlat@smartcontrol.ro>

--- src/@utils/accessDetailsAndPricing.ts
+++ src/@utils/accessDetailsAndPricing.ts
@@ -262,12 +262,23 @@ export async function getOrderPriceAndFees(
   switch (accessDetails.type) {
     case 'dynamic': {
       const poolPrice = await calculateBuyPrice(accessDetails, asset.chainId)
-      orderPriceAndFee.price = poolPrice
+      orderPriceAndFee.price = poolPrice.tokenAmount
+      orderPriceAndFee.liquidityProviderSwapFee =
+        poolPrice.liquidityProviderSwapFeeAmount
+      orderPriceAndFee.publisherMarketPoolSwapFee =
+        poolPrice.publishMarketSwapFeeAmount
+      orderPriceAndFee.consumeMarketPoolSwapFee =
+        poolPrice.consumeMarketSwapFeeAmount
       break
     }
     case 'fixed': {
       const fixed = await getFixedBuyPrice(accessDetails, asset.chainId)
       orderPriceAndFee.price = fixed.baseTokenAmount
+      orderPriceAndFee.opcFee = fixed.oceanFeeAmount
+      orderPriceAndFee.publisherMarketFixedSwapFee = fixed.marketFeeAmount
+      // hack because we don't have it in contracts
+      orderPriceAndFee.consumeMarketFixedSwapFee = fixed.consumeMarketFeeAmount
+
       break
     }
   }
