26291d8e2db43d77728c4edec5e7c0da7e686fee
Moritz Kirstein <mkirstein@protonmail.com>
2022-02-17 22:44:38 +0100

    feat: add nft data to publish preview

--- src/components/Publish/_utils.ts
+++ src/components/Publish/_utils.ts
@@ -182,7 +182,7 @@ export async function transformPublishFormToDdo(
         }
       ],
       nft: {
-        owner: accountId
+        ...generateNftCreateData(values?.metadata.nft)
       }
     })
   }
