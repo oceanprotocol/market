5c95eb9694b3ca0c442f18f7cdce5fa16f5ee11c
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-02-17 17:24:23 +0300

    Creating Cancel Migration component

---
+++ src/components/molecules/migration/cancelMigration.tsx
@@ -0,0 +1,63 @@
+import React, { ReactElement } from 'react'
+import Container from '../../atoms/Container'
+import Alert from '../../atoms/Alert'
+import styles from './startMigration.module.css'
+import { useWeb3 } from '../../../providers/Web3'
+import Web3 from 'web3'
+import { useAsset } from '../../../providers/Asset'
+import { useMigrationStatus } from '../../../providers/Migration'
+import { Migration } from 'v4-migration-lib'
+
+async function liquidateAndCreatePool(
+  web3: Web3,
+  accountId: string,
+  migrationAddress: string,
+  poolV3Address: string
+) {
+  const migration = new Migration(web3)
+  await migration.cancelMigration(accountId, migrationAddress, poolV3Address)
+}
+
+export default function CreateV4Pool(): ReactElement {
+  const { accountId } = useWeb3()
+  const { owner, price } = useAsset()
+  const { status, migrationAddress, thresholdMet, deadlinePassed } =
+    useMigrationStatus()
+  const { web3 } = useWeb3()
+  console.log('Start Migration status', status)
+  console.log('Start Migration thresholdMet', thresholdMet)
+
+  return (
+    <>
+      {owner === accountId && status === '1' && thresholdMet && deadlinePassed && (
+        <Container className={styles.container}>
+          <Alert
+            title="V4 Migration can be canceled"
+            text="**The V3 pool will be remain** \n\Less than 80% of pool shares have now locked and the threshold has not been reached for completing the migration.
+            \n\nThe deadline for locking pool shares has now passed so no additional pool shares can be locked."
+            state="info"
+            action={{
+              name: 'Cancel Migration',
+              handleAction: () =>
+                liquidateAndCreatePool(
+                  web3,
+                  accountId,
+                  migrationAddress,
+                  price.address
+                )
+            }}
+          />
+        </Container>
+      )}
+      {owner === accountId && status !== '0' && thresholdMet !== true && (
+        <Container className={styles.container}>
+          <Alert
+            title="Migration in progress"
+            text="**The threshold of 80% of pool shares locked has not been reached yet**  \n\nThe migration requires 80% of liquidity providers to lock their shares in the migration contract."
+            state="info"
+          />
+        </Container>
+      )}
+    </>
+  )
+}
