c1ab042c5ef6f85cc94abe003f20bdf9c81b9d30
Matthias Kretschmann <m@kretschmann.io>
2022-02-18 15:53:31 +0000

    Restore global stats (#1123)
    * start with a good refactor
    
    * split up into multiple files
    * split up data fetching and manipulation into multiple effects
    * adapt for new subgraph
    
    * bookmarks error fixes
    
    * make numbers show up when a subgraph is down
    
    * use testnets, make numbers show up
    
    * style fixes
    
    * making sense of existing numbers
    
    * more stats
    
    * use original total OCEAN
    
    * output datatokenCount
    
    * state data structure change for all sum values
    
    * copy
    
    * output networks at all times
    
    * switch to mainnets numbers

---
+++ src/components/Footer/MarketStats/Tooltip.tsx
@@ -0,0 +1,51 @@
+import React, { ReactElement } from 'react'
+import Conversion from '@shared/Price/Conversion'
+import PriceUnit from '@shared/Price/PriceUnit'
+import NetworkName from '@shared/NetworkName'
+import styles from './Tooltip.module.css'
+import { StatsValue } from './_types'
+
+export default function MarketStatsTooltip({
+  totalValueLockedInOcean,
+  poolCount,
+  totalOceanLiquidity,
+  mainChainIds
+}: {
+  totalValueLockedInOcean: StatsValue
+  poolCount: StatsValue
+  totalOceanLiquidity: StatsValue
+  mainChainIds: number[]
+}): ReactElement {
+  return (
+    <>
+      <ul className={styles.statsList}>
+        {mainChainIds?.map((chainId, key) => (
+          <li className={styles.tooltipStats} key={key}>
+            <NetworkName networkId={chainId} className={styles.network} />
+            <br />
+            <Conversion
+              price={totalValueLockedInOcean?.[chainId] || '0'}
+              hideApproximateSymbol
+            />{' '}
+            <abbr title="Total Value Locked">TVL</abbr>
+            {' | '}
+            <strong>{poolCount?.[chainId] || '0'}</strong> pools
+            {' | '}
+            <PriceUnit
+              price={totalOceanLiquidity?.[chainId] || '0'}
+              symbol="OCEAN"
+              small
+            />
+          </li>
+        ))}
+      </ul>
+      <p className={styles.note}>
+        Counted on-chain from our NFT and pool factories. Does not filter out
+        assets in{' '}
+        <a href="https://github.com/oceanprotocol/list-purgatory">
+          list-purgatory
+        </a>
+      </p>
+    </>
+  )
+}
