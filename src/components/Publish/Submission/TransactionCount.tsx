ee0a292b7408a950c12cca09527acf7f957d6079
EnzoVezzaro <enzo-vezzaro@live.it>
2022-02-22 09:27:10 +0100

    restyle TransactionCount component

--- src/components/Publish/Submission/TransactionCount.tsx
+++ src/components/Publish/Submission/TransactionCount.tsx
@@ -2,6 +2,9 @@ import ExplorerLink from '@shared/ExplorerLink'
 import React from 'react'
 import styles from './TransactionCount.module.css'
 import Conversion from '@shared/Price/Conversion'
+import Tooltip from '@shared/atoms/Tooltip'
+import { usePrices } from '@context/Prices'
+import web3 from 'web3'
 
 export default function TransactionCount({
   txCount,
@@ -14,6 +17,9 @@ export default function TransactionCount({
   txHash: string
   gasFees?: string
 }) {
+  const { prices } = usePrices()
+  const gasFeeEth = (+gasFees * +(prices as any)?.eth).toString()
+  const gasFeeGwei = web3.utils.toWei(gasFeeEth, 'ether')
   return txHash ? (
     <ExplorerLink
       networkId={chainId}
@@ -26,6 +32,10 @@ export default function TransactionCount({
     <span className={styles.txHash}>
       {txCount} Transaction{txCount > 1 ? 's' : ''}{' '}
       <Conversion price={gasFees} />
+      <Tooltip
+        content={<p>{`Gas fee estimation with ${gasFeeGwei} gwei`}</p>}
+        placement="right"
+      />
     </span>
   )
 }
