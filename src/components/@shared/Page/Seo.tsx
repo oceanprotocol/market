051a8a07d75abf2e2edfac80956a0d21affa9fb2
Matthias Kretschmann <m@kretschmann.io>
2022-03-22 12:57:40 +0000

    solve all favicon things

--- src/components/@shared/Page/Seo.tsx
+++ src/components/@shared/Page/Seo.tsx
@@ -32,6 +32,15 @@ export default function Seo({
       )}
 
       <link rel="canonical" href={canonical} />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="theme-color" content="var(--background-content)" />
 
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
