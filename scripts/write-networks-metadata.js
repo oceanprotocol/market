220f947fa9881fa6693a7ce869010e0a83b177ee
Roberto García <roberto@rhizomik.net>
2022-03-26 18:09:19 +0100

    Add local barge to networks-metadata.json

--- scripts/write-networks-metadata.js
+++ scripts/write-networks-metadata.js
@@ -7,5 +7,22 @@ const axios = require('axios')
 const chainDataUrl = 'https://chainid.network/chains.json'
 
 axios(chainDataUrl).then((response) => {
+  const development = {
+    name: 'Development',
+    chain: 'ETH',
+    icon: 'ethereum',
+    rpc: ['http://localhost:8545'],
+    faucets: [],
+    nativeCurrency: {
+      name: 'Ether',
+      symbol: 'ETH',
+      decimals: 18
+    },
+    shortName: 'eth',
+    chainId: 8996,
+    networkId: 8996,
+    slip44: 60
+  }
+  response.data.push(development)
   process.stdout.write(JSON.stringify(response.data, null, '  '))
 })
