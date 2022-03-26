b8ffcd2ea3ac4ef30eb5b42804bc33dc729684d8
Roberto García <roberto@rhizomik.net>
2022-03-26 18:06:48 +0100

    Add start-local using local ocean-subgraph for codegen

---
+++ scripts/pregenerate-local.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Write out repo metadata
+node ./scripts/write-repo-metadata > content/repo-metadata.json
+
+# Generate Apollo typings
+npm run codegen:apollo-local
+
+# Fetch EVM networks metadata
+node ./scripts/write-networks-metadata > content/networks-metadata.json
