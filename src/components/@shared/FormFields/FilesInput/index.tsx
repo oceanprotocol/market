a129aba410fc5aa3cd5280d9daab3244c6159901
Matthias Kretschmann <m@kretschmann.io>
2022-03-29 22:01:59 +0100

    cleanup

--- src/components/@shared/FormFields/FilesInput/index.tsx
+++ src/components/@shared/FormFields/FilesInput/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useCallback, useEffect, useState } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { useField, useFormikContext } from 'formik'
 import { toast } from 'react-toastify'
 import FileInfo from './Info'
@@ -7,6 +7,7 @@ import { InputProps } from '@shared/FormInput'
 import { initialValues } from 'src/components/Publish/_constants'
 import { getFileUrlInfo } from '@utils/provider'
 import { FormPublishData } from 'src/components/Publish/_types'
+import { LoggerInstance } from '@oceanprotocol/lib'
 
 export default function FilesInput(props: InputProps): ReactElement {
   const [field, meta, helpers] = useField(props.name)
@@ -14,48 +15,22 @@ export default function FilesInput(props: InputProps): ReactElement {
   const [isLoading, setIsLoading] = useState(false)
   const { values } = useFormikContext<FormPublishData>()
 
-  const loadFileInfo = useCallback(
-    (url: string) => {
-      const providerUri =
-        (values.services && values.services[0].providerUrl.url) ||
-        'https://provider.mainnet.oceanprotocol.com'
-
-      async function validateUrl() {
-        try {
-          setIsLoading(true)
-          const checkedFile = await getFileUrlInfo(url, providerUri)
-          setIsInvalidUrl(!checkedFile[0].valid)
-          checkedFile && helpers.setValue([{ url, ...checkedFile[0] }])
-        } catch (error) {
-          toast.error(
-            'Could not fetch file info. Please check URL and try again'
-          )
-          console.error(error.message)
-        } finally {
-          setIsLoading(false)
-        }
-      }
-
-      validateUrl()
-    },
-    [helpers, values.services]
-  )
-
-  useEffect(() => {
-    // try load from initial values, kinda hacky but it works
-    if (
-      props.value &&
-      props.value.length > 0 &&
-      typeof props.value[0] === 'string'
-    ) {
-      loadFileInfo(props.value[0].toString())
-    }
-  }, [loadFileInfo, props])
-
-  async function handleButtonClick(e: React.SyntheticEvent, url: string) {
+  async function handleValidation(e: React.SyntheticEvent, url: string) {
     // File example 'https://oceanprotocol.com/tech-whitepaper.pdf'
     e.preventDefault()
-    loadFileInfo(url)
+
+    try {
+      const providerUrl = values?.services[0].providerUrl.url
+      setIsLoading(true)
+      const checkedFile = await getFileUrlInfo(url, providerUrl)
+      setIsInvalidUrl(!checkedFile[0].valid)
+      checkedFile && helpers.setValue([{ url, ...checkedFile[0] }])
+    } catch (error) {
+      toast.error('Could not fetch file info. Please check URL and try again')
+      LoggerInstance.error(error.message)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   function handleClose() {
@@ -74,7 +49,7 @@ export default function FilesInput(props: InputProps): ReactElement {
           name={`${field.name}[0].url`}
           hasError={Boolean(meta.touched && isInvalidUrl)}
           isLoading={isLoading}
-          handleButtonClick={handleButtonClick}
+          handleButtonClick={handleValidation}
         />
       )}
     </>
