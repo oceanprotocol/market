42ea537c222c6c1cc34e010f6140fb33eec55ce1
Matthias Kretschmann <m@kretschmann.io>
2022-03-29 22:15:53 +0100

    warn about html files

--- src/components/@shared/FormFields/FilesInput/Info.tsx
+++ src/components/@shared/FormFields/FilesInput/Info.tsx
@@ -3,6 +3,7 @@ import { prettySize } from './utils'
 import cleanupContentType from '@utils/cleanupContentType'
 import styles from './Info.module.css'
 import { FileMetadata } from '@oceanprotocol/lib'
+import Alert from '@shared/atoms/Alert'
 
 export default function FileInfo({
   file,
@@ -11,6 +12,15 @@ export default function FileInfo({
   file: FileMetadata
   handleClose(): void
 }): ReactElement {
+  const contentTypeCleaned = file.contentType
+    ? cleanupContentType(file.contentType)
+    : null
+
+  // Prevent accidential publishing of error pages (e.g. 404) for
+  // popular file hosting services by warning about it.
+  // See https://github.com/oceanprotocol/market/issues/1246
+  const shouldWarnAboutFile = file.valid && contentTypeCleaned === 'html'
+
   return (
     <div className={`${styles.info} ${file.valid ? '' : styles.hasError}`}>
       <h3 className={styles.url}>{file.url}</h3>
@@ -19,14 +29,19 @@ export default function FileInfo({
           <li className={styles.success}>✓ URL confirmed</li>
         ) : (
           <li className={styles.error}>
-            {' '}
             ✗ No valid file detected. Check your URL and try again.
           </li>
         )}
-
         {file.contentLength && <li>{prettySize(+file.contentLength)}</li>}
-        {file.contentType && <li>{cleanupContentType(file.contentType)}</li>}
+        {contentTypeCleaned && <li>{contentTypeCleaned}</li>}
       </ul>
+      {shouldWarnAboutFile && (
+        <Alert
+          state="info"
+          text={`Your file was detected as ${contentTypeCleaned}, which is unusal for a data asset. Are you sure?`}
+          className={styles.warning}
+        />
+      )}
       <button className={styles.removeButton} onClick={handleClose}>
         &times;
       </button>
