0cea96e36065bb60a3b59cd4b029750464189db4
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-04-06 16:49:38 +0300

    Showing connect wallet button on submit page when no wallet is connected

--- src/components/Publish/Actions/index.tsx
+++ src/components/Publish/Actions/index.tsx
@@ -5,6 +5,7 @@ import { FormikContextType, useFormikContext } from 'formik'
 import { FormPublishData } from '../_types'
 import { wizardSteps } from '../_constants'
 import SuccessConfetti from '@shared/SuccessConfetti'
+import { useWeb3 } from '../../../@context/Web3'
 
 export default function Actions({
   scrollToRef,
@@ -20,6 +21,15 @@ export default function Actions({
     isSubmitting,
     setFieldValue
   }: FormikContextType<FormPublishData> = useFormikContext()
+
+  const { connect } = useWeb3()
+
+  async function handleActivation(e: FormEvent<HTMLButtonElement>) {
+    // prevent accidentially submitting a form the button might be in
+    e.preventDefault()
+
+    await connect()
+  }
 
   function handleNext(e: FormEvent) {
     e.preventDefault()
@@ -65,14 +75,12 @@ export default function Actions({
             >
               Continue
             </Button>
+          ) : values.user.accountId === '' || !isValid ? (
+            <Button type="submit" style="primary" onClick={handleActivation}>
+              Connect Wallet
+            </Button>
           ) : (
-            <Button
-              type="submit"
-              style="primary"
-              disabled={
-                values.user.accountId === '' || !isValid || isSubmitting
-              }
-            >
+            <Button type="submit" style="primary" disabled={isSubmitting}>
               Submit
             </Button>
           )}
