429087e32c8bcc9710bff7485b967985ad3fc72d
ClaudiaHolhos <claudia@oceanprotocol.com>
2022-04-18 11:41:44 +0300

    remove unused code

--- src/components/@shared/FormFields/FilesInput/index.tsx
+++ src/components/@shared/FormFields/FilesInput/index.tsx
@@ -6,8 +6,6 @@ import { InputProps } from '@shared/FormInput'
 import { getFileUrlInfo } from '@utils/provider'
 import { FormPublishData } from 'src/components/Publish/_types'
 import { LoggerInstance } from '@oceanprotocol/lib'
-import FormHelp from '@shared/FormInput/Help'
-import content from '../../../../../content/publish/form.json'
 
 export default function FilesInput(props: InputProps): ReactElement {
   const [field, meta, helpers] = useField(props.name)
