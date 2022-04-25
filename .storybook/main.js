1e6df7ec2b9ffef55450dd9a775757970f7739f4
Enzo Vezzaro <enzo-vezzaro@live.it>
2022-04-25 05:57:36 -0400

    remove unnecessary file extensions on stories config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
