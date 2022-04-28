97744820acf37eb8ccaf29656af9c7fb26027736
claudiaHash <49017601+claudiaHash@users.noreply.github.com>
2022-04-28 16:39:46 +0300

    Display generic info message for file inputs (#1334)
    * display generic message
    
    * add helper message
    
    * bold text in json, remove unused css file
    
    * fixes
    
    * different help message displayed as tooltip
    
    * tooltip help not displayed for files/links fields on publish
    
    * conditionally hide/show tooltip
    
    * use same mechanism for edit form
    
    * add prominentHelp file into content pages
    
    * type fix
    
    * remove unused code
    
    * error message position fix
    
    * error doesn't overlap help message
    
    * show validation error message fix
    
    Co-authored-by: ClaudiaHolhos <claudia@oceanprotocol.com>

--- src/components/@shared/FormFields/FilesInput/Info.tsx
+++ src/components/@shared/FormFields/FilesInput/Info.tsx
@@ -3,7 +3,6 @@ import { prettySize } from './utils'
 import cleanupContentType from '@utils/cleanupContentType'
 import styles from './Info.module.css'
 import { FileMetadata } from '@oceanprotocol/lib'
-import Alert from '@shared/atoms/Alert'
 
 export default function FileInfo({
   file,
@@ -16,11 +15,6 @@ export default function FileInfo({
     ? cleanupContentType(file.contentType)
     : null
 
-  // Prevent accidential publishing of error pages (e.g. 404) for
-  // popular file hosting services by warning about it.
-  // See https://github.com/oceanprotocol/market/issues/1246
-  const shouldWarnAboutFile = file.valid && contentTypeCleaned === 'html'
-
   return (
     <div className={styles.info}>
       <h3 className={styles.url}>{file.url}</h3>
@@ -29,13 +23,6 @@ export default function FileInfo({
         {file.contentLength && <li>{prettySize(+file.contentLength)}</li>}
         {contentTypeCleaned && <li>{contentTypeCleaned}</li>}
       </ul>
-      {shouldWarnAboutFile && (
-        <Alert
-          state="info"
-          text={`Your file was detected as ${contentTypeCleaned}, which is unusal for a data asset. If you did not intend to use a ${contentTypeCleaned} file, try a different URL pointing directly to your data asset file.`}
-          className={styles.warning}
-        />
-      )}
       <button className={styles.removeButton} onClick={handleClose}>
         &times;
       </button>
