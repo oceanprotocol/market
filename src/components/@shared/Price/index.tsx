4f24ffa4465f419e6f0cfe4e78692864577943a8
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-05-02 17:36:03 +0300

    Removing no price set message

--- src/components/@shared/Price/index.tsx
+++ src/components/@shared/Price/index.tsx
@@ -27,18 +27,7 @@ export default function Price({
       conversion={conversion}
       type={accessDetails.type}
     />
-  ) : accessDetails && !accessDetails.type ? (
-    <div className={styles.empty}>
-      No price set{' '}
-      <Tooltip content="No pricing mechanism has been set on this asset yet." />
-    </div>
   ) : (
-    // TODO: Hacky hack, put back some check for low liquidity
-    // ) : price.isConsumable !== 'true' ? (
-    //   <div className={styles.empty}>
-    //     Low liquidity{' '}
-    //     <Tooltip content="This pool does not have enough liquidity for using this data set." />
-    //   </div>
     <Loader message="Retrieving price..." />
   )
 }
