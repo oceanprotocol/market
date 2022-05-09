c7cf5f133d82bc9447e543bab07f41d492e3d79a
ClaudiaHolhos <claudia@oceanprotocol.com>
2022-05-09 11:01:59 +0300

    use react hooks for modal story

--- src/components/@shared/atoms/Modal/index.stories.tsx
+++ src/components/@shared/atoms/Modal/index.stories.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import Modal, { ModalProps } from '@shared/atoms/Modal'
+import Button from '../Button'
 
 export default {
   title: 'Component/@shared/atoms/Modal',
@@ -14,16 +15,36 @@ const Template: ComponentStory<typeof Modal> = (args: ModalProps) => (
 
 interface Props {
   args: {
+    isOpen: boolean
     title: string
     onToggleModal: () => void
-    // children: any
+    children: any
   }
 }
 
-export const Primary: Props = Template.bind({})
+export const Primary: Props = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+
+  return (
+    <>
+      <Button style="text" size="small" onClick={() => setIsDialogOpen(true)}>
+        Show Details
+      </Button>
+      <Modal
+        title="Modal opened"
+        isOpen={isDialogOpen}
+        onToggleModal={() => setIsDialogOpen(false)}
+      >
+        <p>Click ✗ to close this up!</p>
+      </Modal>
+    </>
+  )
+}
+
 Primary.args = {
+  isOpen: true,
   title: 'Modal title',
-  //   children: 'Button',
+  children: <p>Modal child</p>,
   onToggleModal: () => {
     console.log('Button pressed!')
   }
