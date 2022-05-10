a2d234a76855157fa943cb2a6754f1fac8a2c141
Matthias Kretschmann <m@kretschmann.io>
2022-05-10 12:45:42 +0100

    unused package cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,7 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
   webpackFinal: async (config) => {
     config.resolve.plugins = [
