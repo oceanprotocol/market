4f45af96878b6a6268af7e5c307fdebf9ef5ea14
Matthias Kretschmann <m@kretschmann.io>
2022-05-10 14:16:04 +0100

    split up build & test CI jobs

--- .storybook/storyshots.test.tsx
+++ .storybook/storyshots.test.tsx
@@ -1,18 +1,8 @@
 import initStoryshots from '@storybook/addon-storyshots'
 import { render, waitFor } from '@testing-library/react'
 
-const reactTestingLibrarySerializer = {
-  print: (val, serialize, indent) => serialize(val.container.firstChild),
-  test: (val) => val && val.hasOwnProperty('container')
-}
-
 // Stories are render-tested with @testing-library/react,
 // overwriting default snapshot testing behavior
-// initStoryshots({
-//   renderer: render,
-//   snapshotSerializers: [reactTestingLibrarySerializer]
-// })
-
 initStoryshots({
   asyncJest: true,
   test: async ({ story, done }) => {
