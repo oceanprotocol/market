9f77cf4dbb6f9e4ff8f38f76646d81e673e0eceb
Matthias Kretschmann <m@kretschmann.io>
2022-05-10 15:40:06 +0100

    move  jest config files, remove coverageReporter override

--- .jest/jest.config.js
+++ .jest/jest.config.js
@@ -7,8 +7,9 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
+  rootDir: '../',
   // Add more setup options before each test is run
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
@@ -21,8 +22,7 @@ const customJestConfig = {
     '@utils/(.*)$': '<rootDir>/src/@utils/$1',
     '@content/(.*)$': '<rootDir>/@content/$1'
   },
-  collectCoverage: true,
-  coverageReporters: ['lcov']
+  collectCoverage: true
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
