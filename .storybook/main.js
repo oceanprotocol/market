6f5f998036e4067a29bbabf5ddbe13eb631ec697
Matthias Kretschmann <m@kretschmann.io>
2022-05-10 12:09:20 +0100

    make storybook use webpack 5
    * see https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#webpack-5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,11 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const nextConfig = require('../next.config')
 
+console.log(nextConfig.webpack)
 module.exports = {
+  core: {
+    builder: 'webpack5'
+  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
@@ -11,6 +16,35 @@ module.exports = {
         extensions: config.resolve.extensions
       })
     ]
+
+    config.module.rules.push(
+      {
+        test: /\.svg$/,
+        issuer: /\.(tsx|ts)$/,
+        use: [
+          { loader: require.resolve('@svgr/webpack'), options: { icon: true } }
+        ]
+      },
+      {
+        test: /\.gif$/,
+        // yay for webpack 5
+        // https://webpack.js.org/guides/asset-management/#loading-images
+        type: 'asset/resource'
+      }
+    )
+
+    const fallback = config.resolve.fallback || {}
+    Object.assign(fallback, {
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      fs: false,
+      crypto: false,
+      os: false,
+      stream: false,
+      assert: false
+    })
+    config.resolve.fallback = fallback
+
     return config
   }
 }
