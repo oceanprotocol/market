b7e3027e97f3c50b4dbeb1dedee707718b9547ba
Matthias Kretschmann <m@kretschmann.io>
2022-05-10 13:51:22 +0100

    try original storyshots initialization

--- .storybook/storyshots.test.tsx
+++ .storybook/storyshots.test.tsx
@@ -8,17 +8,17 @@ const reactTestingLibrarySerializer = {
 
 // Stories are render-tested with @testing-library/react,
 // overwriting default snapshot testing behavior
-initStoryshots({
-  renderer: render,
-  snapshotSerializers: [reactTestingLibrarySerializer]
-})
-
 // initStoryshots({
-//   asyncJest: true,
-//   test: async ({ story, done }) => {
-//     const storyElement = story.render()
-//     // render the story with @testing-library/react
-//     render(storyElement)
-//     await waitFor(() => done())
-//   }
+//   renderer: render,
+//   snapshotSerializers: [reactTestingLibrarySerializer]
 // })
+
+initStoryshots({
+  asyncJest: true,
+  test: async ({ story, done }) => {
+    const storyElement = story.render()
+    // render the story with @testing-library/react
+    render(storyElement)
+    await waitFor(() => done())
+  }
+})
