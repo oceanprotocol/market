c854116cda40d58c5d375de7efc8732eb04e310e
ClaudiaHolhos <claudia@oceanprotocol.com>
2022-05-16 18:19:48 +0300

    Modal show story fix

--- src/components/@shared/atoms/Modal/index.tsx
+++ src/components/@shared/atoms/Modal/index.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement, ReactNode } from 'react'
 import ReactModal from 'react-modal'
 import styles from './index.module.css'
 
-if (process.env.NODE_ENV !== 'test') ReactModal.setAppElement('#__next')
+if (process.env.NODE_ENV !== 'test') ReactModal.setAppElement('#root')
 
 export interface ModalProps extends ReactModal.Props {
   title: string
