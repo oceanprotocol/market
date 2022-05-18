5f65260da1cec2268a5114f402e7744793d8ccc8
mihaisc <mihai@oceanprotocol.com>
2022-05-18 17:46:37 +0300

    fixes

--- src/components/Asset/AssetActions/Pool/Remove/index.tsx
+++ src/components/Asset/AssetActions/Pool/Remove/index.tsx
@@ -87,6 +87,7 @@ export default function Remove({
 
   const getValues = useRef(
     debounce(async (poolInstance, id, poolInfo, newAmountPoolShares) => {
+      if (newAmountPoolShares === '0') return
       const newAmountOcean = await poolInstance.calcSingleOutGivenPoolIn(
         id,
         poolInfo.baseTokenAddress,
