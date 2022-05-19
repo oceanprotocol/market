085c69371d0d0f4b721933f5cb4b17e0d832f476
Matthias Kretschmann <m@kretschmann.io>
2022-05-19 11:29:22 +0100

    table stories

--- src/components/@shared/atoms/Table/index.stories.tsx
+++ src/components/@shared/atoms/Table/index.stories.tsx
@@ -10,110 +10,103 @@ export default {
 } as ComponentMeta<typeof Table>
 
 const Template: ComponentStory<typeof Table> = (args) => <Table {...args} />
-//   const { chainIds } = useUserPreferences()
-//   const columns = [
-//     {
-//       name: 'Column 1 ',
-//       maxWidth: '45rem',
-//       grow: 1
-//     },
-//     {
-//       name: 'Column 2',
-//       maxWidth: '10rem'
-//     },
-//     {
-//       name: 'Column 3',
-//       right: true
-//     }
-//   ]
-//   const data: any[] = []
-
-//   return (
-//     <DataTable
-//       {...args}
-//       columns={columns}
-//       data={data}
-//       noDataComponent={<Empty message={'empty table'} />}
-//     />
-//   )
 
 interface Props {
   args: TableProps
 }
 
-export const Default: Props = Template.bind({})
-Default.args = {
-  columns: [
-    {
-      name: 'Data Set',
-      selector: function getAssetRow(row: AssetExtended) {
-        const { metadata } = row
-        return metadata.name
-      },
-      maxWidth: '45rem',
-      grow: 1
+const columns = [
+  {
+    name: 'Data Set',
+    selector: function getAssetRow(row: AssetExtended) {
+      const { metadata } = row
+      return metadata.name
     },
-    {
-      name: 'Datatoken Symbol',
-      selector: function getAssetRow(row: AssetExtended) {
-        return (
-          <Tooltip content={row.datatokens[0].name}>
-            {row.datatokens[0].symbol}
-          </Tooltip>
-        )
-      },
-      maxWidth: '10rem'
+    maxWidth: '45rem',
+    grow: 1
+  },
+  {
+    name: 'Datatoken Symbol',
+    selector: function getAssetRow(row: AssetExtended) {
+      return (
+        <Tooltip content={row.datatokens[0].name}>
+          {row.datatokens[0].symbol}
+        </Tooltip>
+      )
     },
-    {
-      name: 'Price',
-      selector: function getAssetRow(row: AssetExtended) {
-        return row.accessDetails.price
-      },
-      right: true
+    maxWidth: '10rem'
+  },
+  {
+    name: 'Price',
+    selector: function getAssetRow(row: AssetExtended) {
+      return row.accessDetails.price
+    },
+    right: true
+  }
+]
+
+const data = [
+  {
+    datatokens: [
+      {
+        name: 'DataAsset Token',
+        symbol: 'DATA-70'
+      }
+    ],
+    metadata: {
+      name: 'Title asset'
+    },
+    accessDetails: {
+      price: '1.011'
     }
-  ],
-  data: [
-    {
-      datatokens: [
-        {
-          name: 'DataAsset Token',
-          symbol: 'DATA-70'
-        }
-      ],
-      metadata: {
-        name: 'Title asset'
-      },
-      accessDetails: {
-        price: '1.011'
+  },
+  {
+    datatokens: [
+      {
+        name: 'DataAsset Token',
+        symbol: 'DATA-71'
       }
+    ],
+    metadata: {
+      name: 'Title asset'
     },
-    {
-      datatokens: [
-        {
-          name: 'DataAsset Token',
-          symbol: 'DATA-71'
-        }
-      ],
-      metadata: {
-        name: 'Title asset'
-      },
-      accessDetails: {
-        price: '2.011'
-      }
-    },
-    {
-      datatokens: [
-        {
-          name: 'DataAsset Token',
-          symbol: 'DATA-80'
-        }
-      ],
-      metadata: {
-        name: 'Title asset'
-      },
-      accessDetails: {
-        price: '3.011'
-      }
+    accessDetails: {
+      price: '2.011'
     }
-  ]
+  },
+  {
+    datatokens: [
+      {
+        name: 'DataAsset Token',
+        symbol: 'DATA-80'
+      }
+    ],
+    metadata: {
+      name: 'Title asset'
+    },
+    accessDetails: {
+      price: '3.011'
+    }
+  }
+]
+
+export const WithData: Props = Template.bind({})
+WithData.args = {
+  columns,
+  data
 }
+
+export const Loading: Props = Template.bind({})
+Loading.args = {
+  isLoading: true,
+  columns: [],
+  data: []
+}
+
+// TODO: refactor Table component so that Empty component does not use any hooks
+// export const Empty: Props = Template.bind({})
+// Empty.args = {
+//   emptyMessage: 'I am empty',
+//   columns: [],
+//   data: []
+// }
