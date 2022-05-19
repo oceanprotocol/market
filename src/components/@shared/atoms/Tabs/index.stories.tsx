0442f0f58676598a9160ce0e9dfd69c441f84e7e
Matthias Kretschmann <m@kretschmann.io>
2022-05-19 12:33:42 +0100

    simplify Tabs stories

--- src/components/@shared/atoms/Tabs/index.stories.tsx
+++ src/components/@shared/atoms/Tabs/index.stories.tsx
@@ -14,104 +14,34 @@ interface Props {
   args: TabsProps
 }
 
+const items = [
+  {
+    title: 'First tab',
+    content: 'this is the content for the first tab'
+  },
+  {
+    title: 'Second tab',
+    content: 'this is the content for the second tab'
+  },
+  {
+    title: 'Third tab',
+    content: 'this is the content for the third tab'
+  }
+]
+
 export const Default: Props = Template.bind({})
 Default.args = {
-  items: [
-    {
-      title: 'First tab',
-      content: (
-        <>
-          <a href="https://oceanprotocol.com/" target="_blank" rel="noreferrer">
-            this is the content for the first tab
-          </a>
-        </>
-      )
-    },
-    {
-      title: 'Second tab',
-      content: (
-        <>
-          <a>this is the content for the second tab</a>
-        </>
-      )
-    },
-    {
-      title: 'Third tab',
-      content: (
-        <>
-          <a
-            href="https://docs.oceanprotocol.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            this is the content for the third tab
-          </a>
-        </>
-      )
-    }
-  ]
+  items
 }
 
 export const WithRadio: Props = Template.bind({})
 WithRadio.args = {
-  items: [
-    {
-      title: 'First tab',
-      content: (
-        <>
-          <a href="https://oceanprotocol.com/" target="_blank" rel="noreferrer">
-            this is the content for the first tab
-          </a>
-        </>
-      )
-    },
-    {
-      title: 'Second tab',
-      content: (
-        <>
-          <a>this is the content for the second tab</a>
-        </>
-      )
-    }
-  ],
+  items,
   showRadio: true
 }
 
 export const WithDefaultIndex: Props = Template.bind({})
 WithDefaultIndex.args = {
-  items: [
-    {
-      title: 'First tab',
-      content: (
-        <>
-          <a href="https://oceanprotocol.com/" target="_blank" rel="noreferrer">
-            this is the content for the first tab
-          </a>
-        </>
-      )
-    },
-    {
-      title: 'Second tab',
-      content: (
-        <>
-          <a>this is the content for the second tab</a>
-        </>
-      )
-    },
-    {
-      title: 'Third tab',
-      content: (
-        <>
-          <a
-            href="https://docs.oceanprotocol.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            this is the content for the third tab
-          </a>
-        </>
-      )
-    }
-  ],
+  items,
   defaultIndex: 1
 }
