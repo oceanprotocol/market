21e3a30562bfb66b8efeb39344fe821d35adc732
Luca Milanese <luca.milanese90@gmail.com>
2022-05-19 17:49:11 +0200

    feat: add v3 assets redirect

--- src/@utils/aquarius.ts
+++ src/@utils/aquarius.ts
@@ -2,7 +2,7 @@ import { Asset, LoggerInstance } from '@oceanprotocol/lib'
 import { AssetSelectionAsset } from '@shared/FormFields/AssetSelection'
 import axios, { CancelToken, AxiosResponse } from 'axios'
 import { OrdersData_orders as OrdersData } from '../@types/subgraph/OrdersData'
-import { metadataCacheUri } from '../../app.config'
+import { metadataCacheUri, v3MetadataCacheUri } from '../../app.config'
 import {
   SortDirectionOptions,
   SortTermOptions
@@ -114,10 +114,36 @@ export async function retrieveAsset(
       `${metadataCacheUri}/api/aquarius/assets/ddo/${did}`,
       { cancelToken }
     )
+    axios
+      .get(`${metadataCacheUri}/api/aquarius/assets/ddo/${did}`, {
+        cancelToken
+      })
+      .then((e) => console.log(e))
     if (!response || response.status !== 200 || !response.data) return
 
     const data = { ...response.data }
     return data
+  } catch (error) {
+    if (axios.isCancel(error)) {
+      LoggerInstance.log(error.message)
+    } else {
+      LoggerInstance.error(error.message)
+    }
+  }
+}
+
+export async function checkV3Asset(
+  did: string,
+  cancelToken: CancelToken
+): Promise<boolean> {
+  try {
+    const response: AxiosResponse<Asset> = await axios.get(
+      `${v3MetadataCacheUri}/api/v1/aquarius/assets/ddo/${did}`,
+      { cancelToken }
+    )
+    if (!response || response.status !== 200 || !response.data) return false
+
+    return true
   } catch (error) {
     if (axios.isCancel(error)) {
       LoggerInstance.log(error.message)
