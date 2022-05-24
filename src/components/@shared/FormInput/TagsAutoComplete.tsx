afd4169166f39c02a0cac380685aeefcbe066610
Luca Milanese <luca.milanese90@gmail.com>
2022-05-24 18:38:50 +0200

    feat: add initial styling to autocomplete tag component

--- src/components/@shared/FormInput/TagsAutoComplete.tsx
+++ src/components/@shared/FormInput/TagsAutoComplete.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect, useState } from 'react'
+import React, { CSSProperties, ReactElement, useEffect, useState } from 'react'
 import CreatableSelect from 'react-select/creatable'
 import { OnChangeValue } from 'react-select'
 import { useField } from 'formik'
@@ -6,6 +6,69 @@ import { InputProps } from '.'
 import { getTagsList } from '@utils/aquarius'
 import { useUserPreferences } from '@context/UserPreferences'
 import { useCancelToken } from '@hooks/useCancelToken'
+
+const customStyles = {
+  control: (
+    styles: CSSProperties,
+    {
+      isFocused,
+      isSelected
+    }: {
+      isFocused: boolean
+      isSelected: boolean
+    }
+  ) => ({
+    ...styles,
+    borderColor:
+      isFocused || isSelected
+        ? 'var(--font-color-text)'
+        : 'var(--border-color)',
+    borderRadius: 'var(--border-radius)',
+    boxShadow: 'none',
+    background: 'var(--background-content)',
+    fontSize: 'var(--font-size-base)',
+    fontFamily: 'var(--font-family-base)',
+    fontWeight: 'var(--font-weight-bold)',
+    '&:hover': {}
+  }),
+  indicatorSeparator: (styles: CSSProperties) => ({
+    ...styles,
+    backgroundColor: 'var(--border-color)'
+  }),
+  menu: (styles: CSSProperties) => ({
+    ...styles,
+    backgroundColor: 'var(--background-highlight)'
+  }),
+  option: (styles: CSSProperties) => ({
+    ...styles,
+    backgroundColor: 'var(--background-highlight)',
+    color: 'var(--font-color-heading)'
+  }),
+  multiValue: (styles: CSSProperties) => ({
+    ...styles,
+    backgroundColor: 'var(--background-highlight)'
+  }),
+  multiValueLabel: (styles: CSSProperties) => ({
+    ...styles,
+    color: 'var(--font-color-heading)'
+  }),
+  multiValueRemove: (styles: CSSProperties) => ({
+    ...styles,
+    ':hover': {}
+  }),
+  valueContainer: (styles: CSSProperties) => ({
+    ...styles,
+    color: 'var(--font-color-heading)'
+  }),
+  placeholder: (styles: CSSProperties) => ({
+    ...styles,
+    // margin: 0,
+    color: 'var(--color-secondary)',
+    fontWeight: 'var(--font-weight-base)',
+    transition: '0.2s ease-out',
+    opacity: 0.7
+  })
+}
 
 interface AutoCompleteOption {
   readonly value: string
@@ -16,9 +79,9 @@ export default function TagsAutoComplete({
   ...props
 }: InputProps): ReactElement {
   const { chainIds } = useUserPreferences()
+  const { name, placeholder } = props
   const [tagsList, setTagsList] = useState<AutoCompleteOption[]>()
-  const [field, meta, helpers] = useField(props.name)
-
+  const [field, meta, helpers] = useField(name)
   const newCancelToken = useCancelToken()
 
   useEffect(() => {
@@ -43,6 +106,8 @@ export default function TagsAutoComplete({
       isMulti
       onChange={(value: AutoCompleteOption[]) => handleChange(value)}
       options={tagsList}
+      placeholder={placeholder}
+      styles={customStyles}
     />
   )
 }
