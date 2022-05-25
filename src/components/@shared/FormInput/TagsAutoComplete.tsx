d8427b06704e907c2ef10c0fd03d8e86ce9b5d9f
Luca Milanese <luca.milanese90@gmail.com>
2022-05-25 16:47:49 +0200

    feat: move styling elements to module.css file

--- src/components/@shared/FormInput/TagsAutoComplete.tsx
+++ src/components/@shared/FormInput/TagsAutoComplete.tsx
@@ -1,66 +1,12 @@
 import React, { ReactElement, useEffect, useState } from 'react'
 import CreatableSelect from 'react-select/creatable'
-import { OnChangeValue, StylesConfig } from 'react-select'
+import { OnChangeValue } from 'react-select'
 import { useField } from 'formik'
 import { InputProps } from '.'
 import { getTagsList } from '@utils/aquarius'
 import { useUserPreferences } from '@context/UserPreferences'
 import { useCancelToken } from '@hooks/useCancelToken'
-
-const customStyles: StylesConfig<AutoCompleteOption, true> = {
-  control: (styles, { isFocused }) => ({
-    ...styles,
-    borderColor: isFocused ? 'var(--font-color-text)' : 'var(--border-color)',
-    borderRadius: 'var(--border-radius)',
-    boxShadow: 'none',
-    background: 'var(--background-content)',
-    fontSize: 'var(--font-size-base)',
-    fontFamily: 'var(--font-family-base)',
-    fontWeight: 'var(--font-weight-bold)',
-    transition: '0.2s ease-in-out',
-    '&:hover': {}
-  }),
-  indicatorSeparator: (styles) => ({
-    ...styles,
-    backgroundColor: 'var(--border-color)'
-  }),
-  menu: (styles) => ({
-    ...styles,
-    backgroundColor: 'var(--background-highlight)'
-  }),
-  option: (styles) => ({
-    ...styles,
-    backgroundColor: 'var(--background-highlight)',
-    color: 'var(--font-color-heading)'
-  }),
-  multiValue: (styles) => ({
-    ...styles,
-    backgroundColor: 'var(--background-highlight)'
-  }),
-  multiValueLabel: (styles) => ({
-    ...styles,
-    color: 'var(--font-color-heading)',
-    fontSize: 'var(--font-size-base)',
-    fontFamily: 'var(--font-family-base)',
-    fontWeight: 'var(--font-weight-bold)'
-  }),
-  multiValueRemove: (styles) => ({
-    ...styles,
-    ':hover': {}
-  }),
-  valueContainer: (styles) => ({
-    ...styles,
-    color: 'var(--font-color-heading)'
-  }),
-  placeholder: (styles) => ({
-    ...styles,
-    // margin: 0,
-    color: 'var(--color-secondary)',
-    fontWeight: 'var(--font-weight-base)',
-    transition: '0.2s ease-out',
-    opacity: 0.7
-  })
-}
+import styles from './TagsAutoComplete.module.css'
 
 interface AutoCompleteOption {
   readonly value: string
@@ -95,11 +41,11 @@ export default function TagsAutoComplete({
 
   return (
     <CreatableSelect
+      className={styles.select}
       isMulti
       onChange={(value: AutoCompleteOption[]) => handleChange(value)}
       options={tagsList}
       placeholder={placeholder}
-      styles={customStyles}
     />
   )
 }
