6dfeead4fd99f8d4a067abb6bd7a3fbb771866b4
mihaisc <mihai.scarlat@smartcontrol.ro>
2022-06-08 01:46:46 -0700

    Release (#1492)
    * update contracts
    
    * add version selector
    
    * show banner on all pages
    
    * show banner on all pages
    
    * Announcement/warning messages cleanup
    
    * new main announcement message
    * contextual compute-to-data warnings in publish form & asset action
    
    * copy update
    
    * change hardcoded URLs
    
    * change localStorage key
    
    * to prevent user preferences clashes when switching between v3 & v4
    
    * footer stats note changes
    
    * move copy into content file
    * mention external markets
    
    * footer stats note copy changes
    
    * modified pool widget footer
    
    * take out pool reserve display
    * style & spacing updates for fees
    
    Co-authored-by: Matthias Kretschmann <m@kretschmann.io>

--- app.config.js
+++ app.config.js
@@ -19,10 +19,10 @@ module.exports = {
 
   // List of chainIds which metadata cache queries will return by default.
   // This preselects the Chains user preferences.
-  chainIds: [3, 4, 80001, 1287],
+  chainIds: [1, 137, 56, 246, 1285],
 
   // List of all supported chainIds. Used to populate the Chains user preferences list.
-  chainIdsSupported: [3, 4, 80001, 1287],
+  chainIdsSupported: [1, 137, 56, 246, 1285, 3, 4, 80001, 1287],
 
   infuraProjectId: process.env.NEXT_PUBLIC_INFURA_PROJECT_ID || 'xxx',
 
