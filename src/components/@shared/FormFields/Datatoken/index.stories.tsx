ed9177d1d91c789dfeb39324687ddf0ffacbd024
Enzo Vezzaro <enzo-vezzaro@live.it>
2022-06-09 12:29:20 -0400

    added mocked form to datatoken (WIP)

--- src/components/@shared/FormFields/Datatoken/index.stories.tsx
+++ src/components/@shared/FormFields/Datatoken/index.stories.tsx
@@ -2,14 +2,40 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import Datatoken from '@shared/FormFields/Datatoken'
 import { InputProps } from '@shared/FormInput'
+import { withFormik } from 'formik'
 
 export default {
   title: 'Component/@shared/FormFields/Datatoken',
   component: Datatoken
 } as ComponentMeta<typeof Datatoken>
 
+const MockedForm = withFormik({
+  mapPropsToValues: () => ({
+    name: 'OCEAN',
+    symbol: 'OCEAN'
+  }),
+  validate: (values) => {
+    const errors = {
+      name: ''
+    }
+
+    if (!values.name) {
+      errors.name = 'Required'
+    }
+
+    return errors
+  },
+
+  handleSubmit: (values, { setSubmitting }) => {
+    console.log(JSON.stringify(values, null, 2))
+    setSubmitting(false)
+  },
+
+  displayName: 'Datatoken'
+})(Datatoken)
+
 const Template: ComponentStory<typeof Datatoken> = (args: InputProps) => (
-  <Datatoken {...args} />
+  <MockedForm {...args} />
 )
 
 interface Props {
@@ -18,5 +44,6 @@ interface Props {
 
 export const Default: Props = Template.bind({})
 Default.args = {
-  name: 'PARCOUR-73'
+  name: 'PARCOUR-73',
+  symbol: 'PARCOUR-73'
 }
