a986ecc3fc09bba7ef31a6885b5815370e7d1c65
EnzoVezzaro <enzo-vezzaro@live.it>
2022-06-14 08:21:56 -0400

    fix alignment based button on action type (#1491)
    * fix alignment based on action type
    
    * moving to CSS modules

--- src/components/@shared/ButtonBuy/index.tsx
+++ src/components/@shared/ButtonBuy/index.tsx
@@ -161,7 +161,7 @@ export default function ButtonBuy({
             type={type}
             onClick={onClick}
             disabled={disabled}
-            className="center"
+            className={action === 'compute' ? styles.actionsCenter : ''}
           >
             {buttonText}
           </Button>
