3c03198da9f421c45f3d62f126d059a2adf3206a
Matthias Kretschmann <m@kretschmann.io>
2022-06-27 10:19:38 +0100

    remove reconnecting message

--- src/components/Header/Wallet/Account.tsx
+++ src/components/Header/Wallet/Account.tsx
@@ -21,7 +21,7 @@ const Account = React.forwardRef((props, ref: any) => {
   return !accountId && web3Modal?.cachedProvider ? (
     // Improve user experience for cached provider when connecting takes some time
     <button className={styles.button} onClick={(e) => e.preventDefault()}>
-      <Loader message="Reconnecting..." />
+      <Loader />
     </button>
   ) : accountId ? (
     <button
