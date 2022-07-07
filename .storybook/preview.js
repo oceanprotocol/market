a51814a1f53832ae3e2a41e5d8ba88294a8b3355
EnzoVezzaro <enzo-vezzaro@live.it>
2022-07-07 10:23:38 -0400

    added dark mode toggle button with switch class

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,17 @@
 import '@oceanprotocol/typographies/css/ocean-typo.css'
 import '../src/stylesGlobal/styles.css'
+import { ThemeProvider } from 'styled-components'
 
-export const parameters = {
+export const globalTypes = {
+  theme: {
+    name: 'Ocean Market',
+    description: 'Global theme for Ocean Market',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: ['light', 'dark']
+    }
+  },
   backgrounds: {
     default: 'light',
     values: [
@@ -17,3 +27,19 @@ export const parameters = {
     }
   }
 }
+
+const withThemeProvider = (Story, context) => {
+  const theme = context.globals.theme
+
+  var storyBody = document.getElementById('story-body')
+  theme === 'dark'
+    ? storyBody.classList.add('dark')
+    : storyBody.classList.remove('dark')
+
+  return (
+    <ThemeProvider theme={globalTypes}>
+      <Story {...context} />
+    </ThemeProvider>
+  )
+}
+export const decorators = [withThemeProvider]
