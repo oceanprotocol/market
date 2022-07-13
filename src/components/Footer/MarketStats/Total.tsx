787a1278e857743a62a74c76197642dbf64bdc25
mihaisc <mihai.scarlat@smartcontrol.ro>
2022-07-13 14:57:53 +0300

    remove tvl from footr

--- src/components/Footer/MarketStats/Total.tsx
+++ src/components/Footer/MarketStats/Total.tsx
@@ -1,6 +1,4 @@
 import React, { ReactElement } from 'react'
-import Conversion from '@shared/Price/Conversion'
-import PriceUnit from '@shared/Price/PriceUnit'
 import { StatsTotal } from './_types'
 
 export default function MarketStatsTotal({
@@ -15,18 +13,6 @@ export default function MarketStatsTotal({
         <strong>{total.nfts}</strong> assets with{' '}
         <strong>{total.datatokens}</strong> different datatokens.
       </p>
-      <Conversion
-        price={`${total.totalValueLockedInOcean}`}
-        hideApproximateSymbol
-      />{' '}
-      <abbr title="Total Value Locked">TVL</abbr> across{' '}
-      <strong>{total.pools}</strong> asset pools that contain{' '}
-      <PriceUnit
-        price={`${total.totalOceanLiquidity}`}
-        symbol="OCEAN"
-        size="small"
-      />
-      , plus datatokens for each pool.
     </>
   )
 }
