fd4cb581f7464f085569df15ee80265a2dad1436
EnzoVezzaro <enzo-vezzaro@live.it>
2022-07-14 07:13:13 -0400

    added markdownToolbar toggle

--- src/components/@shared/FormInput/InputMarkdown.tsx
+++ src/components/@shared/FormInput/InputMarkdown.tsx
@@ -5,30 +5,34 @@ import styles from './InputMarkdown.module.css'
 interface InputMarkdownProps {
   name: string
   children: ReactNode
+  markdownToolbar?: boolean
 }
 
 export default function InputMarkdown({
   name,
-  children
+  children,
+  markdownToolbar
 }: InputMarkdownProps): ReactElement {
   return (
     <>
-      <div className={styles.markdownToolbarContainer}>
-        <markdown-toolbar for={name} class={styles.markdownToolbar}>
-          <md-bold>bold</md-bold>
-          <md-header>header</md-header>
-          <md-italic>italic</md-italic>
-          <md-quote>quote</md-quote>
-          <md-code>code</md-code>
-          <md-link>link</md-link>
-          <md-image>image</md-image>
-          <md-unordered-list>unordered-list</md-unordered-list>
-          <md-ordered-list>ordered-list</md-ordered-list>
-          <md-task-list>task-list</md-task-list>
-          <md-mention>mention</md-mention>
-          <md-ref>ref</md-ref>
-        </markdown-toolbar>
-      </div>
+      {markdownToolbar && (
+        <div className={styles.markdownToolbarContainer}>
+          <markdown-toolbar for={name} class={styles.markdownToolbar}>
+            <md-bold>bold</md-bold>
+            <md-header>header</md-header>
+            <md-italic>italic</md-italic>
+            <md-quote>quote</md-quote>
+            <md-code>code</md-code>
+            <md-link>link</md-link>
+            <md-image>image</md-image>
+            <md-unordered-list>unordered-list</md-unordered-list>
+            <md-ordered-list>ordered-list</md-ordered-list>
+            <md-task-list>task-list</md-task-list>
+            <md-mention>mention</md-mention>
+            <md-ref>ref</md-ref>
+          </markdown-toolbar>
+        </div>
+      )}
       {children}
     </>
   )
