2211eecf7e3fdf3ec86a423a3813f39d9fda7958
EnzoVezzaro <enzo-vezzaro@live.it>
2022-08-19 08:55:00 -0400

    added commands and extra commands

--- src/components/@shared/FormInput/InputMarkdown.tsx
+++ src/components/@shared/FormInput/InputMarkdown.tsx
@@ -3,6 +3,32 @@ import { useFormikContext } from 'formik'
 import { FormPublishData } from '../../Publish/_types'
 import dynamic from 'next/dynamic'
 import styles from './InputMarkdown.module.css'
+import {
+  bold,
+  code,
+  codeBlock,
+  italic,
+  link,
+  unorderedListCommand,
+  orderedListCommand,
+  checkedListCommand,
+  quote,
+  hr,
+  title,
+  title1,
+  title2,
+  title3,
+  title4,
+  title5,
+  title6,
+  divider,
+  codeLive,
+  fullscreen,
+  image,
+  strikethrough,
+  codeEdit,
+  codePreview
+} from '@uiw/react-md-editor/lib/commands'
 
 import '@uiw/react-md-editor/markdown-editor.css'
 import '@uiw/react-markdown-preview/markdown.css'
@@ -25,7 +51,33 @@ const InputMarkdown = ({ ...props }): ReactElement => {
           ...props,
           className: styles.inputMarkdownContent
         }}
+        preview={'edit'}
         onChange={setValue}
+        commands={[
+          bold,
+          code,
+          codeBlock,
+          italic,
+          link,
+          unorderedListCommand,
+          orderedListCommand,
+          checkedListCommand,
+          quote,
+          hr,
+          title,
+          title1,
+          title2,
+          title3,
+          title4,
+          title5,
+          title6,
+          divider,
+          codeLive,
+          fullscreen,
+          image,
+          strikethrough
+        ]}
+        extraCommands={[codeEdit, codePreview]}
       />
     </div>
   )
