9648a74a520889da6093e52707c0e72f523a2657
Matthias Kretschmann <m@kretschmann.io>
2022-09-22 15:23:45 +0100

    test tweaks

--- src/components/@shared/Publisher/index.test.tsx
+++ src/components/@shared/Publisher/index.test.tsx
@@ -1,34 +1,56 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import * as axios from 'axios'
 import Publisher from './'
 
 const account = '0x0000000000000000000000000000000000000000'
 
-test('should return correct markup by default', async () => {
-  render(<Publisher account={account} />)
+jest.mock('axios')
 
-  const element = await screen.findByRole('link')
-  expect(element).toContainHTML('<a')
-  expect(element).toHaveAttribute('href', `/profile/${account}`)
-})
+describe('Publisher', () => {
+  test('should return correct markup by default', async () => {
+    ;(axios as any).get.mockImplementationOnce(() =>
+      Promise.resolve({ data: { name: 'jellymcjellyfish.eth' } })
+    )
 
-test('should truncate account by default', async () => {
-  render(<Publisher account={account} />)
+    render(<Publisher account={account} />)
 
-  const element = await screen.findByText('0x…00000000')
-  expect(element).toBeInTheDocument()
-})
+    const element = await screen.findByRole('link')
+    expect(element).toBeInTheDocument()
+    expect(element).toContainHTML('<a')
+    expect(element).toHaveAttribute('href', `/profile/${account}`)
+  })
 
-test('should return correct markup in minimal state', async () => {
-  render(<Publisher minimal account={account} />)
+  test('should truncate account by default', async () => {
+    ;(axios as any).get.mockImplementationOnce(() =>
+      Promise.resolve({ data: { name: null } })
+    )
 
-  const element = await screen.findByText('0x…00000000')
-  expect(element).not.toHaveAttribute('href')
-})
+    render(<Publisher account={account} />)
 
-test('should return markup with empty account', async () => {
-  render(<Publisher account={null} />)
+    const element = await screen.findByText('0x…00000000')
+    expect(element).toBeInTheDocument()
+  })
 
-  const element = await screen.findByRole('link')
-  expect(element).toBeInTheDocument()
+  test('should return correct markup in minimal state', async () => {
+    ;(axios as any).get.mockImplementationOnce(() =>
+      Promise.resolve({ data: { name: null } })
+    )
+
+    render(<Publisher minimal account={account} />)
+
+    const element = await screen.findByText('0x…00000000')
+    expect(element).not.toHaveAttribute('href')
+  })
+
+  test('should return markup with empty account', async () => {
+    ;(axios as any).get.mockImplementationOnce(() =>
+      Promise.resolve({ data: { name: null } })
+    )
+
+    render(<Publisher account={null} />)
+
+    const element = await screen.findByRole('link')
+    expect(element).toBeInTheDocument()
+  })
 })
