e4601051ce5795d6b643a14b21ea4e5844b179cf
Matthias Kretschmann <m@kretschmann.io>
2022-09-23 11:07:59 +0100

    put back Rinkeby & Ropsten to supported networks

--- src/@context/UserPreferences.tsx
+++ src/@context/UserPreferences.tsx
@@ -135,11 +135,11 @@ function UserPreferencesProvider({
 
   // chainIds old data migration
   // remove deprecated networks from user-saved chainIds
-  useEffect(() => {
-    if (!chainIds.includes(3) || !chainIds.includes(4)) return
-    const newChainIds = chainIds.filter((id) => id !== 3 && id !== 4)
-    setChainIds(newChainIds)
-  }, [chainIds])
+  // useEffect(() => {
+  //   if (!chainIds.includes(3) || !chainIds.includes(4)) return
+  //   const newChainIds = chainIds.filter((id) => id !== 3 && id !== 4)
+  //   setChainIds(newChainIds)
+  // }, [chainIds])
 
   return (
     <UserPreferencesContext.Provider
