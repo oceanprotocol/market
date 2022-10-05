5083ceeb527b50e7156d1dc4da5e03bf91f2dfc5
Nuary Pradipta <nuary.pradipta@gmail.com>
2022-10-05 17:01:00 +0700

    Add chatbox emoji

--- src/components/@shared/Orbis/FloatingChat/ChatToolbar.tsx
+++ src/components/@shared/Orbis/FloatingChat/ChatToolbar.tsx
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import styles from './ChatToolbar.module.css'
 import SendIcon from '@images/send.svg'
+import EmojiIcon from '@images/emoji.svg'
 import Input from '@shared/FormInput'
 import { useOrbis } from '@context/Orbis'
+import dynamic from 'next/dynamic'
+import EmojiPicker, {
+  EmojiClickData,
+  EmojiStyle,
+  Theme
+} from 'emoji-picker-react'
 
 export default function ChatToolbar() {
   const { orbis, conversationId } = useOrbis()
-  const [content, setContent] = useState()
+  const [content, setContent] = useState<string>('')
+  const [showPicker, setShowPicker] = useState(false)
+
+  const Picker = dynamic(
+    () => {
+      return import('emoji-picker-react')
+    },
+    { ssr: false }
+  )
+
+  const onEmojiClick = (emojiData: EmojiClickData) => {
+    setContent((prevInput) => prevInput + emojiData.emoji)
+    setShowPicker(false)
+  }
 
   useEffect(() => {
     console.log(content)
@@ -26,17 +46,39 @@ export default function ChatToolbar() {
   return (
     <div className={styles.chatToolbar}>
       <div className={styles.chatMessage}>
-        <Input
-          type="input"
-          name="message"
-          size="small"
-          placeholder="Type Message"
-          value={content}
-          onInput={(e) => setContent(e.target.value)}
-        />
+        <div className={styles.inputWrap}>
+          <Input
+            type="input"
+            name="message"
+            size="small"
+            placeholder="Type Message"
+            value={content}
+            onInput={(e) => setContent(e.target.value)}
+          />
+          <button
+            className={styles.button}
+            type="button"
+            onClick={() => setShowPicker((val) => !val)}
+          >
+            <EmojiIcon className={styles.buttonIcon} />
+          </button>
+        </div>
         <button className={styles.button} onClick={sendMessage}>
-          <SendIcon className={styles.sendIcon} />
+          <SendIcon className={styles.buttonIcon} />
         </button>
+      </div>
+      <div className={styles.pickerBox}>
+        {showPicker && (
+          <Picker
+            onEmojiClick={onEmojiClick}
+            theme={Theme.DARK}
+            emojiStyle={EmojiStyle.NATIVE}
+            autoFocusSearch={false}
+            lazyLoadEmojis={true}
+            width={324}
+            height={400}
+          />
+        )}
       </div>
     </div>
   )
