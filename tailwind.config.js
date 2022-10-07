bb733d85520637a722127586ef0f51fd4d0da7aa
ykhuda <pavel.khodakovsky1996@gmail.com>
2022-10-07 12:46:09 +0300

    Implemented Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,42 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
-const plugin = require("tailwindcss/plugin");
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  darkMode: "class",
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans]
       },
       colors: {
-        current: "currentColor",
+        current: 'currentColor',
         green: colors.emerald,
         yellow: colors.amber,
         purple: colors.violet,
-        gray: colors.neutral,
-      },
-    },
+        gray: colors.neutral
+      }
+    }
   },
   plugins: [
-    require("@tailwindcss/forms")({
-      strategy: "class",
+    require('@tailwindcss/forms')({
+      strategy: 'class'
     }),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio"),
-    plugin(function ({addVariant, e}) {
-      addVariant("collapsed", ({modifySelectors, separator}) => {
-        modifySelectors(({className}) => {
-          return `.collapsed .${e(`collapsed${separator}${className}`)}`;
-        });
-      });
-      addVariant("rtl", ({modifySelectors, separator}) => {
-        modifySelectors(({className}) => {
-          return `.rtl .${e(`rtl${separator}${className}`)}`;
-        });
-      });
-    }),
-  ],
-};
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    plugin(function ({ addVariant, e }) {
+      addVariant('collapsed', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.collapsed .${e(`collapsed${separator}${className}`)}`
+        })
+      })
+      addVariant('rtl', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.rtl .${e(`rtl${separator}${className}`)}`
+        })
+      })
+    })
+  ]
+}
