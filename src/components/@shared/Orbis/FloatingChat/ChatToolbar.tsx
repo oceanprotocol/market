6ee1de9ffa3694a619d6ad547c44a1028489e284
Nuary Pradipta <nuary.pradipta@gmail.com>
2022-10-10 15:23:54 +0700

    Update Emoji Picker

--- src/components/@shared/Orbis/FloatingChat/ChatToolbar.tsx
+++ src/components/@shared/Orbis/FloatingChat/ChatToolbar.tsx
@@ -1,39 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import styles from './ChatToolbar.module.css'
 import SendIcon from '@images/send.svg'
-import EmojiIcon from '@images/emoji.svg'
 import Input from '@shared/FormInput'
 import { useOrbis } from '@context/Orbis'
-// import dynamic from 'next/dynamic'
-import Picker, { EmojiClickData, EmojiStyle, Theme } from 'emoji-picker-react'
+import EmojiPicker from './EmojiPicker'
+import { EmojiClickData } from 'emoji-picker-react'
 
 export default function ChatToolbar({
   callbackMessage
 }: {
   callbackMessage: (post: OrbisPostInterface) => void
 }) {
-  const { orbis, conversationId, account, convOpen } = useOrbis()
+  const { orbis, conversationId, account } = useOrbis()
   const [content, setContent] = useState<string>('')
-  const [showPicker, setShowPicker] = useState(false)
-
-  // const Picker = dynamic(
-  //   () => {
-  //     return import('emoji-picker-react')
-  //   },
-  //   { ssr: false }
-  // )
 
   const onEmojiClick = (emojiData: EmojiClickData) => {
     setContent((prevInput) => prevInput + emojiData.emoji)
-    setShowPicker(false)
   }
-
-  useEffect(() => {
-    console.log(convOpen)
-    if (!convOpen) {
-      setShowPicker(false)
-    }
-  }, [convOpen])
 
   const sendMessage = async () => {
     const _callbackMessage: OrbisPostInterface = {
@@ -72,32 +55,13 @@ export default function ChatToolbar({
             size="small"
             placeholder="Type Message"
             value={content}
-            onInput={(e) => setContent(e.target.value)}
+            onChange={(e: any) => setContent(e.target.value)}
           />
-          <button
-            className={styles.button}
-            type="button"
-            onClick={() => setShowPicker((val) => !val)}
-          >
-            <EmojiIcon className={styles.buttonIcon} />
-          </button>
+          <EmojiPicker onEmojiClick={onEmojiClick} />
         </div>
         <button className={styles.button} onClick={sendMessage}>
           <SendIcon className={styles.buttonIcon} />
         </button>
-      </div>
-      <div className={styles.pickerBox}>
-        {showPicker && (
-          <Picker
-            onEmojiClick={onEmojiClick}
-            theme={Theme.DARK}
-            emojiStyle={EmojiStyle.APPLE}
-            autoFocusSearch={false}
-            lazyLoadEmojis={true}
-            width={324}
-            height={400}
-          />
-        )}
       </div>
     </div>
   )
---
+++ src/components/@shared/Orbis/FloatingChat/EmojiPicker.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import EmojiIcon from '@images/emoji.svg'
+import Picker, { EmojiStyle, Theme } from 'emoji-picker-react'
+import styles from './EmojiPicker.module.css'
+import useDarkMode from '@oceanprotocol/use-dark-mode'
+import { useMarketMetadata } from '@context/MarketMetadata'
+import Tooltip from '@shared/atoms/Tooltip'
+
+export default function EmojiPicker({ onEmojiClick }: { onEmojiClick: any }) {
+  const { appConfig } = useMarketMetadata()
+  const darkMode = useDarkMode(false, appConfig?.darkModeConfig)
+
+  return (
+    <Tooltip
+      content={
+        <Picker
+          onEmojiClick={onEmojiClick}
+          theme={darkMode.value ? Theme.DARK : Theme.LIGHT}
+          emojiStyle={EmojiStyle.NATIVE}
+          autoFocusSearch={false}
+          lazyLoadEmojis={true}
+          width={322}
+          height={399}
+        />
+      }
+      trigger="click focus"
+      zIndex={21}
+      placement={'top-end'}
+      className={styles.emojiToolTip}
+    >
+      <>
+        <EmojiIcon className={styles.icon} />
+      </>
+    </Tooltip>
+  )
+}
