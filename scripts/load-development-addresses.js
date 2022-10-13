b017988345105ca333ddf07419f1ccfe8bc3442a
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2022-10-13 16:38:00 +0300

    update env vars from script

--- scripts/load-development-addresses.js
+++ scripts/load-development-addresses.js
@@ -19,18 +19,20 @@ fs.readFileSync('.env', 'utf8', function (err, data) {
 })
 
 const addresses = getLocalAddresses()
-console.log('addresses', addresses)
 const envVars = []
-envVars.push(`NFT_FACTORY_ADDRESS='${addresses.ERC721Factory}'`)
+envVars.push(`NEXT_PUBLIC_NFT_FACTORY_ADDRESS='${addresses.ERC721Factory}'`)
 envVars.push(
-  `OPF_COMMUNITY_FEE_COLECTOR='${addresses.OPFCommunityFeeCollector}'`
+  `NEXT_PUBLIC_OPF_COMMUNITY_FEE_COLECTOR='${addresses.OPFCommunityFeeCollector}'`
 )
-envVars.push(`FIXED_RATE_EXCHANGE_ADDRESS='${addresses.FixedPrice}'`)
-envVars.push(`DISPENSER_ADDRESS='${addresses.Dispenser}'`)
-envVars.push(`OCEAN_TOKEN_ADDRESS='${addresses.Ocean}'`)
+envVars.push(
+  `NEXT_PUBLIC_FIXED_RATE_EXCHANGE_ADDRESS='${addresses.FixedPrice}'`
+)
+envVars.push(`NEXT_PUBLIC_DISPENSER_ADDRESS='${addresses.Dispenser}'`)
+envVars.push(`NEXT_PUBLIC_OCEAN_TOKEN_ADDRESS='${addresses.Ocean}'`)
 envVars.push(`NEXT_PUBLIC_METADATACACHE_URI='http://localhost:5000'`)
-envVars.push(`NEXT_PUBLIC_SUBGRAPH_URI='$http://localhost:9000'`)
+envVars.push(`NEXT_PUBLIC_SUBGRAPH_URI='http://localhost:9000'`)
 envVars.push(`NEXT_RBAC_URL='http://localhost:3000'`)
+envVars.push(`NEXT_PUBLIC_MARKET_DEVELOPMENT='true'`)
 
 var stream = fs.createWriteStream('.env', { flags: 'a' })
 
