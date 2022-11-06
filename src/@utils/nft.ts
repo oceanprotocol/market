739685cf146c72c3b6fb91e59905dfba0962d65d
Shadman <shadow.xtinct@gmail.com>
2022-11-06 04:12:26 +0000

    update copy

--- src/@utils/nft.ts
+++ src/@utils/nft.ts
@@ -50,9 +50,9 @@ export function generateNftMetadata(): NftMetadata {
   const imageData = `data:image/svg+xml,${encodeSvg(svg.outerHTML)}`
 
   const newNft: NftMetadata = {
-    name: 'Ocean Data NFT',
-    symbol: 'OCEAN-NFT',
-    description: `This NFT represents an asset in the Ocean Protocol v4 ecosystem.`,
+    name: 'Data NFT',
+    symbol: 'DATA-NFT',
+    description: `This NFT represents an asset in the DataPlace Protocol v4 ecosystem.`,
     external_url: 'https://market.oceanprotocol.com',
     background_color: '141414', // dark background
     image_data: imageData
