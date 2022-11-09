04fdb1c119cfa87bec1990a6a738d3fe9e99ccb4
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-11-09 17:43:49 +0300

    Additional tests for BuyButton

--- src/components/Asset/AssetActions/ButtonBuy/index.test.tsx
+++ src/components/Asset/AssetActions/ButtonBuy/index.test.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import ButtonBuy from './'
 
-test('Renders download button without crashing', async () => {
+//  TESTS FOR DOWNLOAD
+test('Renders Buy button without crashing', async () => {
   render(
     <ButtonBuy
       action="download"
@@ -16,7 +17,7 @@ test('Renders download button without crashing', async () => {
         console.log('TEST')
       }}
       assetTimeout="1 day"
-      assetType="Download"
+      assetType="Dataset"
       stepText=" "
       isLoading={false}
       priceType="fixed"
@@ -31,12 +32,12 @@ test('Renders download button without crashing', async () => {
   expect(button).toContainHTML('<button')
 })
 
-test('Renders compute button without crashing', async () => {
+test('Renders Buy button without crashing when hasPreviousOrder=true', async () => {
   render(
     <ButtonBuy
-      action="compute"
+      action="download"
       disabled={false}
-      hasPreviousOrder={false}
+      hasPreviousOrder={true}
       hasDatatoken={false}
       btSymbol="btSymbol"
       dtSymbol="dtSymbol"
@@ -45,7 +46,7 @@ test('Renders compute button without crashing', async () => {
         console.log('TEST')
       }}
       assetTimeout="1 day"
-      assetType="Download"
+      assetType="Dataset"
       stepText=" "
       isLoading={false}
       priceType="fixed"
@@ -56,7 +57,7 @@ test('Renders compute button without crashing', async () => {
     />
   )
 
-  const button = screen.getByText('Buy Compute Job')
+  const button = screen.getByText('Download')
   expect(button).toContainHTML('<button')
 })
 
@@ -74,13 +75,213 @@ test('Renders retry button for download without crashing', async () => {
         console.log('TEST')
       }}
       assetTimeout="1 day"
-      assetType="Download"
+      assetType="Dataset"
       stepText=" "
       isLoading={false}
       priceType="fixed"
       isConsumable={true}
       isBalanceSufficient={true}
       consumableFeedback="TEST: consumableFeedback"
+      retry={true}
+    />
+  )
+
+  const button = screen.getByText('Retry')
+  expect(button).toContainHTML('<button')
+})
+
+test('Renders download button for download without crashing', async () => {
+  render(
+    <ButtonBuy
+      action="download"
+      disabled={false}
+      hasPreviousOrder={true}
+      hasDatatoken={false}
+      btSymbol="btSymbol"
+      dtSymbol="dtSymbol"
+      dtBalance="100000000000"
+      onClick={() => {
+        console.log('TEST')
+      }}
+      assetTimeout="1 day"
+      assetType="Dataset"
+      stepText=" "
+      isLoading={false}
+      priceType="fixed"
+      isConsumable={false}
+      isBalanceSufficient={true}
+      consumableFeedback="TEST: consumableFeedback"
+      retry={false}
+    />
+  )
+
+  const button = screen.getByText('Download')
+  expect(button).toContainHTML('<button')
+})
+
+test('Renders get button for free download without crashing', async () => {
+  render(
+    <ButtonBuy
+      action="download"
+      disabled={false}
+      hasPreviousOrder={false}
+      hasDatatoken={false}
+      btSymbol="btSymbol"
+      dtSymbol="dtSymbol"
+      dtBalance="100000000000"
+      onClick={() => {
+        console.log('TEST')
+      }}
+      assetTimeout="1 day"
+      assetType="Dataset"
+      stepText=" "
+      isLoading={false}
+      priceType="free"
+      isConsumable={false}
+      isBalanceSufficient={true}
+      consumableFeedback="TEST: consumableFeedback"
+      retry={false}
+    />
+  )
+
+  const button = screen.getByText('Get')
+  expect(button).toContainHTML('<button')
+})
+
+// TESTS FOR COMPUTE
+
+test('Renders "Buy Compute Job" button for compute without crashing', async () => {
+  render(
+    <ButtonBuy
+      action="compute"
+      disabled={false}
+      hasPreviousOrder={false}
+      hasDatatoken={true}
+      btSymbol="btSymbol"
+      dtSymbol="dtSymbol"
+      dtBalance="100000000000"
+      assetTimeout="1 day"
+      assetType="algorithm"
+      hasPreviousOrderSelectedComputeAsset={false}
+      hasDatatokenSelectedComputeAsset={true}
+      dtSymbolSelectedComputeAsset="dtSymbol"
+      dtBalanceSelectedComputeAsset="dtBalance"
+      selectedComputeAssetType="selectedComputeAssetType"
+      stepText=" "
+      isLoading={false}
+      type="submit"
+      priceType="fixed"
+      algorithmPriceType="free"
+      isBalanceSufficient={true}
+      isConsumable={true}
+      consumableFeedback="consumableFeedback"
+      isAlgorithmConsumable={true}
+      hasProviderFee={false}
+      retry={false}
+    />
+  )
+
+  const button = screen.getByText('Buy Compute Job')
+  expect(button).toContainHTML('<button')
+})
+
+test('Renders "Start Compute Job" button', async () => {
+  render(
+    <ButtonBuy
+      action="compute"
+      disabled={false}
+      hasPreviousOrder={true}
+      hasDatatoken={true}
+      btSymbol="btSymbol"
+      dtSymbol="dtSymbol"
+      dtBalance="100000000000"
+      assetTimeout="1 day"
+      assetType="algorithm"
+      hasPreviousOrderSelectedComputeAsset={true}
+      hasDatatokenSelectedComputeAsset={true}
+      dtSymbolSelectedComputeAsset="dtSymbol"
+      dtBalanceSelectedComputeAsset="dtBalance"
+      selectedComputeAssetType="selectedComputeAssetType"
+      stepText=" "
+      isLoading={false}
+      type="submit"
+      priceType="fixed"
+      algorithmPriceType="free"
+      isBalanceSufficient={true}
+      isConsumable={true}
+      consumableFeedback="consumableFeedback"
+      isAlgorithmConsumable={true}
+      hasProviderFee={false}
+      retry={false}
+    />
+  )
+
+  const button = screen.getByText('Start Compute Job')
+  expect(button).toContainHTML('<button')
+})
+
+test('Renders "Order Compute Job" button', async () => {
+  render(
+    <ButtonBuy
+      action="compute"
+      disabled={false}
+      hasPreviousOrder={true}
+      hasDatatoken={true}
+      btSymbol="btSymbol"
+      dtSymbol="dtSymbol"
+      dtBalance="100000000000"
+      assetTimeout="1 day"
+      assetType="algorithm"
+      hasPreviousOrderSelectedComputeAsset={true}
+      hasDatatokenSelectedComputeAsset={true}
+      dtSymbolSelectedComputeAsset="dtSymbol"
+      dtBalanceSelectedComputeAsset="dtBalance"
+      selectedComputeAssetType="selectedComputeAssetType"
+      stepText=" "
+      isLoading={false}
+      type="submit"
+      priceType="free"
+      algorithmPriceType="free"
+      isBalanceSufficient={true}
+      isConsumable={true}
+      consumableFeedback="consumableFeedback"
+      isAlgorithmConsumable={true}
+      hasProviderFee={true}
+      retry={false}
+    />
+  )
+
+  const button = screen.getByText('Order Compute Job')
+  expect(button).toContainHTML('<button')
+})
+
+test('Renders "retry" button for compute without crashing', async () => {
+  render(
+    <ButtonBuy
+      action="compute"
+      disabled={false}
+      hasPreviousOrder={false}
+      hasDatatoken={true}
+      btSymbol="btSymbol"
+      dtSymbol="dtSymbol"
+      dtBalance="100000000000"
+      assetTimeout="1 day"
+      assetType="algorithm"
+      hasPreviousOrderSelectedComputeAsset={false}
+      hasDatatokenSelectedComputeAsset={true}
+      dtSymbolSelectedComputeAsset="dtSymbol"
+      dtBalanceSelectedComputeAsset="dtBalance"
+      selectedComputeAssetType="selectedComputeAssetType"
+      stepText=" "
+      isLoading={false}
+      type="submit"
+      priceType="fixed"
+      algorithmPriceType="free"
+      isBalanceSufficient={true}
+      isConsumable={true}
+      consumableFeedback="consumableFeedback"
+      isAlgorithmConsumable={true}
+      hasProviderFee={false}
       retry={true}
     />
   )
