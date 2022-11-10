8777c1b0dbd0ee6e27ce752dbda3e0ce0308b37a
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-11-10 11:46:39 +0300

    Refactoring compute tests

--- src/components/Asset/AssetActions/ButtonBuy/index.test.tsx
+++ src/components/Asset/AssetActions/ButtonBuy/index.test.tsx
@@ -51,7 +51,6 @@ describe('Asset/AssetActions/ButtonBuy', () => {
   //  TESTS FOR LOADING
   it('Renders Buy button without crashing', async () => {
     render(<ButtonBuy {...downloadProps} isLoading />)
-
     const button = screen.getByText('TEST')
     expect(button).toContainHTML('<Loader')
   })
@@ -59,7 +58,6 @@ describe('Asset/AssetActions/ButtonBuy', () => {
   //  TESTS FOR DOWNLOAD
   it('Renders Buy button without crashing', async () => {
     render(<ButtonBuy {...downloadProps} />)
-
     const button = screen.getByText('Buy for 1 day')
     expect(button).toContainHTML('<button')
   })
@@ -67,14 +65,12 @@ describe('Asset/AssetActions/ButtonBuy', () => {
   it('Renders Buy button without crashing when hasPreviousOrder=true', async () => {
     downloadProps.hasPreviousOrder = true
     render(<ButtonBuy {...downloadProps} />)
-
     const button = screen.getByText('Download')
     expect(button).toContainHTML('<button')
   })
 
   it('Renders retry button for download without crashing', async () => {
     render(<ButtonBuy {...downloadProps} retry />)
-
     const button = screen.getByText('Retry')
     expect(button).toContainHTML('<button')
   })
@@ -83,7 +79,6 @@ describe('Asset/AssetActions/ButtonBuy', () => {
     downloadProps.priceType = 'free'
     downloadProps.hasPreviousOrder = true
     render(<ButtonBuy {...downloadProps} />)
-
     const button = screen.getByText('Download')
     expect(button).toContainHTML('<button')
   })
@@ -92,7 +87,6 @@ describe('Asset/AssetActions/ButtonBuy', () => {
     downloadProps.priceType = 'free'
     downloadProps.hasPreviousOrder = false
     render(<ButtonBuy {...downloadProps} />)
-
     const button = screen.getByText('Get')
     expect(button).toContainHTML('<button')
   })
@@ -101,192 +95,51 @@ describe('Asset/AssetActions/ButtonBuy', () => {
     downloadProps.assetTimeout = 'Forever'
     downloadProps.priceType = 'fixed'
     downloadProps.isConsumable = false
-
     render(<ButtonBuy {...downloadProps} />)
     const button = screen.getByText('Buy')
     expect(button).toContainHTML('<button')
   })
 
   // TESTS FOR COMPUTE
-
   it('Renders "Buy Compute Job" button for compute without crashing', async () => {
     render(<ButtonBuy {...computeProps} />)
-
     const button = screen.getByText('Buy Compute Job')
     expect(button).toContainHTML('<button')
   })
 
   it('Renders "Buy Compute Job" button for compute without crashing', async () => {
-    render(
-      <ButtonBuy
-        action="compute"
-        disabled={false}
-        hasPreviousOrder={false}
-        hasDatatoken={true}
-        btSymbol="btSymbol"
-        dtSymbol="dtSymbol"
-        dtBalance="100000000000"
-        assetTimeout="1 day"
-        assetType="algorithm"
-        hasPreviousOrderSelectedComputeAsset={false}
-        hasDatatokenSelectedComputeAsset={false}
-        dtSymbolSelectedComputeAsset="dtSymbol"
-        dtBalanceSelectedComputeAsset="dtBalance"
-        selectedComputeAssetType="selectedComputeAssetType"
-        stepText=" "
-        isLoading={false}
-        type="submit"
-        priceType="fixed"
-        algorithmPriceType="free"
-        isBalanceSufficient={true}
-        isConsumable={true}
-        consumableFeedback="consumableFeedback"
-        isAlgorithmConsumable={true}
-        hasProviderFee={false}
-        retry={false}
-      />
-    )
-
+    computeProps.hasDatatokenSelectedComputeAsset = false
+    render(<ButtonBuy {...computeProps} />)
     const button = screen.getByText('Buy Compute Job')
     expect(button).toContainHTML('<button')
   })
 
   it('Renders "Start Compute Job" button', async () => {
-    render(
-      <ButtonBuy
-        action="compute"
-        disabled={false}
-        hasPreviousOrder={true}
-        hasDatatoken={true}
-        btSymbol="btSymbol"
-        dtSymbol="dtSymbol"
-        dtBalance="100000000000"
-        assetTimeout="1 day"
-        assetType="algorithm"
-        hasPreviousOrderSelectedComputeAsset={true}
-        hasDatatokenSelectedComputeAsset={false}
-        dtSymbolSelectedComputeAsset="dtSymbol"
-        dtBalanceSelectedComputeAsset="dtBalance"
-        selectedComputeAssetType="selectedComputeAssetType"
-        stepText=" "
-        isLoading={false}
-        type="submit"
-        priceType="fixed"
-        algorithmPriceType="free"
-        isBalanceSufficient={true}
-        isConsumable={true}
-        consumableFeedback="consumableFeedback"
-        isAlgorithmConsumable={true}
-        hasProviderFee={false}
-        retry={false}
-      />
-    )
-
+    computeProps.hasPreviousOrder = true
+    computeProps.hasPreviousOrderSelectedComputeAsset = true
+    render(<ButtonBuy {...computeProps} />)
     const button = screen.getByText('Start Compute Job')
     expect(button).toContainHTML('<button')
   })
 
   it('Renders "Order Compute Job" button', async () => {
-    render(
-      <ButtonBuy
-        action="compute"
-        disabled={false}
-        hasPreviousOrder={true}
-        hasDatatoken={true}
-        btSymbol="btSymbol"
-        dtSymbol="dtSymbol"
-        dtBalance="100000000000"
-        assetTimeout="1 day"
-        assetType="algorithm"
-        hasPreviousOrderSelectedComputeAsset={true}
-        hasDatatokenSelectedComputeAsset={false}
-        dtSymbolSelectedComputeAsset="dtSymbol"
-        dtBalanceSelectedComputeAsset="dtBalance"
-        selectedComputeAssetType="selectedComputeAssetType"
-        stepText=" "
-        isLoading={false}
-        type="submit"
-        priceType="free"
-        algorithmPriceType="free"
-        isBalanceSufficient={true}
-        isConsumable={true}
-        consumableFeedback="consumableFeedback"
-        isAlgorithmConsumable={true}
-        hasProviderFee={true}
-        retry={false}
-      />
-    )
-
+    computeProps.priceType = 'free'
+    computeProps.hasProviderFee = true
+    render(<ButtonBuy {...computeProps} />)
     const button = screen.getByText('Order Compute Job')
     expect(button).toContainHTML('<button')
   })
 
   it('Renders "Order Compute Job" button', async () => {
-    render(
-      <ButtonBuy
-        action="compute"
-        disabled={false}
-        hasPreviousOrder={true}
-        hasDatatoken={true}
-        btSymbol="btSymbol"
-        dtSymbol="dtSymbol"
-        dtBalance="100000000000"
-        assetTimeout="1 day"
-        assetType="algorithm"
-        hasPreviousOrderSelectedComputeAsset={true}
-        hasDatatokenSelectedComputeAsset={true}
-        dtSymbolSelectedComputeAsset="dtSymbol"
-        dtBalanceSelectedComputeAsset="dtBalance"
-        selectedComputeAssetType="selectedComputeAssetType"
-        stepText=" "
-        isLoading={false}
-        type="submit"
-        priceType="free"
-        algorithmPriceType="free"
-        isBalanceSufficient={false}
-        isConsumable={true}
-        consumableFeedback="consumableFeedback"
-        isAlgorithmConsumable={true}
-        hasProviderFee={true}
-        retry={false}
-      />
-    )
-
+    computeProps.hasDatatokenSelectedComputeAsset = true
+    computeProps.isBalanceSufficient = false
+    render(<ButtonBuy {...computeProps} />)
     const button = screen.getByText('Order Compute Job')
     expect(button).toContainHTML('<button')
   })
 
   it('Renders "retry" button for compute without crashing', async () => {
-    render(
-      <ButtonBuy
-        action="compute"
-        disabled={false}
-        hasPreviousOrder={false}
-        hasDatatoken={true}
-        btSymbol="btSymbol"
-        dtSymbol="dtSymbol"
-        dtBalance="100000000000"
-        assetTimeout="1 day"
-        assetType="algorithm"
-        hasPreviousOrderSelectedComputeAsset={false}
-        hasDatatokenSelectedComputeAsset={true}
-        dtSymbolSelectedComputeAsset="dtSymbol"
-        dtBalanceSelectedComputeAsset="dtBalance"
-        selectedComputeAssetType="selectedComputeAssetType"
-        stepText=" "
-        isLoading={false}
-        type="submit"
-        priceType="fixed"
-        algorithmPriceType="free"
-        isBalanceSufficient={true}
-        isConsumable={true}
-        consumableFeedback="consumableFeedback"
-        isAlgorithmConsumable={true}
-        hasProviderFee={false}
-        retry={true}
-      />
-    )
-
+    render(<ButtonBuy {...computeProps} retry />)
     const button = screen.getByText('Retry')
     expect(button).toContainHTML('<button')
   })
