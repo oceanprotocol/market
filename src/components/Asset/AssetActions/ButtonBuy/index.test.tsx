70ef1f1e17ac6bc9b3ba36a4cc1b7502450bb68a
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-11-10 17:04:26 +0300

    Refactoring tests

--- src/components/Asset/AssetActions/ButtonBuy/index.test.tsx
+++ src/components/Asset/AssetActions/ButtonBuy/index.test.tsx
@@ -49,96 +49,93 @@ const computeProps: ButtonBuyProps = {
 
 describe('Asset/AssetActions/ButtonBuy', () => {
   //  TESTS FOR LOADING
-  it('Renders Buy button without crashing', async () => {
+  it('Renders Buy button without crashing', () => {
     render(<ButtonBuy {...downloadProps} isLoading />)
     const button = screen.getByText('TEST')
     expect(button).toContainHTML('<Loader')
   })
 
   //  TESTS FOR DOWNLOAD
-  it('Renders Buy button without crashing', async () => {
+  it('Renders Buy button without crashing', () => {
     render(<ButtonBuy {...downloadProps} />)
     const button = screen.getByText('Buy for 1 day')
     expect(button).toContainHTML('<button')
   })
 
-  it('Renders Buy button without crashing when hasPreviousOrder=true', async () => {
-    downloadProps.hasPreviousOrder = true
-    render(<ButtonBuy {...downloadProps} />)
+  it('Renders Buy button without crashing when hasPreviousOrder=true', () => {
+    render(<ButtonBuy {...downloadProps} hasPreviousOrder />)
     const button = screen.getByText('Download')
     expect(button).toContainHTML('<button')
   })
 
-  it('Renders retry button for download without crashing', async () => {
+  it('Renders retry button for download without crashing', () => {
     render(<ButtonBuy {...downloadProps} retry />)
     const button = screen.getByText('Retry')
     expect(button).toContainHTML('<button')
   })
 
-  it('Renders get button for free download without crashing', async () => {
-    downloadProps.priceType = 'free'
-    downloadProps.hasPreviousOrder = true
-    render(<ButtonBuy {...downloadProps} />)
+  it('Renders get button for free download without crashing', () => {
+    render(<ButtonBuy {...downloadProps} priceType="free" hasPreviousOrder />)
     const button = screen.getByText('Download')
     expect(button).toContainHTML('<button')
   })
 
-  it('Renders "Get" button for free assets without crashing', async () => {
-    downloadProps.priceType = 'free'
-    downloadProps.hasPreviousOrder = false
-    render(<ButtonBuy {...downloadProps} />)
+  it('Renders "Get" button for free assets without crashing', () => {
+    render(<ButtonBuy {...downloadProps} priceType="free" />)
     const button = screen.getByText('Get')
     expect(button).toContainHTML('<button')
   })
 
-  it('Renders Buy button without crashing', async () => {
-    downloadProps.assetTimeout = 'Forever'
-    downloadProps.priceType = 'fixed'
-    downloadProps.isConsumable = false
-    render(<ButtonBuy {...downloadProps} />)
+  it('Renders Buy button without crashing', () => {
+    render(
+      <ButtonBuy
+        {...downloadProps}
+        assetTimeout="Forever"
+        isConsumable={false}
+      />
+    )
     const button = screen.getByText('Buy')
     expect(button).toContainHTML('<button')
   })
 
   // TESTS FOR COMPUTE
-  it('Renders "Buy Compute Job" button for compute without crashing', async () => {
+  it('Renders "Buy Compute Job" button for compute without crashing', () => {
     render(<ButtonBuy {...computeProps} />)
     const button = screen.getByText('Buy Compute Job')
     expect(button).toContainHTML('<button')
   })
 
-  it('Renders "Buy Compute Job" button for compute without crashing', async () => {
-    computeProps.hasDatatokenSelectedComputeAsset = false
-    render(<ButtonBuy {...computeProps} />)
+  it('Renders "Buy Compute Job" button for compute without crashing', () => {
+    render(<ButtonBuy {...computeProps} hasDatatokenSelectedComputeAsset />)
     const button = screen.getByText('Buy Compute Job')
     expect(button).toContainHTML('<button')
   })
 
-  it('Renders "Start Compute Job" button', async () => {
-    computeProps.hasPreviousOrder = true
-    computeProps.hasPreviousOrderSelectedComputeAsset = true
-    render(<ButtonBuy {...computeProps} />)
+  it('Renders "Start Compute Job" button', () => {
+    render(
+      <ButtonBuy
+        {...computeProps}
+        hasPreviousOrder
+        hasPreviousOrderSelectedComputeAsset
+      />
+    )
     const button = screen.getByText('Start Compute Job')
     expect(button).toContainHTML('<button')
   })
 
-  it('Renders "Order Compute Job" button', async () => {
-    computeProps.priceType = 'free'
-    computeProps.hasProviderFee = true
-    render(<ButtonBuy {...computeProps} />)
+  it('Renders "Order Compute Job" button', () => {
+    render(<ButtonBuy {...computeProps} priceType="free" hasProviderFee />)
     const button = screen.getByText('Order Compute Job')
     expect(button).toContainHTML('<button')
   })
 
-  it('Renders "Order Compute Job" button', async () => {
-    computeProps.hasDatatokenSelectedComputeAsset = true
-    computeProps.isBalanceSufficient = false
-    render(<ButtonBuy {...computeProps} />)
+  it('Renders "Order Compute Job" button', () => {
+    render(<ButtonBuy {...computeProps} priceType="free" hasProviderFee />)
     const button = screen.getByText('Order Compute Job')
     expect(button).toContainHTML('<button')
   })
 
-  it('Renders "retry" button for compute without crashing', async () => {
+  it('Renders "retry" button for compute without crashing', () => {
     render(<ButtonBuy {...computeProps} retry />)
     const button = screen.getByText('Retry')
     expect(button).toContainHTML('<button')
