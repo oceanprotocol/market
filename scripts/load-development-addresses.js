eba86ae2cb852cb837dd696a68e62f19a99205c3
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2022-11-15 07:35:07 +0200

    update readme

--- scripts/load-development-addresses.js
+++ scripts/load-development-addresses.js
@@ -25,13 +25,6 @@ envVars.push(`NEXT_PUBLIC_DISPENSER_ADDRESS='${addresses.Dispenser}'`)
 envVars.push(`NEXT_PUBLIC_OCEAN_TOKEN_ADDRESS='${addresses.Ocean}'`)
 envVars.push(`NEXT_PUBLIC_MARKET_DEVELOPMENT='true'`)
 
-/*
- check this on oceanjs and move thinks there as well if not exposed
-*/
-envVars.push(`NEXT_PUBLIC_METADATACACHE_URI='http://127.0.0.1:5000'`)
-envVars.push(`NEXT_PUBLIC_SUBGRAPH_URI='http://127.0.0.1:9000'`)
-envVars.push(`NEXT_RBAC_URL='http://127.0.0.1:3000'`)
-
 var stream = fs.createWriteStream('.env', { flags: 'a' })
 
 envVars.forEach((envVar) => {
