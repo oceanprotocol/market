c2514debf05dc46a1f3ee9da7bb4e27d4ea4fde9
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-11-15 18:58:07 +0300

    Adding test for FormActions

--- src/components/Asset/Edit/FormEditMetadata.test.tsx
+++ src/components/Asset/Edit/FormEditMetadata.test.tsx
@@ -1,22 +1,32 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
-import Formik, { useFormikContext } from 'formik'
+import Formik, { FormikErrors, FormikTouched, FormikState } from 'formik'
 import FormEditMetadata from './FormEditMetadata'
 import content from '../../../../content/pages/editMetadata.json'
 
-jest.mock('formik')
+let fieldMock = {}
+const metaMock = {}
+const helperMock = {}
+
+jest.mock('formik', () => ({
+  ...jest.requireActual('formik'),
+  useField: jest.fn(() => {
+    return [fieldMock, metaMock, helperMock]
+  })
+}))
 
 describe('src/components/Asset/Edit/FormEditMetadata.tsx', () => {
-  const useFormikContextMock = jest.spyOn(Formik, 'useFormikContext')
+  // const useFormikContextMock = jest.spyOn(Formik, 'useFormikContext')
 
   it('renders fixed price', () => {
-    // render()
-    // (useFormikContext as jest.Mock).
-    // <FormEditMetadata
-    //   data={content.form.data}
-    //   showPrice={true}
-    //   isComputeDataset={false}
-    // />
+    fieldMock = { foo: 'bar' }
+    render(
+      <FormEditMetadata
+        data={content.form.data}
+        showPrice={true}
+        isComputeDataset={false}
+      />
+    )
     expect(screen.getByText('New Title')).toBeInTheDocument()
   })
 })
