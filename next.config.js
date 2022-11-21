c03b203d844ba386d495e55de01d7122dbece944
ogiste <alfred.opon@strathmore.edu>
2022-11-21 22:07:06 +0300

    - Ignore linting for staging testing

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ module.exports = (phase, { defaultConfig }) => {
    * @type {import('next').NextConfig}
    */
   const nextConfig = {
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true
+    },
     webpack: (config, options) => {
       config.module.rules.push(
         {
