e8caa5392824011da906582cd9b564887bce1fc6
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-01-02 14:59:01 +0000

    Updating tests

--- src/@utils/url/index.test.ts
+++ src/@utils/url/index.test.ts
@@ -11,17 +11,11 @@ describe('@utils/url', () => {
 describe('isGoogleUrl', () => {
   it('should return true if the url is a google domain', () => {
     expect(isGoogleUrl('https://google.com')).toBe(true)
-  })
-  it('should return true if the url is a google domain', () => {
     expect(isGoogleUrl('https://drive.google.com')).toBe(true)
   })
   it('should return false if the url is not a google domain', () => {
     expect(isGoogleUrl('https://drive.gloogle.com')).toBe(false)
-  })
-  it('should return false if the url is not a google domain', () => {
     expect(isGoogleUrl('https://drive.google.test.com')).toBe(false)
-  })
-  it('should return false if the url is not a google domain', () => {
     expect(isGoogleUrl('https://google.com.test.com')).toBe(false)
   })
 })
