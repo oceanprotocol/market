36ef3276e0d8c5198bab5274013e93a64fcd593a
mihaisc <mihai.scarlat@smartcontrol.ro>
2023-03-21 11:20:14 +0200

    add posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 // import App from "next/app";
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect } from 'react'
 import type { AppProps /*, AppContext */ } from 'next/app'
 import Web3Provider from '@context/Web3'
 import { UserPreferencesProvider } from '@context/UserPreferences'
@@ -12,25 +12,53 @@ import '@oceanprotocol/typographies/css/ocean-typo.css'
 import '../stylesGlobal/styles.css'
 import Decimal from 'decimal.js'
 import MarketMetadataProvider from '@context/MarketMetadata'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import { useRouter } from 'next/router'
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.posthog.com/',
+    // Disable in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing()
+    }
+  })
+}
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   Decimal.set({ rounding: 1 })
+  const router = useRouter()
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture('$pageview')
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
   return (
-    <MarketMetadataProvider>
-      <Web3Provider>
-        <UrqlProvider>
-          <UserPreferencesProvider>
-            <PricesProvider>
-              <ConsentProvider>
-                <App>
-                  <Component {...pageProps} />
-                </App>
-              </ConsentProvider>
-            </PricesProvider>
-          </UserPreferencesProvider>
-        </UrqlProvider>
-      </Web3Provider>
-    </MarketMetadataProvider>
+    <PostHogProvider client={posthog}>
+      <MarketMetadataProvider>
+        <Web3Provider>
+          <UrqlProvider>
+            <UserPreferencesProvider>
+              <PricesProvider>
+                <ConsentProvider>
+                  <App>
+                    <Component {...pageProps} />
+                  </App>
+                </ConsentProvider>
+              </PricesProvider>
+            </UserPreferencesProvider>
+          </UrqlProvider>
+        </Web3Provider>
+      </MarketMetadataProvider>
+    </PostHogProvider>
   )
 }
 
