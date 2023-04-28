73fa000cdc2f5a33f121b67d155ca346b9d8d0b6
Luca Milanese <luca.milanese90@gmail.com>
2023-04-28 18:09:26 +0200

    feat: parse consumer parameters in edit form

--- src/components/Asset/Edit/_constants.ts
+++ src/components/Asset/Edit/_constants.ts
@@ -1,9 +1,32 @@
 import { Metadata, ServiceComputeOptions } from '@oceanprotocol/lib'
 import { secondsToString } from '@utils/ddo'
 import { ComputeEditForm, MetadataEditForm } from './_types'
+import {
+  AlgorithmConsumerParameter,
+  MetadataAlgorithmExtended
+} from '@components/Publish/_types'
+
+function parseConsumerParameters(
+  consumerParameters: AlgorithmConsumerParameter[]
+): AlgorithmConsumerParameter[] {
+  if (!consumerParameters?.length) return []
+
+  return consumerParameters.map((param) =>
+    param.type === 'select'
+      ? {
+          ...param,
+          options: JSON.parse(param.options as string)
+        }
+      : param.type === 'number'
+      ? { ...param, default: Number(param.default) }
+      : param.type === 'boolean'
+      ? { ...param, default: param.default === 'true' }
+      : param
+  )
+}
 
 export function getInitialValues(
-  metadata: Metadata,
+  metadata: Metadata & { algorithm?: MetadataAlgorithmExtended },
   timeout: number,
   price: string,
   paymentCollector: string,
@@ -18,6 +41,10 @@ export function getInitialValues(
     timeout: secondsToString(timeout),
     author: metadata?.author,
     tags: metadata?.tags,
+    usesConsumerParameters: metadata?.algorithm?.consumerParameters?.length > 0,
+    consumerParameters: parseConsumerParameters(
+      metadata?.algorithm?.consumerParameters
+    ),
     paymentCollector,
     assetState
   }
