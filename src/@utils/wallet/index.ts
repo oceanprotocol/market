6e802593fbe90f0a888a04631cfeaa8cd62f8beb
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2023-05-22 11:41:12 +0300

    bump lib to next.5 and add more modal style

--- src/@utils/wallet/index.ts
+++ src/@utils/wallet/index.ts
@@ -24,6 +24,7 @@ export const connectKitTheme = {
   '--ck-font-family': 'var(--font-family-base)',
   '--ck-border-radius': 'var(--border-radius)',
   '--ck-overlay-background': 'var(--background-body-transparent)',
+  '--ck-modal-box-shadow': '0 0 20px 20px var(--box-shadow-color)',
   '--ck-body-background': 'var(--background-body)',
   '--ck-body-color': 'var(--font-color-text)',
   '--ck-primary-button-border-radius': 'var(--border-radius)',
