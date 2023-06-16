9db01b6a30e6042f1e651fa161e87ef52b6b3771
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2023-06-16 17:34:15 +0300

    remove not used networks (#1933)

--- app.config.js
+++ app.config.js
@@ -11,10 +11,10 @@ module.exports = {
 
   // List of chainIds which metadata cache queries will return by default.
   // This preselects the Chains user preferences.
-  chainIds: [1, 137, 56, 246, 1285],
+  chainIds: [1, 137],
 
   // List of all supported chainIds. Used to populate the Chains user preferences list.
-  chainIdsSupported: [1, 137, 56, 246, 1285, 5, 80001],
+  chainIdsSupported: [1, 137, 5, 80001],
 
   defaultDatatokenTemplateIndex: 2,
   // The ETH address the marketplace fee will be sent to.
