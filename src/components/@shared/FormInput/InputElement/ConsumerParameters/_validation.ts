c776646a54e9444528b59fd847e146e9f1765eed
Luca Milanese <luca.milanese90@gmail.com>
2023-06-30 11:59:35 +0200

    feat: add string length min/max to consumer parameters validation

--- src/components/@shared/FormInput/InputElement/ConsumerParameters/_validation.ts
+++ src/components/@shared/FormInput/InputElement/ConsumerParameters/_validation.ts
@@ -34,10 +34,21 @@ export const validationConsumerParameters: {
       )
       return occasions.length === 1
     })
+    .min(4, (param) => `Name must be at least ${param.min} characters`)
+    .max(50, (param) => `Name must have maximum ${param.max} characters`)
     .required('Required'),
   type: Yup.string().oneOf(paramTypes).required('Required'),
-  description: Yup.string().required('Required'),
-  label: Yup.string().required('Required'),
+  description: Yup.string()
+    .min(10, (param) => `Description must be at least ${param.min} characters`)
+    .max(
+      500,
+      (param) => `Description must have maximum ${param.max} characters`
+    )
+    .required('Required'),
+  label: Yup.string()
+    .min(4, (param) => `Label must be at least ${param.min} characters`)
+    .max(50, (param) => `Label must have maximum ${param.max} characters`)
+    .required('Required'),
   required: Yup.string().oneOf(['optional', 'required']).required('Required'),
   default: Yup.mixed().required('Required'),
   options: Yup.array().when('type', {
