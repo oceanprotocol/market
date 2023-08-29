b7a28df97e60de748f60ac7286a0424d607dca09
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2023-08-29 10:02:45 +0300

    Fix template id type (#1958)
    * fix template id type
    
    * remove logs

--- src/@utils/accessDetailsAndPricing.ts
+++ src/@utils/accessDetailsAndPricing.ts
@@ -111,7 +111,10 @@ function getAccessDetailsFromTokenPrice(
     // the last valid order should be the last reuse order tx id if there is one
     accessDetails.validOrderTx = reusedOrder?.tx || order?.tx
   }
-  accessDetails.templateId = tokenPrice.templateId
+  accessDetails.templateId =
+    typeof tokenPrice.templateId === 'string'
+      ? parseInt(tokenPrice.templateId)
+      : tokenPrice.templateId
   // TODO: fetch order fee from sub query
   accessDetails.publisherMarketOrderFee = tokenPrice?.publishMarketFeeAmount
 
