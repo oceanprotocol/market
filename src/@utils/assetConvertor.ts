ef5b236482d9d925b2c74b6bf0a0ded6d23b666e
Ana <84312885+AnaLoznianu@users.noreply.github.com>
2023-11-10 13:24:47 +0200

    Added normalize url function to check the endpoint (#2000)

--- src/@utils/assetConvertor.ts
+++ src/@utils/assetConvertor.ts
@@ -1,6 +1,7 @@
 import { PublisherTrustedAlgorithm, Asset } from '@oceanprotocol/lib'
 import { AssetSelectionAsset } from '@shared/FormInput/InputElement/AssetSelection'
 import { getServiceByName } from './ddo'
+import normalizeUrl from 'normalize-url'
 
 export async function transformAssetToAssetSelection(
   datasetProviderEndpoint: string,
@@ -15,7 +16,8 @@ export async function transformAssetToAssetSelection(
 
     if (
       asset?.stats?.price?.value >= 0 &&
-      algoService?.serviceEndpoint === datasetProviderEndpoint
+      normalizeUrl(algoService?.serviceEndpoint) ===
+        normalizeUrl(datasetProviderEndpoint)
     ) {
       let selected = false
       selectedAlgorithms?.forEach((algorithm: PublisherTrustedAlgorithm) => {
