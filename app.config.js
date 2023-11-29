422c43b37c111fdae91c67478bc2be4347e7d521
Ana Loznianu <ana@oceanprotocol.com>
2023-11-29 13:24:20 +0200

    Enabled optimism network by default

--- app.config.js
+++ app.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   // List of chainIds which metadata cache queries will return by default.
   // This preselects the Chains user preferences.
-  chainIds: [1, 137],
+  chainIds: [1, 137, 10],
 
   // List of all supported chainIds. Used to populate the Chains user preferences list.
   chainIdsSupported: [1, 137, 10, 5, 80001, 11155111],
