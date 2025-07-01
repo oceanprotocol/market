b35bf2cb6c6c5e21772694730829f50bdc7b6876
ahmedraza118 <ahmedrazachrtx@gmail.com>
2025-07-01 09:35:08 +0300

    updating price conversion for weth

--- src/@context/Prices/_utils.ts
+++ src/@context/Prices/_utils.ts
@@ -3,15 +3,18 @@
 //
 export function getCoingeckoTokenId(symbol: string) {
   // can be WETH or mOCEAN
-  const isOcean = symbol?.toLowerCase().includes('weth')
+  const isOcean = symbol?.toLowerCase().includes('ocean')
   // can be H2O or H20
   // const isH2o = symbol?.toLowerCase().includes('h2')
+  const isWeth = symbol?.toLowerCase() === 'weth'
   const isEth = symbol?.toLowerCase() === 'eth'
   const isMatic = symbol?.toLowerCase() === 'matic'
 
   const priceTokenId = isOcean
     ? 'ocean-protocol'
     : isEth
+    ? 'ethereum'
+    : isWeth
     ? 'ethereum'
     : isMatic
     ? 'matic-network'
